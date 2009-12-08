6bea678ebd34302880779861571b6a36937fa07d
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2009-12-07 20:57:42 -0800

    Added method to clean up ActivityStreams without associates.

--- app/models/activity_stream.rb
+++ app/models/activity_stream.rb
@@ -2,4 +2,13 @@ class ActivityStream < ActiveRecord::Base
   belongs_to :user
   belongs_to :subscriber, :class_name => 'User', :foreign_key => 'subscriber_id'
   belongs_to :activity_object, :polymorphic => true
+  
+  # Destroys activity streams that have an activity_object that no longer 
+  # exists
+  def self.destroy_nils_for_activity_object_type(klass)
+    as = ActivityStream.all(
+      :joins => "LEFT OUTER JOIN #{klass.table_name} ON #{klass.table_name}.id = activity_streams.activity_object_id", 
+      :conditions => "#{klass.table_name}.id is null AND activity_object_type = '#{klass.to_s}'")
+    as.each(&:destroy)
+  end
 end
