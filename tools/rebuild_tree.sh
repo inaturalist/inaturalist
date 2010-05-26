a482bb51914c5398a302119fc3418443e199fd9f
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2010-05-25 22:11:08 -0700

    Added little script for rebuilding the tree.

---
+++ tools/rebuild_tree.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Seems that sometimes our tree gets out of whack and needs to be rebuilt. 
+# This is a long and painful process that probably requires taking down the
+# site for several hours.  You may also want to comment out some callbacks in
+# app/models/taxon.rb to help this run a bit quicker, except the after_moves.
+
+
+# Yes, twice.  This does a lot of merging, not 100% convinced it gets
+# everything the 1st time
+ruby script/runner "ThinkingSphinx.deltas_enabled = false; Taxon.find_duplicates"
+ruby script/runner "ThinkingSphinx.deltas_enabled = false; Taxon.find_duplicates"
+
+ruby script/runner 'ThinkingSphinx.deltas_enabled = false; Taxon.rebuild!'
+ruby script/runner 'ListedTaxon.update_all_taxon_attributes'
+
+rake ts:in
