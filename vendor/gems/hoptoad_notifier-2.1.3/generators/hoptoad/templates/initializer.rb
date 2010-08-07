f8e307aade83142c42cfa63fa3855ace98163c93
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2010-08-06 23:14:19 -0700

    Removed old hoptoad (stupid git...)

--- vendor/gems/hoptoad_notifier-2.1.3/generators/hoptoad/templates/initializer.rb
+++
@@ -1,6 +0,0 @@
-<% if Rails::VERSION::MINOR < 2 -%>
-require 'hoptoad_notifier/rails'
-<% end -%>
-HoptoadNotifier.configure do |config|
-  config.api_key = '<%= api_key %>'
-end
--- vendor/gems/hoptoad_notifier-2.1.3/lib/hoptoad_notifier/version.rb
+++
@@ -1,3 +0,0 @@
-module HoptoadNotifier
-  VERSION = "2.1.3".freeze
-end
