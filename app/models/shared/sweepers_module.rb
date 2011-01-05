d7ef1f24141c5ef190627d521526af301d389f56
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2011-01-05 09:16:46 -0800

    Disabled delayed sweeping until I figure out why it's broken.

--- app/models/shared/sweepers_module.rb
+++ app/models/shared/sweepers_module.rb
@@ -1,53 +1,89 @@
 module Shared::SweepersModule
   
-  def self.included(base)
-    base.extend ActionController::Caching::Fragments
-    base.extend ExpireHelpers
-  end
+  # def self.included(base)
+  #   base.extend ExpireHelpers
+  # end
+  # 
+  # module ExpireHelpers
+  #   def expire_observation_components(observation)
+  #     controller.expire_fragment(
+  #       :controller => 'observations', 
+  #       :action => 'component', 
+  #       :id => observation.id)
+  #     controller.expire_fragment(
+  #       :controller => 'observations', 
+  #       :action => 'component', 
+  #       :id => observation.id,
+  #       :for_owner => true)
+  # 
+  #     # Expire page-cached tile_points JSON
+  #     if observation.latitude? && observation.longitude?
+  #       SPHERICAL_MERCATOR.levels.times do |zoom|
+  #         x, y = SPHERICAL_MERCATOR.from_ll_to_pixel(
+  #           [observation.longitude, observation.latitude], zoom)
+  #         controller.expire_page :controller => 'observations', :action => 'tile_points', 
+  #           :zoom => zoom, :x => x, :y => y
+  #       end
+  #     end
+  #   end
+  # 
+  #   def expire_listed_taxon(listed_taxon)
+  #     controller.expire_fragment(:controller => 'listed_taxa', :action => 'show', :id => listed_taxon.id)
+  #     controller.expire_fragment(:controller => 'listed_taxa', :action => 'show', :id => listed_taxon.id, :for_owner => true)
+  #     controller.expire_fragment(List.icon_preview_cache_key(listed_taxon.list_id))
+  #     ListedTaxon::ORDERS.each do |order|
+  #       controller.expire_fragment(:controller => 'observations', :action => 'add_from_list', :id => listed_taxon.list_id, :order => order)
+  #     end
+  #   end
+  # 
+  #   def expire_listed_taxa(taxon)
+  #     ListedTaxon.find_each(:conditions => ["taxon_id = ?", taxon]) do |lt|
+  #       expire_listed_taxon(lt)
+  #     end
+  #   end
+  #   
+  #   def controller
+  #     is_a?(ApplicationController) ? self : ApplicationController.new
+  #   end
+  # end
+  # 
+  # private
+  # include ExpireHelpers
   
-  module ExpireHelpers
-    def expire_observation_components(observation)
-      controller.expire_fragment(
-        :controller => 'observations', 
-        :action => 'component', 
-        :id => observation.id)
-      controller.expire_fragment(
-        :controller => 'observations', 
-        :action => 'component', 
-        :id => observation.id,
-        :for_owner => true)
+  def expire_observation_components(observation)
+    expire_fragment(
+      :controller => 'observations', 
+      :action => 'component', 
+      :id => observation.id)
+    expire_fragment(
+      :controller => 'observations', 
+      :action => 'component', 
+      :id => observation.id,
+      :for_owner => true)
 
-      # Expire page-cached tile_points JSON
-      if observation.latitude? && observation.longitude?
-        SPHERICAL_MERCATOR.levels.times do |zoom|
-          x, y = SPHERICAL_MERCATOR.from_ll_to_pixel(
-            [observation.longitude, observation.latitude], zoom)
-          controller.expire_page :controller => 'observations', :action => 'tile_points', 
-            :zoom => zoom, :x => x, :y => y
-        end
+    # Expire page-cached tile_points JSON
+    if observation.latitude? && observation.longitude?
+      SPHERICAL_MERCATOR.levels.times do |zoom|
+        x, y = SPHERICAL_MERCATOR.from_ll_to_pixel(
+          [observation.longitude, observation.latitude], zoom)
+        expire_page :controller => 'observations', :action => 'tile_points', 
+          :zoom => zoom, :x => x, :y => y
       end
-    end
-
-    def expire_listed_taxon(listed_taxon)
-      controller.expire_fragment(:controller => 'listed_taxa', :action => 'show', :id => listed_taxon)
-      controller.expire_fragment(:controller => 'listed_taxa', :action => 'show', :id => listed_taxon, :for_owner => true)
-      controller.expire_fragment(List.icon_preview_cache_key(listed_taxon.list_id))
-      ListedTaxon::ORDERS.each do |order|
-        controller.expire_fragment(:controller => 'observations', :action => 'add_from_list', :id => listed_taxon.list_id, :order => order)
-      end
-    end
-
-    def expire_listed_taxa(taxon)
-      ListedTaxon.find_each(:conditions => ["taxon_id = ?", taxon]) do |lt|
-        expire_listed_taxon(lt)
-      end
-    end
-    
-    def controller
-      is_a?(ApplicationController) ? self : ApplicationController.new
     end
   end
-  
-  private
-  include ExpireHelpers
+
+  def expire_listed_taxon(listed_taxon)
+    expire_fragment(:controller => 'listed_taxa', :action => 'show', :id => listed_taxon.id)
+    expire_fragment(:controller => 'listed_taxa', :action => 'show', :id => listed_taxon.id, :for_owner => true)
+    expire_fragment(List.icon_preview_cache_key(listed_taxon.list_id))
+    ListedTaxon::ORDERS.each do |order|
+      expire_fragment(:controller => 'observations', :action => 'add_from_list', :id => listed_taxon.list_id, :order => order)
+    end
+  end
+
+  def expire_listed_taxa(taxon)
+    ListedTaxon.find_each(:conditions => ["taxon_id = ?", taxon]) do |lt|
+      expire_listed_taxon(lt)
+    end
+  end
 end
--- app/models/taxon_sweeper.rb
+++ app/models/taxon_sweeper.rb
@@ -2,24 +2,37 @@ class TaxonSweeper < ActionController::Caching::Sweeper
   observe Taxon
   include Shared::SweepersModule
   
+  # def after_update(taxon)
+  #   TaxonSweeper.send_later(:after_update, taxon.id)
+  #   true
+  # end
+  # 
+  # def after_destroy(taxon)
+  #   TaxonSweeper.send_later(:after_destroy, taxon.id)
+  #   true
+  # end
+  # 
+  # def self.after_update(taxon)
+  #   return unless (taxon = Taxon.find_by_id(taxon) unless taxon.is_a?(Taxon))
+  #   Observation.of(taxon).find_each {|obs| expire_observation_components(obs)}
+  #   expire_listed_taxa(taxon)
+  #   controller.expire_fragment(:controller => 'taxa', :action => 'photos', :id => taxon.id, :partial => "photo")
+  # end
+  # 
+  # def self.after_destroy(taxon)
+  #   return unless (taxon = Taxon.find_by_id(taxon) unless taxon.is_a?(Taxon))
+  #   Observation.of(taxon).find_each {|obs| expire_observation_components(obs)}
+  #   expire_listed_taxa(taxon)
+  # end
+  
   def after_update(taxon)
-    TaxonSweeper.send_later(:after_update, taxon.id)
-    true
-  end
-  
-  def after_destroy(taxon)
-    TaxonSweeper.send_later(:after_destroy, taxon.id)
-    true
-  end
-  
-  def self.after_update(taxon)
     return unless (taxon = Taxon.find_by_id(taxon) unless taxon.is_a?(Taxon))
     Observation.of(taxon).find_each {|obs| expire_observation_components(obs)}
     expire_listed_taxa(taxon)
-    controller.expire_fragment(:controller => 'taxa', :action => 'photos', :id => taxon.id, :partial => "photo")
+    expire_fragment(:controller => 'taxa', :action => 'photos', :id => taxon.id, :partial => "photo")
   end
   
-  def self.after_destroy(taxon)
+  def after_destroy(taxon)
     return unless (taxon = Taxon.find_by_id(taxon) unless taxon.is_a?(Taxon))
     Observation.of(taxon).find_each {|obs| expire_observation_components(obs)}
     expire_listed_taxa(taxon)
