18d542158a4ec461651f6f803b536dec9e462759
altay <aguvench@gmail.com>
2011-04-29 10:42:20 +0000

    don't send confirm+welcome emails if user email is blank

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,10 +1,10 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
     user.reload
-    UserMailer.deliver_signup_notification(user)
+    UserMailer.deliver_signup_notification(user) unless user.email.blank?
   end
   def after_save(user)
     user.reload
-    UserMailer.deliver_activation(user) if user.recently_activated?
+    UserMailer.deliver_activation(user) if (user.recently_activated? && !user.email.blank?)
   end
 end
