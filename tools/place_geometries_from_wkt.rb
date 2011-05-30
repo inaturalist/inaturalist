87cccff5a963ba86ce91c2eb90a1600d3a720240
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2011-05-29 20:05:00 -0700

    Added tools to export/import place geometries to/from WKT.

---
+++ tools/place_geometries_from_wkt.rb
@@ -0,0 +1,12 @@
+# Dump all PlaceGeometry geometries to WKT files named by place_id
+Place.find_each do |place|
+  print "#{place.display_name} (#{place.id}): "
+  begin
+    open "place_wkts/#{place.id}.wkt" do |wktfile|
+      place.save_geom(MultiPolygon.from_ewkt(wktfile.read))
+      puts "Saved"
+    end
+  rescue => e
+    puts e.message
+  end
+end
