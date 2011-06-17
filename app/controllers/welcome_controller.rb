7704f2bfe7ed30a93b45c097f8c23fc2e81b8358
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2011-06-17 12:46:06 -0700

    More fixes.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,7 +11,7 @@ class WelcomeController < ApplicationController
   
   def index
     @announcement = Announcement.last(:conditions => [
-      "placement = 'welcome/index' AND ? BETWEEN start AND end", Time.now.utc])
+      "placement = 'welcome/index' AND ? BETWEEN 'start' AND 'end'", Time.now.utc])
     @observations = Observation.all( 
       :include => :photos,
       :limit => 4,
