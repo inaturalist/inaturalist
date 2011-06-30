b952773c4071a5d39de13545f74c962207fc8f80
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2011-06-29 22:21:50 -0700

    Primarily fixed issue with session erasing after auth from Google.
    Protect from forgery screws this up.  See
    
    http://stackoverflow.com/questions/5049994/omniauth-current-session-not-loaded-on-openid-callback
    
    Got ca-bundle.crt from http://certifie.com/ca-bundle/ca-bundle.crt.txt

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 #config/initializers/omniauth.rb
 require 'openid/store/filesystem'
+require "openid/fetchers"
+OpenID.fetcher.ca_file = "#{Rails.root}/config/ca-bundle.crt"
 
 ActionController::Dispatcher.middleware.use OmniAuth::Builder do #if you are using rails 2.3.x
 #Rails.application.config.middleware.use OmniAuth::Builder do #comment out the above line and use this if you are using rails 3
