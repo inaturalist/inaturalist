4d5ac163ec0a6cf3aa2365bb6170653d5b087692
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2011-07-02 14:19:14 -0700

    Made sure taxon names don't have html.

--- app/models/taxon_name.rb
+++ app/models/taxon_name.rb
@@ -10,7 +10,7 @@ class TaxonName < ActiveRecord::Base
                           :scope => [:lexicon, :taxon_id], 
                           :message => "already exists for this taxon in this lexicon",
                           :case_sensitive => false
-  before_validation :strip_name, :remove_rank_from_name, :normalize_lexicon
+  before_validation :strip_tags, :strip_name, :remove_rank_from_name, :normalize_lexicon
   before_validation do |tn|
     tn.name = tn.name.capitalize if tn.lexicon == LEXICONS[:SCIENTIFIC_NAMES]
   end
@@ -75,16 +75,24 @@ class TaxonName < ActiveRecord::Base
   
   def strip_name
     self.name.strip! if self.name
+    true
+  end
+  
+  def strip_tags
+    self.name.gsub!(/<.*?>/, '')
+    true
   end
   
   def remove_rank_from_name
     return unless self.lexicon == LEXICONS[:SCIENTIFIC_NAMES]
     self.name = Taxon.remove_rank_from_name(self.name)
+    true
   end
   
   def normalize_lexicon
     return true if lexicon.blank?
     self.lexicon = LEXICONS[lexicon.upcase.to_sym] if LEXICONS[lexicon.upcase.to_sym]
+    true
   end
   
   def update_unique_names
