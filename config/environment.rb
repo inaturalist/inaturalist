3d6703e15c65f2951de9f90906f75444e3b24a81
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2011-07-21 18:03:03 -0700

    Ugh ugh ugh.

--- config/environment.rb
+++ config/environment.rb
@@ -49,7 +49,7 @@ Rails::Initializer.run do |config|
   # Make sure the secret is at least 30 characters and all random, 
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
-    :key => INAT_CONFIG['rails']['secret'],
+    :key => INAT_CONFIG['rails']['session_key'],
     :secret => INAT_CONFIG['rails']['secret']
   }
 
