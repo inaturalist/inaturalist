166ff8662ca1d0affd3da39590e1b931bd464ca7
Altay Guvench <aguvench@gmail.com>
2011-09-17 01:43:48 -0400

    facebook/options page, and import fb albums in observations/new

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -6,7 +6,8 @@ class FacebookController < ApplicationController
   # signup process.
   def options
     begin
-      @album_cover_photos = @user.facebook_album_cover_photos[0..5]
+      #@album_cover_photos = @user.facebook_album_cover_photos[0..5]
+      @album_cover_photos = @user.facebook_albums[0..3]
 #      unless @user.flickr_identity
 #        @flickr_url = @flickr.auth.url_webapp(:write)
 #      else
@@ -20,6 +21,30 @@ class FacebookController < ApplicationController
                        "help@inaturalist.org if you still can't get this " + 
                        "working.  Error: #{e.message}"
     end
+  end
+
+  # Return an HTML fragment containing a list of the user's fb albums
+  def albums
+    @albums = @user.facebook_albums
+    render(:partial => 'facebook/albums')
+  end
+
+  # Return an HTML fragment containing photos in the album with the given param[:id]
+  def album
+    render(:text=>@user.facebook_album_photos(params[:id]).to_json)
+#    @photos = @user.facebook_album_photos(params[:id])
+#    @synclink_base = params[:synclink_base] unless params[:synclink_base].blank?
+#    respond_to do |format|
+#      format.html do
+#        render :partial => 'photos/photo_list_form', 
+#               :locals => {
+#                 :photos => @photos, 
+#                 :index => params[:index],
+#                 :synclink_base => @synclink_base,
+#                 :local_photos => false
+#               }
+#      end
+#    end
   end
   
 end
