545fbde5432fc86de10da11a7489d388330d1d2b
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2011-09-26 22:12:23 -0700

    Minor change for flickr invites.

--- app/controllers/provider_authorizations_controller.rb
+++ app/controllers/provider_authorizations_controller.rb
@@ -52,7 +52,7 @@ class ProviderAuthorizationsController < ApplicationController
         self.current_user = User.create_from_omniauth(auth_info)
         handle_remember_cookie! true # set 'remember me' to true
         flash[:notice] = "Welcome!"
-        if session[:flickr_invite_photo_id].nil?
+        if session[:flickr_invite_params].nil?
           flash[:allow_edit_after_auth] = true
           redirect_to edit_after_auth_url and return
         end
