35a7fb8e04a3aa4e087415da9fb5f4d02e06397a
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2011-10-05 12:49:27 -0700

    Fixed a bunch of minor bugs.

--- app/helpers/observations_helper.rb
+++ app/helpers/observations_helper.rb
@@ -36,7 +36,9 @@ module ObservationsHelper
     google_search_link = link_to("Google", "http://maps.google.com/?q=#{observation.place_guess}", :target => "_blank")
     google_coords_link = link_to("Google", "http://maps.google.com/?q=#{display_lat},#{display_lon}&z=#{observation.map_scale}", :target => "_blank")
     osm_search_link = link_to("OSM", "http://nominatim.openstreetmap.org/search?q=#{observation.place_guess}", :target => "_blank")
-    osm_coords_link = link_to("OSM", "http://www.openstreetmap.org/?mlat=#{display_lat}&mlon=#{display_lon}&zoom=#{observation.map_scale}", :target => "_blank")
+    osm_coords_url = "http://www.openstreetmap.org/?mlat=#{display_lat}&mlon=#{display_lon}"
+    osm_coords_url += "&zoom=#{observation.map_scale}" unless observation.map_scale.blank?
+    osm_coords_link = link_to("OSM", osm_coords_url, :target => "_blank")
     
     if coordinate_truncation = options[:truncate_coordinates]
       coordinate_truncation = 6 unless coordinate_truncation.is_a?(Fixnum)
