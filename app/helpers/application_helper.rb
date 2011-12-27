f37e5f6d63521ee9681444de71ed102785499d3f
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2011-12-27 12:48:45 -0500

    Made sure flickr photos show small_url if medium unavailable.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -215,11 +215,13 @@ module ApplicationHelper
   def modal_image(flickr_photo, params = {})
     size = params.delete(:size)
     size_method = size ? "#{size}_url" : 'square_url'
+    img_url = flickr_photo.send(size_method)
+    img_url ||= flickr_photo.send("small_url") if size_method != "square_url"
     link_options = params.merge(:rel => photo_path(flickr_photo, :partial => 'photo'))
     link_options[:class] ||= ''
     link_options[:class] += ' modal_image_link'
     link_to(
-      image_tag(flickr_photo.send(size_method),
+      image_tag(img_url,
         :title => flickr_photo.attribution,
         :id => "flickr_photo_#{flickr_photo.id}",
         :class => 'image') + 
