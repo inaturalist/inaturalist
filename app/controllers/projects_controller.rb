fbb75884a4b98ce944ce437d37d52395d77818ba
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2012-02-23 23:03:32 -0800

    Showing 10 top contributors on project iframe.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -40,7 +40,7 @@ class ProjectsController < ApplicationController
     respond_to do |format|
       format.html do
         @observed_taxa_count = @project.observed_taxa_count
-        @top_observers = @project.project_users.all(:order => "taxa_count desc, observations_count desc", :limit => 3, :conditions => "taxa_count > 0")
+        @top_observers = @project.project_users.all(:order => "taxa_count desc, observations_count desc", :limit => 10, :conditions => "taxa_count > 0")
         @project_users = @project.project_users.paginate(:page => 1, :per_page => 5, :include => :user, :order => "id DESC")
         @project_observations = @project.project_observations.paginate(:page => 1, 
           :include => {
