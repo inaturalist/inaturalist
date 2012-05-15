4fa52c0c81a7c5d6a2fa7add556c57c61adb69f0
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2012-05-15 12:23:03 -0700

    IE fixes for dashboard and updates.

--- public/javascripts/calendars.js
+++ public/javascripts/calendars.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  if ($.browser.msie && parseInt($.browser.version) < 9) {
+    return
+  }
   window.colorscale = d3.scale.linear()
     .domain([0,50])
     .range(["#fefefe", "#aaa"])
