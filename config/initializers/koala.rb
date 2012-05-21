68f485e0c5a7ffc1f66559e668dc0739bdd2001b
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2012-05-21 13:42:01 -0700

    Bugfix: old versions of Koala apparently have a different way of specifying the CA cert path.

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,3 +1,7 @@
-if File.exists?("/etc/ssl/certs") && Koala.http_service.respond_to?("http_options=")
-  Koala.http_service.http_options = { :ssl => { :ca_path => "/etc/ssl/certs" } }
+if File.exists?("/etc/ssl/certs")
+  if Koala.http_service.respond_to?("http_options=")
+    Koala.http_service.http_options = { :ssl => { :ca_path => "/etc/ssl/certs" } }
+  elsif Koala.http_service.respond_to?("ca_path=")
+    Koala.http_service.ca_path = "/etc/ssl/certs"
+  end
 end
