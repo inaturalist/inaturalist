8ee11c963f7e1692955ebdee051b90cab7fe139b
Scott Loarie <loarie@gmail.com>
2012-05-30 21:52:10 -0700

    made some improvements to EOL photos including multiple photos

--- app/controllers/eol_controller.rb
+++ app/controllers/eol_controller.rb
@@ -5,12 +5,21 @@ class EolController < ApplicationController
   # Return an HTML fragment containing checkbox inputs for EOL photos.
   # Params:
   #   taxon_id:        a taxon_id
+  #   q:        a search param
   def photo_fields
     @photos = []
     taxon_id = params[:taxon_id]
     @taxon = Taxon.find_by_id(taxon_id)
+    taxon_name = @taxon.name
+    alt_taxon_name = params[:q]
+    unless alt_taxon_name == ""
+      taxon_name = alt_taxon_name
+    end
+    
     begin
-       @photos << EolPhoto.new_from_taxon_name(@taxon.name)
+      @photos = EolPhoto.api_response_from_taxon_name(taxon_name).map do |ep|
+        EolPhoto.new_from_api_response(ep)
+      end
     rescue
       @photos = []
     end
