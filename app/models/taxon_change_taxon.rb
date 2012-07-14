54c2ededb68ee0e44df75abab0fdb7ae3ab1f1ce
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2012-07-13 17:46:52 -0700

    Changed validation to make builds easier.

--- app/models/taxon_change_taxon.rb
+++ app/models/taxon_change_taxon.rb
@@ -2,5 +2,5 @@ class TaxonChangeTaxon < ActiveRecord::Base
   belongs_to :taxon_change
   belongs_to :taxon
 
-  validates_presence_of :taxon_id, :taxon_change_id
+  validates_presence_of :taxon, :taxon_change
 end
