8384e1328b85e717e444cd7024cfc5d150284f28
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2012-08-08 17:53:30 -0700

    Replaced acts_as_tree with has_ancestry for places, fixed some views.

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,5 @@
 class Place < ActiveRecord::Base
-  acts_as_tree
+  has_ancestry
   belongs_to :user
   belongs_to :check_list, :dependent => :destroy
   has_many :check_lists, :dependent => :destroy
