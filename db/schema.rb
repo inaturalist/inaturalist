d2a7e062368ed681c8e22aaed2879556c9d35a75
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2012-08-08 17:53:30 -0700

    Replaced acts_as_tree with has_ancestry for places, fixed some views.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120801204921) do
+ActiveRecord::Schema.define(:version => 20120808224842) do
 
   create_table "announcements", :force => true do |t|
     t.string   "placement"
