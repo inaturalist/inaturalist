e6fa33c6afe9d7e5828b1c57936c482956eb37e0
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2012-10-03 12:56:49 -0700

    Fixed several bugs with mobile signup flow.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,17 +6,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
         sign_in(resource_name, resource)
-        respond_with resource, :location => after_sign_up_path_for(resource)
+        redirect_to home_path
+        return
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
         expire_session_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+        redirect_to root_url
+        return
       end
     else
       clean_up_passwords resource
       respond_with(resource) do |format|
         format.html { render :new }
-        format.mobile { render :new }
+        format.mobile { render :new, :status => 422 }
       end
     end
   end
