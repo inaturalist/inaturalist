7ec29ae5b23b6c62ebabc8535f92cac0ff7b09f3
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2012-10-17 23:19:13 -0700

    Most of the UI working for batch adding OFVs, still a bug in submission.

--- public/javascripts/observations/edit_batch.js
+++ public/javascripts/observations/edit_batch.js
@@ -13,3 +13,36 @@ function batchTaxon() {
     $.fn.simpleTaxonSelector.selectTaxon(this, taxon)
   })
 }
+
+function batchObservationFields() {
+  var fields = $('#batchform .observation_field')
+  $('#batchcol .observation_fields').each(function() {
+    var container = this,
+        currentFields = $('.observation_field', this)
+    fields.each(function() {
+      var newField = $(this).clone()
+      var fieldId = $(newField).data('observation-field-id')
+      var existing = $('[data-observation-field-id='+fieldId+']', container).get(0)
+      if (!existing) {
+        $(container).append(newField)
+      }
+      var val = $('.ofv_value_field', this).val()
+      $('.ofv_value_field', existing).val(val)
+      $('select', existing).val(val)
+    })
+
+    var index = $(this).parents('.observation:first').attr('id').split('-')[1]
+    $(':input', this).each(function() {
+      if ($(this).attr('id')) {
+        $(this).attr('id', $(this).attr('id').replace(/\d+_value/, index+'_value'))
+      }
+      if ($(this).attr('name')) {
+        $(this).attr('name', $(this).attr('name').replace(/\[\d+?\]/, '['+index+']'))
+      }
+    })
+  })
+}
+
+$(document).ready(function() {
+  $('.observation_fields_form_fields').observationFieldsForm()
+})
