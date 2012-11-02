5f8271577325c6a3580c4b892ffa5c999a08daaf
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2012-11-02 12:19:40 -0700

    Observation deletion should destroy idents, not delete_all.

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -99,7 +99,7 @@ class Observation < ActiveRecord::Base
            :as => :contribution,
            :dependent => :destroy
   has_many :comments, :as => :parent, :dependent => :destroy
-  has_many :identifications, :dependent => :delete_all
+  has_many :identifications, :dependent => :destroy
   has_many :project_observations, :dependent => :destroy
   has_many :project_invitations, :dependent => :destroy
   has_many :projects, :through => :project_observations
