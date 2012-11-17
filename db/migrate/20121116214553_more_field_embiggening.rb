211ed3f5e1d21c230345338ac79a82924cb9b3e3
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2012-11-16 19:13:12 -0800

    Fixed encoding issue with list CSV upload.

--- db/migrate/20121116214553_more_field_embiggening.rb
+++ db/migrate/20121116214553_more_field_embiggening.rb
@@ -4,6 +4,6 @@ class MoreFieldEmbiggening < ActiveRecord::Migration
   end
 
   def down
-    change_column :observation_fields, :allowed_values, :string, :limit => 256
+    change_column :sources, :citation, :string, :limit => 256
   end
 end
