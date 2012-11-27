c1ca60d700e4fbf71a43a2afe59c7733931ef548
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2012-11-27 01:52:58 -0800

    Added i18n fallbacks.

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,8 +1,10 @@
 # tell the I18n library where to find your translations
 # I18n.load_path += Dir[Rails.root.join('i18n', '*.{rb,yml}')]
 I18n.load_path += Dir[Rails.root.join('i18n', '**', '*.{rb,yml}')]
-
  
 # set default locale to something other than :en
 I18n.default_locale = INAT_CONFIG['default_locale'].to_sym if INAT_CONFIG['default_locale']
 
+# set up fallbacks
+require "i18n/backend/fallbacks"
+I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
