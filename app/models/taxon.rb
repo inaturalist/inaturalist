0599915b67865deda9c76eb3de054f15133b61cb
Scott Loarie <loarie@gmail.com>
2012-11-28 22:50:25 +0000

    allowed curators to add sources to manually created taxa

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -38,6 +38,7 @@ class Taxon < ActiveRecord::Base
   has_and_belongs_to_many :colors
   
   accepts_nested_attributes_for :conservation_status_source
+  accepts_nested_attributes_for :source
   
   define_index do
     indexes :name
