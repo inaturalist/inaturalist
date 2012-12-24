c83b12746839a8fa55c639d087d7c812cb87cfbc
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2012-12-23 20:56:30 -0500

    Bugfix: people/index species counts were off.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -149,7 +149,7 @@ class UsersController < ApplicationController
       @updates = hash.values.sort_by(&:created_at).reverse[0..11]
     end
 
-    @leaderboard_key = "leaderboard_#{I18n.locale}_#{SITE_NAME}"
+    @leaderboard_key = "leaderboard_#{I18n.locale}_#{SITE_NAME}_1"
     unless fragment_exist?(@leaderboard_key)
       @most_observations = most_observations(:per => 'month')
       @most_species = most_species(:per => 'month')
@@ -458,13 +458,12 @@ protected
         count(*) AS count_all
       FROM
         (
-          SELECT
-            DISTINCT ON (o.taxon_id) o.user_id
+          SELECT DISTINCT o.taxon_id, o.user_id
           FROM
             observations o
               JOIN taxa t ON o.taxon_id = t.id
-            WHERE
-              t.rank_level <= 10 AND
+          WHERE
+            t.rank_level <= 10 AND
               #{date_clause}
         ) as o
       GROUP BY o.user_id
