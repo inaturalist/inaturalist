2b4b3380085c51ba06893ddf4f66c23fcc49173b
Jonathan Yankovich <jonathan.yankovich@gmail.com>
2013-01-17 16:06:56 -0800

    fixed typo in 'add index' migration for Assessments, Assessment Sections

--- db/migrate/20130116165914_add_indexes_to_assessments.rb
+++ db/migrate/20130116165914_add_indexes_to_assessments.rb
@@ -4,6 +4,7 @@ class AddIndexesToAssessments < ActiveRecord::Migration
     add_index :assessments, :taxon_id
     add_index :assessments, :user_id
 
-    add_index :assessment_sections, :assessment_id
     add_index :assessment_sections, :user_id
+    add_index :assessment_sections, :assessment_id
+  end
 end
