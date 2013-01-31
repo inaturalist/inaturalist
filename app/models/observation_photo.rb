8668d41bcf401b9da4466e955938bee6b2f60478
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2013-01-30 21:34:18 -0800

    Added photos_count to observations, plus a couple more optimizations.

--- app/models/observation_photo.rb
+++ app/models/observation_photo.rb
@@ -3,8 +3,9 @@ class ObservationPhoto < ActiveRecord::Base
   belongs_to :photo
   
   after_create :set_observation_quality_grade,
-               :set_user_on_photo
-  after_destroy :destroy_orphan_photo, :set_observation_quality_grade
+               :set_user_on_photo,
+               :set_observation_photos_count
+  after_destroy :destroy_orphan_photo, :set_observation_quality_grade, :set_observation_photos_count
   
   def destroy_orphan_photo
     Photo.delay.destroy_orphans(photo_id)
@@ -21,6 +22,11 @@ class ObservationPhoto < ActiveRecord::Base
   def set_user_on_photo
     return true unless observation && photo
     Photo.update_all(["user_id = ?", observation.user_id], ["id = ?", photo.id])
+    true
+  end
+
+  def set_observation_photos_count
+    Observation.update_all(["photos_count = ?", observation.observation_photos(:reload => true).count], ["id = ?", observation])
     true
   end
   
