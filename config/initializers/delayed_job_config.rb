402a728c1b4cda5be7dcb894f7fe8a99ea330626
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2013-02-05 23:43:11 -0800

    Disabled custom DJ logger, seems to be wreaking havoc.

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,24 +1,26 @@
-# monkey patch BufferedLogger to allow a custom formatter
-class ActiveSupport::BufferedLogger
-  def formatter=(formatter)
-    @log.formatter = formatter
-  end
-end
+# # # monkey patch BufferedLogger to allow a custom formatter
+# # class ActiveSupport::BufferedLogger
+# #   def formatter=(formatter)
+# #     @log.formatter = formatter
+# #   end
+# # end
 
-# ensure DJ is logging to the right file
-Delayed::Worker.logger = ActiveSupport::BufferedLogger.new("log/delayed_job.log", Rails.logger.level)
-if caller.last =~ /delayed_job/
-  # create a custom formatter than includes the pid
-  # from http://cbpowell.wordpress.com/2012/04/05/beautiful-logging-for-ruby-on-rails-3-2/
-  class DJFormatter
-    def call(severity, time, progname, msg)
-      formatted_severity = sprintf("%-5s","#{severity}")
-      "[#{formatted_severity} pid:#{$$}] #{msg.strip}\n"
-    end
-  end
-  Delayed::Worker.logger.formatter = DJFormatter.new
+# # ensure DJ is logging to the right file
+# # Delayed::Worker.logger = ActiveSupport::BufferedLogger.new("log/delayed_job.log", Rails.logger.level)
+# Delayed::Worker.logger ||= Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
+# if caller.last =~ /delayed_job/
+#   # # create a custom formatter than includes the pid
+#   # # from http://cbpowell.wordpress.com/2012/04/05/beautiful-logging-for-ruby-on-rails-3-2/
+#   # class DJFormatter
+#   #   def call(severity, time, progname, msg)
+#   #     formatted_severity = sprintf("%-5s","#{severity}")
+#   #     "[#{formatted_severity} pid:#{$$}] #{msg.strip}\n"
+#   #   end
+#   # end
+#   # Delayed::Worker.logger.formatter = DJFormatter.new
 
-  # log AR calls to the log file
-  ActiveRecord::Base.logger = Delayed::Worker.logger
-  Rails.logger = Delayed::Worker.logger
-end
+#   Rails.logger.debug "[DEBUG] Delayed::Worker.logger: #{Delayed::Worker.logger}"
+#   # log AR calls to the log file
+#   ActiveRecord::Base.logger = Delayed::Worker.logger
+#   Rails.logger = Delayed::Worker.logger
+# end
