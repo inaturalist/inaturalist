902eb8556171f1ebdeeda7b9a069379eae3f154e
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2013-02-18 17:01:26 -0800

    Bust the header cache.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
   
   PER_PAGES = [10,30,50,100,200]
-  HEADER_VERSION = 9
+  HEADER_VERSION = 10
   
   alias :logged_in? :user_signed_in?
   
