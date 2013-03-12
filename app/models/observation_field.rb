ead1ef2c9b0726d9badc908dbd7a2ae1266a62b1
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2013-03-12 11:55:16 -0700

    Added some validations for observation fields.

--- app/models/observation_field.rb
+++ app/models/observation_field.rb
@@ -11,6 +11,8 @@ class ObservationField < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_presence_of :name
   validates_length_of :allowed_values, :maximum => 512, :allow_blank => true
+  validates_length_of :name, :maximum => 255, :allow_blank => true
+  validates_length_of :description, :maximum => 255, :allow_blank => true
   
   before_validation :strip_tags
   before_validation :strip_name
