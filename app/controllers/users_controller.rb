5c33b7365c5a1a72cea7d6c7e3c237aa68789cd2
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2013-05-23 10:07:36 -0700

    Bugfix: curators couldn't suspend users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,9 @@ class UsersController < ApplicationController
     :except => [:index, :show, :new, :create, :activate, :relationships]
   before_filter :find_user, :only => [:suspend, :unsuspend, :destroy, :purge, 
     :show, :update, :relationships, :add_role, :remove_role]
-  before_filter :ensure_user_is_current_user_or_admin, :only => [:update, :destroy, :suspend, :unsuspend]
+  before_filter :ensure_user_is_current_user_or_admin, :only => [:update, :destroy]
   before_filter :admin_required, :only => [:curation]
+  before_filter :curator_required, :only => [:suspend, :unsuspend]
   before_filter :return_here, :only => [:index, :show, :relationships, :dashboard, :curation]
   
   MOBILIZED = [:show, :dashboard, :new, :create]
