92d6b9f2a50371151e81a28ab6e040bc3e533ee4
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2013-05-31 10:14:36 -0700

    Mobile needs 404 too.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -175,7 +175,7 @@ class ApplicationController < ActionController::Base
   #
   def render_404
     respond_to do |format|
-      format.html { render(:file => "#{Rails.root}/public/404.html", :status => 404, :layout => false) }
+      format.any(:html, :mobile) { render(:file => "#{Rails.root}/public/404.html", :status => 404, :layout => false) }
       format.json { render :json => {:error => "Not found"}, :status => 404 }
     end
   end
