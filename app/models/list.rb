42a76a9d70fc422cd8acbde88e63593b6de061aa
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2013-08-13 23:52:05 -0700

    List icon preview cache needed to be locale specific.

--- app/models/list.rb
+++ app/models/list.rb
@@ -226,7 +226,7 @@ class List < ActiveRecord::Base
   
   def self.icon_preview_cache_key(list)
     list_id = list.is_a?(List) ? list.id : list
-    FakeView.url_for(:controller => "lists", :action => "icon_preview", :list_id => list_id)
+    FakeView.url_for(:controller => "lists", :action => "icon_preview", :list_id => list_id, :locale => I18n.locale)
   end
   
   def self.refresh_for_user(user, options = {})
