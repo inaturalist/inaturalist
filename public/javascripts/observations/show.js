4cfccdd87127a0c12106e7aac4e111f72bdc3457
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2014-06-19 19:01:34 -0700

    Another minor favicon fix.

--- public/javascripts/observations/show.js
+++ public/javascripts/observations/show.js
@@ -103,7 +103,7 @@ $(document).ready(function() {
   
   $('a[rel=alternate]').each(function() {
     $(this).css({
-      background: "url(http://www.google.com/s2/u/0/favicons?domain=" + this.hostname + ") left center no-repeat",
+      background: "url(http://www.google.com/s2/favicons?domain=" + this.hostname + ") left center no-repeat",
       'background-size': '16px 16px',
       "padding": "1px 0 1px 20px"
     })
