7e56a5c5b4bbf28bc67741ab1f4e0c453fdecee4
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2014-07-21 20:13:35 -0700

    Removed specs for ofv datatype validation.

--- spec/models/observation_field_value_spec.rb
+++ spec/models/observation_field_value_spec.rb
@@ -85,71 +85,71 @@ describe ObservationFieldValue, "destruction" do
 end
 
 describe ObservationFieldValue, "validation" do
-  it "should work for numeric fields" do
-    of = ObservationField.make!(:datatype => "numeric")
-    lambda {
-      ObservationFieldValue.make!(:observation_field => of, :value => "fop")
-    }.should raise_error(ActiveRecord::RecordInvalid)
-    lambda {
-      ObservationFieldValue.make!(:observation_field => of, :value => "12")
-    }.should_not raise_error(ActiveRecord::RecordInvalid)
-    lambda {
-      ObservationFieldValue.make!(:observation_field => of, :value => "12.3")
-    }.should_not raise_error(ActiveRecord::RecordInvalid)
-  end
+  # it "should work for numeric fields" do
+  #   of = ObservationField.make!(:datatype => "numeric")
+  #   lambda {
+  #     ObservationFieldValue.make!(:observation_field => of, :value => "fop")
+  #   }.should raise_error(ActiveRecord::RecordInvalid)
+  #   lambda {
+  #     ObservationFieldValue.make!(:observation_field => of, :value => "12")
+  #   }.should_not raise_error(ActiveRecord::RecordInvalid)
+  #   lambda {
+  #     ObservationFieldValue.make!(:observation_field => of, :value => "12.3")
+  #   }.should_not raise_error(ActiveRecord::RecordInvalid)
+  # end
 
-  it "should pass for a numeric value of 0" do
-    of = ObservationField.make!(:datatype => "numeric")
-    lambda {
-      ObservationFieldValue.make!(:observation_field => of, :value => "0")
-    }.should_not raise_error(ActiveRecord::RecordInvalid)
-  end
+  # it "should pass for a numeric value of 0" do
+  #   of = ObservationField.make!(:datatype => "numeric")
+  #   lambda {
+  #     ObservationFieldValue.make!(:observation_field => of, :value => "0")
+  #   }.should_not raise_error(ActiveRecord::RecordInvalid)
+  # end
   
-  it "should work for location" do
-    of = ObservationField.make!(:datatype => "location")
-    lambda {
-      ObservationFieldValue.make!(:observation_field => of, :value => "what")
-    }.should raise_error(ActiveRecord::RecordInvalid)
-    lambda {
-      ObservationFieldValue.make!(:observation_field => of, :value => "35 45")
-    }.should raise_error(ActiveRecord::RecordInvalid)
-    lambda {
-      ObservationFieldValue.make!(:observation_field => of, :value => "35,45")
-    }.should_not raise_error(ActiveRecord::RecordInvalid)
-  end
+  # it "should work for location" do
+  #   of = ObservationField.make!(:datatype => "location")
+  #   lambda {
+  #     ObservationFieldValue.make!(:observation_field => of, :value => "what")
+  #   }.should raise_error(ActiveRecord::RecordInvalid)
+  #   lambda {
+  #     ObservationFieldValue.make!(:observation_field => of, :value => "35 45")
+  #   }.should raise_error(ActiveRecord::RecordInvalid)
+  #   lambda {
+  #     ObservationFieldValue.make!(:observation_field => of, :value => "35,45")
+  #   }.should_not raise_error(ActiveRecord::RecordInvalid)
+  # end
   
-  it "should work for date" do
-    of = ObservationField.make!(:datatype => "date")
-    lambda {
-      ObservationFieldValue.make!(:observation_field => of, :value => "what")
-    }.should raise_error(ActiveRecord::RecordInvalid)
-    lambda {
-      ObservationFieldValue.make!(:observation_field => of, :value => "2011-12-32")
-    }.should_not raise_error(ActiveRecord::RecordInvalid)
-  end
+  # it "should work for date" do
+  #   of = ObservationField.make!(:datatype => "date")
+  #   lambda {
+  #     ObservationFieldValue.make!(:observation_field => of, :value => "what")
+  #   }.should raise_error(ActiveRecord::RecordInvalid)
+  #   lambda {
+  #     ObservationFieldValue.make!(:observation_field => of, :value => "2011-12-32")
+  #   }.should_not raise_error(ActiveRecord::RecordInvalid)
+  # end
   
-  it "should work for datetime" do
-    of = ObservationField.make!(:datatype => "datetime")
-    lambda {
-      ObservationFieldValue.make!(:observation_field => of, :value => "what")
-    }.should raise_error(ActiveRecord::RecordInvalid)
-    lambda {
-      ObservationFieldValue.make!(:observation_field => of, :value => Time.now.iso8601)
-    }.should_not raise_error(ActiveRecord::RecordInvalid)
-  end
+  # it "should work for datetime" do
+  #   of = ObservationField.make!(:datatype => "datetime")
+  #   lambda {
+  #     ObservationFieldValue.make!(:observation_field => of, :value => "what")
+  #   }.should raise_error(ActiveRecord::RecordInvalid)
+  #   lambda {
+  #     ObservationFieldValue.make!(:observation_field => of, :value => Time.now.iso8601)
+  #   }.should_not raise_error(ActiveRecord::RecordInvalid)
+  # end
 
-  it "should work for time" do
-    of = ObservationField.make!(:datatype => "time")
-    lambda {
-      ObservationFieldValue.make!(:observation_field => of, :value => "what")
-    }.should raise_error(ActiveRecord::RecordInvalid)
-    lambda {
-      ObservationFieldValue.make!(:observation_field => of, :value => "44:23")
-    }.should raise_error(ActiveRecord::RecordInvalid)
-    lambda {
-      ObservationFieldValue.make!(:observation_field => of, :value => "04:23")
-    }.should_not raise_error(ActiveRecord::RecordInvalid)
-  end
+  # it "should work for time" do
+  #   of = ObservationField.make!(:datatype => "time")
+  #   lambda {
+  #     ObservationFieldValue.make!(:observation_field => of, :value => "what")
+  #   }.should raise_error(ActiveRecord::RecordInvalid)
+  #   lambda {
+  #     ObservationFieldValue.make!(:observation_field => of, :value => "44:23")
+  #   }.should raise_error(ActiveRecord::RecordInvalid)
+  #   lambda {
+  #     ObservationFieldValue.make!(:observation_field => of, :value => "04:23")
+  #   }.should_not raise_error(ActiveRecord::RecordInvalid)
+  # end
 
   it "should pass for allowed values" do
     of = ObservationField.make!(:datatype => "text", :allowed_values => "foo|bar")
@@ -160,6 +160,7 @@ describe ObservationFieldValue, "validation" do
       ObservationFieldValue.make!(:observation_field => of, :value => "bar")
     }.should_not raise_error(ActiveRecord::RecordInvalid)
   end
+  
   it "should fail for disallowed values" do
     of = ObservationField.make!(:datatype => "text", :allowed_values => "foo|bar")
     lambda {
