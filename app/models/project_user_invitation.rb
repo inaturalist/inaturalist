cca1f6bda551bf0b6942a511ee71c954d65532fa
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2014-08-12 17:11:30 -0700

    Minor fixes for invite-only projects.

--- app/models/project_user_invitation.rb
+++ app/models/project_user_invitation.rb
@@ -4,7 +4,8 @@ class ProjectUserInvitation < ActiveRecord::Base
   belongs_to :invited_user, :class_name => "User", :inverse_of => :project_user_invitations_received
   belongs_to :project, :inverse_of => :project_user_invitations
   validates_presence_of :user_id, :invited_user_id, :project_id
-  validates_uniqueness_of :invited_user_id, :scope => :project_id
+  validates_uniqueness_of :invited_user_id, :scope => :project_id, :message => "has already been invited"
+  validate :user_is_not_already_a_member
   after_create :email_user, :create_update_for_user
   after_destroy :destroy_updates
 
@@ -31,5 +32,13 @@ class ProjectUserInvitation < ActiveRecord::Base
 
   def pending?
     !accepted?
+  end
+
+  def user_is_not_already_a_member
+    return true if project_id.blank?|| invited_user_id.blank?
+    if ProjectUser.where(:project_id => project_id, :user_id => invited_user_id).exists?
+      errors.add(:invited_user_id, "is already a member of that project")
+    end
+    true
   end
 end
--- spec/models/project_user_invitation_spec.rb
+++ spec/models/project_user_invitation_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe ProjectUserInvitation do
+describe ProjectUserInvitation, "creation" do
   it "should email the user" do
     lambda {
       ProjectUserInvitation.make!
@@ -11,5 +11,11 @@ describe ProjectUserInvitation do
     lambda {
       ProjectUserInvitation.make!
     }.should change(Update, :count).by(1)
+  end
+
+  it "should not be possible for members of the project" do
+    pu = ProjectUser.make!
+    pui = ProjectUserInvitation.make(:project => pu.project, :invited_user => pu.user)
+    pui.should_not be_valid
   end
 end
