f58b85cd553e3a7bf1ec604d805e068ebc646989
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2014-10-01 15:49:12 -0700

    Added custom mimetype for DwC. GET /observations/:id.dwc was messing things up.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -5,9 +5,9 @@
 # Mime::Type.register_alias "text/html", :iphone
 
 Mime::Type.register "application/vnd.google-earth.kml+xml", :kml
-Mime::Type.register "text/xml", :dwc
+Mime::Type.register "text/dwc+xml", :dwc
 Mime::Type.register "text/json", :node
-Mime::Type.register "text/json", :geojson
+Mime::Type.register "text/geojson+json", :geojson
 Mime::Type.register_alias "text/javascript", :widget, %w( application/javascript application/x-javascript )
 Mime::Type.register_alias "text/html", :mobile
 Mime::Type.register "application/pdf", :pdf
