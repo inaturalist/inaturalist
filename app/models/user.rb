5cdd9e595ce5ddfe45613094c1134d1b9949300d
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2014-10-26 19:05:43 -0700

    Relaxed restrictions on valid emails.

--- app/models/user.rb
+++ app/models/user.rb
@@ -139,7 +139,7 @@ class User < ActiveRecord::Base
   bad_login_message = "use only letters, numbers, and -_ please.".freeze
   email_name_regex  = '[\w\.%\+\-]+'.freeze
   domain_head_regex = '(?:[A-Z0-9\-]+\.)+'.freeze
-  domain_tld_regex  = '(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|jobs|museum)'.freeze
+  domain_tld_regex  = '(?:[A-Z]+)'.freeze
   email_regex       = /\A#{email_name_regex}@#{domain_head_regex}#{domain_tld_regex}\z/i
   bad_email_message = "should look like an email address.".freeze
   
