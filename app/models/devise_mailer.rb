e251bbafcafd44fcb8ecf5665839e3f060b88e7a
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2014-10-30 23:36:01 +1300

    Made emails more site-specific.
    All emails should now come from the site the recipient belongs to,
    defaulting to the server settings where the mail was sent from
    (usually a bg processor).

--- app/models/devise_mailer.rb
+++ app/models/devise_mailer.rb
@@ -1,18 +1,25 @@
 class DeviseMailer < Devise::Mailer
-  def headers_for(action, opts)
-    opts[:subject] = t(:welcome_to_inat, :site_name => SITE_NAME) if action == :confirmation_instructions
-    super(action, opts)
-  end
-
   def devise_mail(record, action, opts={})
     user = if record.is_a?(User)
       record
     elsif record.respond_to?(:user)
       record.user
     end
+    opts = opts.merge(:subject => t(:welcome_to_inat, :site_name => SITE_NAME))
     if user
       old_locale = I18n.locale
       I18n.locale = user.locale.blank? ? I18n.default_locale : user.locale
+      if site = @site || user.site
+        opts = opts.merge(
+          :subject => t(:welcome_to_inat, :site_name => site.name),
+          :from => "#{site.name} <#{site.email_noreply.blank? ? CONFIG.noreply_email : site.email_noreply}>",
+          :reply_to => site.email_noreply.blank? ? CONFIG.noreply_email : site.email_noreply
+        )
+        DeviseMailer.default_url_options[:host] = site.url
+      else
+        # re-translate
+        opts = opts.merge(:subject => t(:welcome_to_inat, :site_name => SITE_NAME))
+      end
       r = super(record, action, opts)
       I18n.locale = old_locale
       r
