577bc98c4ccb293f04529d7078d1dc99df246556
Patrick Leary <patrick.r.leary@gmail.com>
2014-11-11 13:49:49 -0500

    Bumping the asset version to refresh all assets

--- config/application.rb
+++ config/application.rb
@@ -71,7 +71,7 @@ module Inaturalist
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
 
     # Compile localized CSS:
     config.assets.precompile += ['*.css', '*.js']
