289e8edd2b7e0012557e4095f38c04ea1b57a7c9
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2014-12-05 14:00:53 -0800

    Bugfix: reset password email had the wrong subject. Closes #398.

--- app/models/devise_mailer.rb
+++ app/models/devise_mailer.rb
@@ -5,24 +5,26 @@ class DeviseMailer < Devise::Mailer
     elsif record.respond_to?(:user)
       record.user
     end
-    opts = opts.merge(:subject => t(:welcome_to_inat, :site_name => SITE_NAME))
+    opts = opts.merge(:subject => t(:welcome_to_inat, :site_name => SITE_NAME)) if action.to_s == "confirmation_instructions"
     if user
       old_locale = I18n.locale
       I18n.locale = user.locale.blank? ? I18n.default_locale : user.locale
-      if site = @site || user.site
-        opts = opts.merge(
-          :subject => t(:welcome_to_inat, :site_name => site.name),
-          :from => "#{site.name} <#{site.email_noreply.blank? ? CONFIG.noreply_email : site.email_noreply}>",
-          :reply_to => site.email_noreply.blank? ? CONFIG.noreply_email : site.email_noreply
-        )
-        begin
-          DeviseMailer.default_url_options[:host] = URI.parse(site.url).host
-        rescue
-          # url didn't parse for some reason, leave it as the default
+      if action.to_s == "confirmation_instructions"
+        if site = @site || user.site
+          opts = opts.merge(
+            :subject => t(:welcome_to_inat, :site_name => site.name),
+            :from => "#{site.name} <#{site.email_noreply.blank? ? CONFIG.noreply_email : site.email_noreply}>",
+            :reply_to => site.email_noreply.blank? ? CONFIG.noreply_email : site.email_noreply
+          )
+          begin
+            DeviseMailer.default_url_options[:host] = URI.parse(site.url).host
+          rescue
+            # url didn't parse for some reason, leave it as the default
+          end
+        else
+          # re-translate
+          opts = opts.merge(:subject => t(:welcome_to_inat, :site_name => SITE_NAME))
         end
-      else
-        # re-translate
-        opts = opts.merge(:subject => t(:welcome_to_inat, :site_name => SITE_NAME))
       end
       r = super(record, action, opts)
       I18n.locale = old_locale
