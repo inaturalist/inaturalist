06a28ac4ca19fdf5d66b0e09862b12c060e28399
Patrick Leary <patrick.r.leary@gmail.com>
2015-02-26 15:16:45 -0500

    Fixed mobile view toggle for daskboard

--- app/models/user_sweeper.rb
+++ app/models/user_sweeper.rb
@@ -6,5 +6,6 @@ class UserSweeper < ActionController::Caching::Sweeper
     Site.limit(100).each do |s|
       ctrl.expire_fragment(User.header_cache_key_for(user, site: s))
     end
+    ctrl.send :expire_action, FakeView.home_url(user_id: user.id)
   end
 end
