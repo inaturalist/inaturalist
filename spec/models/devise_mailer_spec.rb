e1cfe1bf8be196d09ded6c19925b668adc455103
Patrick Leary <patrick.r.leary@gmail.com>
2015-03-03 12:17:26 -0500

    Fixed confirmation token for registration emails

--- spec/models/devise_mailer_spec.rb
+++ spec/models/devise_mailer_spec.rb
@@ -5,13 +5,13 @@ describe DeviseMailer, "confirmation_instructions" do
   it "should default to English if no locale" do
     u = User.make!
     mail = DeviseMailer.confirmation_instructions(u, u.confirmation_token)
-    mail.body.should =~ /Welcome/
+    expect( mail.body ).to include "Welcome"
   end
 
   it "should use the user's locale" do
     u = User.make!(:locale => "es-MX")
     mail = DeviseMailer.confirmation_instructions(u, u.confirmation_token)
-    mail.body.should =~ /Bienvenido/
+    expect( mail.body ).to include "Bienvenido"
   end
 end
 
@@ -19,13 +19,14 @@ describe DeviseMailer, "reset_password_instructions" do
   it "should have the right subject" do
     u = User.make!
     mail = DeviseMailer.reset_password_instructions(u, u.confirmation_token)
-    mail.subject.should_not =~ /Welcome/
-    mail.subject.should =~ /Reset/
+    expect( mail.subject ).to_not include "Welcome"
+    expect( mail.subject ).to include "Reset"
   end
 
   it "should use the user's locale" do
     u = User.make!(:locale => "es-MX")
     mail = DeviseMailer.reset_password_instructions(u, u.confirmation_token)
-    mail.subject.downcase.should_not =~ /reset/
+    expect( mail.subject.downcase ).to_not include "reset"
+    expect( mail.subject.downcase ).to include "reinicio"
   end
 end
