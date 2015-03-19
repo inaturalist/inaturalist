8e0c3aaa476202a34b913e20281923c9258c9b5d
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2015-03-19 14:52:55 -0700

    Bugfix: apparently it was impossible to leave a project.

--- config/routes.rb
+++ config/routes.rb
@@ -203,7 +203,7 @@ Rails.application.routes.draw do
   get 'observations/of/:id.:format' => 'observations#of', :as => :observations_of
   match 'observations/:id/quality/:metric' => 'quality_metrics#vote', :as => :observation_quality, :via => [:post, :delete]
   match 'projects/:id/join' => 'projects#join', :as => :join_project, :via => [ :get, :post ]
-  get 'projects/:id/leave' => 'projects#leave', :as => :leave_project
+  delete 'projects/:id/leave' => 'projects#leave', :as => :leave_project
   post 'projects/:id/add' => 'projects#add', :as => :add_project_observation
   match 'projects/:id/remove' => 'projects#remove', :as => :remove_project_observation, :via => [:post, :delete]
   post 'projects/:id/add_batch' => 'projects#add_batch', :as => :add_project_observation_batch
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -10,11 +10,29 @@ describe ProjectsController, "spam" do
 
   it "should render 403 when the owner is a spammer" do
     get :show, id: spammer_content.id
-    response.response_code.should == 403
+    expect(response.response_code).to eq 403
   end
 
   it "should render 403 when content is flagged as spam" do
     get :show, id: spammer_content.id
-    response.response_code.should == 403
+    expect(response.response_code).to eq 403
+  end
+end
+
+describe ProjectsController, "leave" do
+  let(:user) { User.make! }
+  let(:project) { Project.make! }
+  before do
+    sign_in user
+  end
+  it "should destroy the project user" do
+    pu = ProjectUser.make!(user: user, project: project)
+    delete :leave, id: project.id
+    expect( ProjectUser.find_by_id(pu.id) ).to be_blank
+  end
+  describe "routes" do
+    it "should accept DELETE requests" do
+      expect(delete: "/projects/#{project.slug}/leave").to be_routable
+    end
   end
 end
