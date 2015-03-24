b9a49ca5a9e597bf9348493cb6c90a4d6ec304e5
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2015-03-24 11:56:50 -0700

    Cosmetics and sanity check for ObservationPhotosController spec.

--- spec/controllers/observation_photos_controller_api_spec.rb
+++ spec/controllers/observation_photos_controller_api_spec.rb
@@ -4,35 +4,37 @@ shared_examples_for "an ObservationPhotosController" do
   describe "create" do
     let(:file) { fixture_file_upload('files/egg.jpg', 'image/jpeg') }
     it "should work" do
-      lambda {
+      expect {
         post :create, :format => :json, :observation_photo => {:observation_id => observation.id}, :file => file
-      }.should change(ObservationPhoto, :count).by(1)
-      response.should be_success
+      }.to change(ObservationPhoto, :count).by(1)
+      expect(response).to be_success
     end
 
     it "should not duplicate observation photos with the same uuid" do
       uuid = "some really long identifier"
       op = ObservationPhoto.make!(:uuid => uuid, :observation => observation)
       post :create, :format => :json, :observation_photo => {:observation_id => observation.id, :uuid => uuid}, :file => file
-      ObservationPhoto.where(:uuid => uuid).count.should eq 1
+      expect(ObservationPhoto.where(:uuid => uuid).count).to eq 1
+      observation.reload
+      expect(observation.photos.size).to eq 1
     end
   end
 
   it "should update" do
     p = LocalPhoto.make!(:user => user)
     op = ObservationPhoto.make!(:photo => p, :observation => observation)
-    op.position.should be_blank
+    expect(op.position).to be_blank
     put :update, :format => :json, :id => op.id, :observation_photo => {:position => 1}
-    response.should be_success
+    expect(response).to be_success
     op.reload
-    op.position.should eq(1)
+    expect(op.position).to eq(1)
   end
 
   it "should destroy" do
     p = LocalPhoto.make!(:user => user)
     op = ObservationPhoto.make!(:photo => p, :observation => observation)
     delete :destroy, :format => :json, :id => op.id
-    ObservationPhoto.find_by_id(op.id).should be_blank
+    expect(ObservationPhoto.find_by_id(op.id)).to be_blank
   end
 end
 
@@ -42,7 +44,7 @@ describe ObservationPhotosController, "oauth authentication" do
   let(:observation) { Observation.make!(:user => user)}
   before do
     request.env["HTTP_AUTHORIZATION"] = "Bearer xxx"
-    controller.stub(:doorkeeper_token) { token }
+    allow(controller).to receive(:doorkeeper_token) { token }
   end
   it_behaves_like "an ObservationPhotosController"
 end
