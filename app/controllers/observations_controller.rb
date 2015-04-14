34d2eba9f0e612d72770f2b31193ffbe2aca4a75
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2015-04-14 16:51:47 -0700

    Allow external sites to use observations/map and observations/user_stats. Fixes #527

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -2,7 +2,7 @@
 class ObservationsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :index, if: :json_request?
   protect_from_forgery unless: -> { request.format.widget? } #, except: [:stats, :user_stags, :taxa]
-  before_filter :allow_external_iframes, only: [:stats, :user_stags, :taxa]
+  before_filter :allow_external_iframes, only: [:stats, :user_stats, :taxa, :map]
 
   WIDGET_CACHE_EXPIRATION = 15.minutes
   caches_action :index, :by_login, :project,
