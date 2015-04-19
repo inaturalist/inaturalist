4baf002a1b98f9c0e64bb632313843a7cb422d67
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2015-04-19 12:05:56 -0700

    Made GLoBI script use their API, which apparently exists.

--- tools/globi_observation_links.rb
+++ tools/globi_observation_links.rb
@@ -8,17 +8,12 @@ Globi.
 
 Usage:
 
-  rails runner tools/globi_observation_links.rb URL
-
-URL should lead to a CSV file of the form
-  provider, consumer
-  INAT OBS URL, EOL URL
-
-E.g. the one at https://raw.githubusercontent.com/jhpoelen/backwarden/master/provider-consumer.csv
+  rails runner tools/globi_observation_links.rb
 
 where [options] are:
 EOS
-  opt :debug, "Print debug statements", :type => :boolean, :short => "-d"
+  opt :debug, "Print debug statements", type: :boolean, short: "-d"
+  opt :according_to, "Data provider name in GLoBI, defaults to inaturalist", type: :string, short: "-p", default: 'inaturalist'
 end
 
 start_time = Time.now
@@ -26,33 +21,39 @@ new_count = 0
 old_count = 0
 delete_count = 0
 href_name = "GloBI"
+according_to = opts.according_to
+limit = 1000
+skip = 0
 
-url = ARGV[0]
-Trollop::die "You must specify URL where we can retrieve the mappings" if url.blank?
-CSV.foreach(open(url), headers: true) do |row|
-  observation_id = row['provider'].to_s[/\/(\d+)$/, 1]
-  observation = Observation.find_by_id(observation_id)
-  if observation.blank?
-    puts "\tobservation #{observation_id} doesn't exist, skipping..."
-    next
+while true do
+  url = "http://api.globalbioticinteractions.org/interaction?accordingTo=#{according_to}&field=study_url&includeObservations=true&limit=#{limit}&skip=#{skip}"
+  puts
+  puts url
+  puts
+  data = JSON.parse(open(url).read)
+  records = data['data']
+  break if records.size == 0
+  records.each do |record|
+    observation_id = record[0].to_s[/\/(\d+)$/, 1]
+    observation = Observation.find_by_id(observation_id)
+    if observation.blank?
+      puts "\tobservation #{observation_id} doesn't exist, skipping..."
+      next
+    end
+    href = "http://www.globalbioticinteractions.org/#interactionType=interactsWith&accordingTo=#{FakeView.observation_url(observation_id)}"
+    existing = ObservationLink.where(observation_id: observation_id, href: href).first
+    if existing
+      existing.touch unless opts[:debug]
+      old_count += 1
+      puts "\tobservation link already exists, skipping"
+    else
+      ol = ObservationLink.new(:observation => observation, :href => href, :href_name => href_name, :rel => "alternate")
+      ol.save unless opts[:debug]
+      new_count += 1
+      puts "\tCreated #{ol}"
+    end
   end
-  eol_url = row['consumer']
-  if eol_url.blank?
-    puts "\tGloBI / EOL URL missing, skipping..."
-    next
-  end
-  href = "http://www.globalbioticinteractions.org/#interactionType=interactsWith&accordingTo=http%3A%2F%2Fwww.inaturalist.org%2Fobservations%2F#{observation_id}"
-  existing = ObservationLink.where(observation_id: observation_id, href: href).first
-  if existing
-    existing.touch unless opts[:debug]
-    old_count += 1
-    puts "\tobservation link already exists, skipping"
-  else
-    ol = ObservationLink.new(:observation => observation, :href => href, :href_name => href_name, :rel => "alternate")
-    ol.save unless opts[:debug]
-    new_count += 1
-    puts "\tCreated #{ol}"
-  end
+  skip += 1000
 end
 
 delete_count = ObservationLink.where(href_name: href_name).where("updated_at < ?", start_time).count
