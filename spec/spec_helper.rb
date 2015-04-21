72cf3cfd1c05ead67a40dfbd7982cfe43b43e8e4
Patrick Leary <patrick.r.leary@gmail.com>
2015-04-21 11:09:42 -0400

    Fixed bug with some life list names

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -69,6 +69,7 @@ RSpec.configure do |config|
   end
 
   config.include Devise::TestHelpers, :type => :controller
+  config.include Devise::TestHelpers, :type => :view
   config.fixture_path = "#{::Rails.root}/spec/fixtures/"
   config.infer_spec_type_from_file_location!
 end
