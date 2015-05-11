0bc247834a2b7c40487e96e098c8ac2e1050c54e
Patrick Leary <patrick.r.leary@gmail.com>
2015-05-11 10:29:13 -0400

    different way of disabling flickr cert check for now

--- app/models/flickr_photo.rb
+++ app/models/flickr_photo.rb
@@ -24,7 +24,6 @@ class FlickrPhoto < Photo
   
   def self.flickraw_for_user(user)
     return flickr unless (user && user.flickr_identity)
-    FlickRaw.check_certificate = false
     f = FlickRaw::Flickr.new
     f.access_token = user.flickr_identity.token
     f.access_secret = user.flickr_identity.secret
