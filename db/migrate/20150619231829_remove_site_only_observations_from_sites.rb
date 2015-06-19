a2593f2c53036969b3445625812689c2fa30bfe0
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2015-06-19 16:26:01 -0700

    Data migration for site preference switcheroo.

---
+++ db/migrate/20150619231829_remove_site_only_observations_from_sites.rb
@@ -0,0 +1,8 @@
+class RemoveSiteOnlyObservationsFromSites < ActiveRecord::Migration
+  def up
+    Preference.where(owner_type: 'Site', name: 'site_only_observations', value: 't').update_all("name = 'site_observations_filter', value = 'site'")
+  end
+  def down
+    Preference.where(owner_type: 'Site', name: 'site_observations_filter', value: 'site').update_all("name = 'site_only_observations', value = 't'")
+  end
+end
