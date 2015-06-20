baab5e7bfbcfa236295010cd76b3f9bec19ca293
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2015-06-19 21:50:16 -0700

    Yes, altering a migration after release. Sue me.

--- db/migrate/20150619231829_remove_site_only_observations_from_sites.rb
+++ db/migrate/20150619231829_remove_site_only_observations_from_sites.rb
@@ -1,6 +1,7 @@
 class RemoveSiteOnlyObservationsFromSites < ActiveRecord::Migration
   def up
     Preference.where(owner_type: 'Site', name: 'site_only_observations', value: 't').update_all("name = 'site_observations_filter', value = 'site'")
+    Preference.where(owner_type: 'Site', name: 'site_only_observations', value: 'f').delete_all
   end
   def down
     Preference.where(owner_type: 'Site', name: 'site_observations_filter', value: 'site').update_all("name = 'site_only_observations', value = 't'")
