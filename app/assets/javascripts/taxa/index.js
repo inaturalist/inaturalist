ecc726d3cc34febf26254ceca6302fc7028324a8
Patrick Leary <patrick.r.leary@gmail.com>
2015-08-06 18:33:22 -0400

    added search external name providers to taxon autocomplete

--- app/assets/javascripts/taxa/index.js
+++ app/assets/javascripts/taxa/index.js
@@ -3,6 +3,7 @@ $(function() {
     thumbnail: false,
     allow_placeholders: true,
     allow_enter_submit: true,
+    search_external: false,
     afterSelect: function( ui ) {
       window.location.href = inaturalist.TAXON_ROOT_URL + ui.item.id;
     }
--- app/assets/javascripts/taxa/show.js
+++ app/assets/javascripts/taxa/show.js
@@ -185,6 +185,7 @@ $(document).ready(function(){
     thumbnail: false,
     allow_placeholders: true,
     allow_enter_submit: true,
+    search_external: false,
     afterSelect: function( ui ) {
       window.location.href = inaturalist.TAXON_ROOT_URL + ui.item.id;
     }
