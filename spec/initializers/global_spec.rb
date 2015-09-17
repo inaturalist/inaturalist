fb1f5d660ef05b8e8128745aeb42e5369255fed5
Patrick Leary <patrick.r.leary@gmail.com>
2015-09-17 14:42:05 -0400

    fixed bug in String.context_of_pattern

--- spec/initializers/global_spec.rb
+++ spec/initializers/global_spec.rb
@@ -6,7 +6,7 @@ describe String do
     it "finds mentioned users that exist" do
       quick = User.make!(login: "quick")
       fox = User.make!(login: "fox")
-      expect("The @quick, @brown @fox @fox!".mentioned_users).to eq [ quick, fox ]
+      expect("The @quick, @brown @fox @fox!".mentioned_users).to include(quick, fox)
     end
   end
 
@@ -16,9 +16,9 @@ describe String do
     end
 
     it "returns a number of characters around a pattern" do
-      str = ("a" * 100) + "pattern" + ("b" * 100)
+      str = ("a" * 100) + " pattern " + ("b" * 100)
       expect(str.context_of_pattern("pattern", 15)).to eq(
-        "..." + ("a" * 15) + "pattern" + ("b" * 15) + "...")
+        "..." + ("a" * 14) + " pattern " + ("b" * 14) + "...")
     end
   end
 end
