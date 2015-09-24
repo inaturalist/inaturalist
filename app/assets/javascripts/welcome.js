87ab7fbce8c3a0d7ceb3ecf7f29562ab78202ddb
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2015-09-23 17:59:05 -0700

    Hero carousel with stats.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,7 +1,6 @@
 if (HOMEPAGE_DATA_URL) {
   console.log("[DEBUG] HOMEPAGE_DATA_URL: ", HOMEPAGE_DATA_URL)
   $.getJSON(HOMEPAGE_DATA_URL, function(json) {
-    console.log("[DEBUG] json: ", json)
     addObservations(json.observations)
     addTestimonials(json.testimonials)
   })
@@ -11,18 +10,38 @@ function addObservations(observations) {
   if (!observations) {
     return
   }
-  var o = observations[0]
-  $('#hero .heroimage').html(
-    $('<img/>').attr('src', o.image_url)
-  )
-  $("#herofooter [class*='col-']").html(
-    $('<a>').attr('href', '/people/'+o.user.login).html(
-      $('<img/>').attr('src', o.user.user_icon_url)
+  for (var i = 0; i < observations.length; i++) {
+    var o = observations[i]
+    var item = $('<div class="item">').append(
+      $('<div class="heroimage">').css('backgroundImage', 'url('+o.image_url+')').html(
+        '........... ........... ........... ........... ........... ........... ' +
+        '........... ........... ........... ........... ........... ........... ' +
+        '........... ........... ........... ........... ........... ........... ' +
+        '........... ........... ........... ........... ........... ........... ' +
+        '........... ........... ........... ........... ........... ........... ' +
+        '........... ........... ........... ........... ........... ........... '
+      ),
+      $('<div class="herofooter">').append(
+        $('<div class="row">').append(
+          $('<div class="col-xs-11 col-xs-offset-1">').append(
+            $('<a>').attr('href', '/people/'+o.user.login).html(
+              $('<img/>').attr('src', o.user.user_icon_url)
+            ),
+            $('<a class="obstext">').attr('href', '/observations/'+o.id).html(o.taxon.default_name.name),
+            $('<a class="username">').attr('href', '/observations/'+o.id).html(I18n.t('observed_by') + ' ' + o.user.name)
+          )
+        )
+      ),
+      $('#herobox').clone().attr('id', '').removeClass('fade')
     )
-  ).append(
-    $('<a class="obstext">').attr('href', '/observations/'+o.id).html(o.taxon.default_name.name),
-    $('<a class="username">').attr('href', '/observations/'+o.id).html(I18n.t('observed_by') + ' ' + o.user.name)
-  )
+    if (i == 0) {
+      item.addClass('active')
+      $('#hero .item').removeClass('active')
+      $('#hero .carousel-inner').prepend(item, ' ')
+    } else {
+      $('#hero .carousel-inner').append(' ', item)
+    }
+  }
 }
 
 function addTestimonials(testimonials) {
@@ -41,15 +60,18 @@ function addTestimonials(testimonials) {
           )
         ),
         $('<div class="col-xs-3">').append(
-          $('<a class="name">').attr('href', t.url).html(
+          $('<a>').attr('href', t.url).html(
             $('<img class="img-circle img-responsive">').attr('src', t.image_url)
           )
         )
       )
     )
+    var indicator = $('<li>').attr('data-target', '#who-carousel').attr('data-slide-to', i)
     if (i == 0) {
       item.addClass('active')
+      indicator.addClass('active')
     }
-    $('#who .carousel-inner').append(item)
+    $('#who .carousel-inner').append(item, ' ')
+    $('#who .carousel-indicators').append(indicator, ' ')
   }
 }
