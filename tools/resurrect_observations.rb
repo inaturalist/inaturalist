87501f8820c31e989105ee8b504915b103fda716
Patrick Leary <patrick.r.leary@gmail.com>
2015-10-06 12:10:21 -0400

    can resurrect observations by ID, gets LocalPhotos, some fixes

--- tools/resurrect_observations.rb
+++ tools/resurrect_observations.rb
@@ -6,19 +6,20 @@ opts = Trollop::options do
 Resurrect observations
 
 Usage:
-  rails runner tools/resurrect_observations.rb -u USER_ID
+  rails runner tools/resurrect_observations.rb [ -u USER_ID, -o OBSERVATION_ID ]
 
 Options:
 EOS
   opt :user, "User whose observations to resurret", :short => "-u", :type => :string
+  opt :observation, "Observations to resurret", :short => "-o", :type => :integer
   opt :observed_on, "Observed date to resurrect observations from, format: YYYY-MM-DD", :short => "-d", :type => :string
   opt :created_on, "Created date to resurrect observations from, format: YYYY-MM-DD", :short => "-c", :type => :string
 end
 
 OPTS = opts
 
-unless OPTS.user || OPTS.observed_on || OPTS.created_on
-  puts "You must specify a user or a date"
+unless OPTS.user || OPTS.observation || OPTS.observed_on || OPTS.created_on
+  puts "You must specify a user, an observation, or a date"
   exit(0)
 end
 
@@ -33,6 +34,15 @@ if OPTS.user
     @where << "observations.user_id = #{@user.id}"
   else
     puts "Couldn't find user matching '#{OPTS.user}'"
+    exit(0)
+  end
+end
+
+if OPTS.observation
+  if @observation = Observation.find_by_id(OPTS.observation)
+    @where << "observations.id = #{@observation.id}"
+  else
+    puts "Couldn't find observation matching '#{OPTS.observation}'"
     exit(0)
   end
 end
@@ -64,12 +74,12 @@ puts "\t#{cmd}"
 system cmd
 resurrection_cmds << "psql inaturalist_production -c \"\\copy observations FROM '#{fname}' WITH CSV\""
 
-# puts "Exporting from photos (except LocalPhotos, which are gone)..."
-# fname = "resurrect_#{session_id}-photos.csv"
-# cmd = "psql inaturalist_production -c \"COPY (SELECT * FROM photos WHERE user_id = #{user_id} AND type != 'LocalPhoto') TO STDOUT WITH CSV\" > #{fname}"
-# puts "\t#{cmd}"
-# system cmd
-# resurrection_cmds << "psql inaturalist_production -c \"\\copy photos FROM '#{fname}' WITH CSV\""
+puts "Exporting from photos..."
+fname = "resurrect_#{session_id}-photos.csv"
+cmd = "psql inaturalist_production -c \"COPY (SELECT p.* FROM photos p JOIN observation_photos op ON (p.id=op.photo_id) JOIN observations ON (op.observation_id=observations.id) WHERE #{@where.join(' AND ')}) TO STDOUT WITH CSV\" > #{fname}"
+puts "\t#{cmd}"
+system cmd
+resurrection_cmds << "psql inaturalist_production -c \"\\copy photos FROM '#{fname}' WITH CSV\""
 
 update_statements = []
 
@@ -96,15 +106,14 @@ has_many_reflections.each do |k, reflection|
   resurrection_cmds << "psql inaturalist_production -c \"\\copy #{reflection.table_name} FROM '#{fname}' WITH CSV\""
 end
 
-puts "Exporting from photos (except LocalPhotos, which are gone)..."
+puts "Exporting from photos..."
 fname = "resurrect_#{session_id}-photos.csv"
-w = @where + ["photos.type != 'LocalPhoto'"]
 sql = <<-SQL
   SELECT photos.* 
   FROM photos 
     JOIN observation_photos ON observation_photos.photo_id = photos.id
     JOIN observations ON observations.id = observation_photos.observation_id
-  WHERE #{w.join(' AND ')}
+  WHERE #{@where.join(' AND ')}
 SQL
 cmd = "psql inaturalist_production -c \"COPY (#{sql}) TO STDOUT WITH CSV\" > #{fname}"
 puts "\t#{cmd}"
@@ -113,18 +122,17 @@ resurrection_cmds << "psql inaturalist_production -c \"\\copy photos FROM '#{fna
 
 puts "Exporting from observation_photos..."
 fname = "resurrect_#{session_id}-observation_photos.csv"
-w = @where + ["photos.type != 'LocalPhoto'"]
 sql = <<-SQL
   SELECT observation_photos.* 
   FROM observation_photos 
     JOIN photos ON photos.id = observation_photos.photo_id
     JOIN observations ON observations.id = observation_photos.observation_id
-  WHERE #{w.join(' AND ')}
+  WHERE #{@where.join(' AND ')}
 SQL
 cmd = "psql inaturalist_production -c \"COPY (#{sql}) TO STDOUT WITH CSV\" > #{fname}"
 puts "\t#{cmd}"
 system cmd
-resurrection_cmds << "psql inaturalist_production -c \"\\copy photos FROM '#{fname}' WITH CSV\""
+resurrection_cmds << "psql inaturalist_production -c \"\\copy observation_photos FROM '#{fname}' WITH CSV\""
 
 puts "Exporting from sounds..."
 fname = "resurrect_#{session_id}-sounds.csv"
