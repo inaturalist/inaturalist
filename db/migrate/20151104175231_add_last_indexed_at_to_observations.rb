4713c2e35616919d5f833b1bc14a411aed390df6
Patrick Leary <patrick.r.leary@gmail.com>
2015-11-04 15:05:35 -0500

    not setting initial last_indexed_at in migration

--- db/migrate/20151104175231_add_last_indexed_at_to_observations.rb
+++ db/migrate/20151104175231_add_last_indexed_at_to_observations.rb
@@ -1,7 +1,6 @@
 class AddLastIndexedAtToObservations < ActiveRecord::Migration
   def up
     add_column :observations, :last_indexed_at, :datetime
-    Observation.update_all(last_indexed_at: Time.now)
     add_index :observations, :last_indexed_at
   end
 
