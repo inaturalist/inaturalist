eed9ad996591fb90c725616fa381227866ddc70a
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2016-01-14 16:12:55 -0800

    Fixed capitalization bug with accents.
    This relies on some pt-BR specific code, which works fine for other romance
    languages, but might cause problems in the future.

--- app/assets/javascripts/angular_bundle.js
+++ app/assets/javascripts/angular_bundle.js
@@ -7,6 +7,7 @@
 //= require_tree ./ang/templates
 //= require ngInfiniteScroll/build/ng-infinite-scroll.min
 //= require angular-capitalize/angular-capitalize
+//= require angular-capitalize/angular-capitalize-locale_pt-br
 //= require angular-bootstrap/ui-bootstrap-tpls
 //= require moment/moment
 //= require angular-moment/angular-moment
