11cb8e6521e9623057bc0531c8752c331c3267ab
Patrick Leary <patrick.r.leary@gmail.com>
2016-01-21 11:57:58 -0500

    obs search accepting projects[] param; fixed improper links with taxon autocomplete

--- app/assets/javascripts/taxa/index.js
+++ app/assets/javascripts/taxa/index.js
@@ -5,7 +5,7 @@ $(function() {
     allow_enter_submit: true,
     search_external: false,
     afterSelect: function( ui ) {
-      window.location.href = inaturalist.TAXON_ROOT_URL + ui.item.id;
+      window.location.href = "/taxa/" + ui.item.id;
     }
   });
 });
