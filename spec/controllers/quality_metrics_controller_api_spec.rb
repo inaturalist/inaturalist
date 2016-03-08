477debca235ddaef1de87a8383ba3a3c4936410f
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2016-03-08 12:31:24 -0800

    Added quality metrics controller to the Rails API.

---
+++ spec/controllers/quality_metrics_controller_api_spec.rb
@@ -0,0 +1,59 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+shared_examples_for "a QualityMetricsController" do
+  let(:user) { User.make! }
+
+  describe "vote" do
+    let(:o) { Observation.make! }
+
+    describe "route" do
+      it "should accept POST requests" do
+        expect( post: "observations/#{o.id}/quality/#{QualityMetric::WILD}" ).to be_routable
+      end
+      it "should accept POST requests" do
+        expect( delete: "observations/#{o.id}/quality/#{QualityMetric::WILD}" ).to be_routable
+      end
+    end
+
+    it "should create a QualityMetric in response to POST" do
+      expect {
+        post :vote, format: :json, id: o.id, metric: QualityMetric::WILD, agree: "true"
+      }.to change( QualityMetric, :count ).by( 1 )
+    end
+    it "should set agree to true if true" do
+      post :vote, format: :json, id: o.id, metric: QualityMetric::WILD, agree: "true"
+      expect( o.quality_metrics.last ).to be_agree
+    end
+    it "should set agree to false if false" do
+      post :vote, format: :json, id: o.id, metric: QualityMetric::WILD, agree: "false"
+      expect( o.quality_metrics.last ).not_to be_agree
+    end
+    it "should destroy an existing QualityMetric in response to DELETE" do
+      qm = QualityMetric.make!( user: user, observation: o, metric: QualityMetric::WILD, agree: true)
+      delete :vote, format: :json, id: o.id, metric: QualityMetric::WILD
+      expect( QualityMetric.find_by_id( qm.id ) ).to be_nil
+      o.reload
+      expect( o.quality_metrics ).to be_blank
+    end
+  end
+
+end
+
+describe QualityMetricsController, "oauth authentication" do
+  let(:token) do
+    double acceptable?: true,
+      accessible?: true,
+      resource_owner_id: user.id,
+      application: OauthApplication.make!
+  end
+  before do
+    request.env["HTTP_AUTHORIZATION"] = "Bearer xxx"
+    allow(controller).to receive(:doorkeeper_token) { token }
+  end
+  it_behaves_like "a QualityMetricsController"
+end
+
+describe QualityMetricsController, "devise authentication" do
+  before { http_login(user) }
+  it_behaves_like "a QualityMetricsController"
+end
