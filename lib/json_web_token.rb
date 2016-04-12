ab933d98075d46a591b8e6d6b46fcbab8cf0c18c
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2016-04-12 16:28:22 -0700

    Change expiration date to 24 hours.

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,7 +1,7 @@
 require "jwt"
 
 class JsonWebToken
-  def self.encode(payload, expiration = 30.minutes.from_now)
+  def self.encode(payload, expiration = 24.hours.from_now)
     payload = payload.dup
     payload[:exp] = expiration.to_i
     JWT.encode(payload, CONFIG.jwt_secret || "secret", "HS512")
