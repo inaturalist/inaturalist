75170f32a8a2a58078656112eb74ca5ef02a88d1
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2016-04-13 18:17:55 -0700

    Fix issue with cache-control screwing up reviewed stats.

--- app/webpack/observations/identify/reducers/search_params_reducer.js
+++ app/webpack/observations/identify/reducers/search_params_reducer.js
@@ -2,8 +2,13 @@ import { UPDATE_SEARCH_PARAMS } from "../actions";
 
 const searchParamsReducer = ( state = {
   reviewed: false,
-  quality_grade: "needs_id",
-  verifiable: true
+  verifiable: true,
+  // This is a hack to get around our node API's cache control settings, since
+  // it defaults to something, and we hit obs search repeatedly for stats. A
+  // better approach might be to have a separate endpoints that delivers these
+  // stats uncached, or disable cache-control when viewer_id is set or
+  // something.
+  ttl: -1
 }, action ) => {
   if ( action.type === UPDATE_SEARCH_PARAMS ) {
     return Object.assign( {}, state, action.params );
