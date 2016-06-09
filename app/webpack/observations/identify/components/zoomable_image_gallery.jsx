53b21159ad3f46cf3522e609c073a1e817a71867
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2016-06-08 21:47:45 -0400

    Made image zoom in Identify an explicit click.

--- app/webpack/observations/identify/components/zoomable_image_gallery.jsx
+++ app/webpack/observations/identify/components/zoomable_image_gallery.jsx
@@ -14,6 +14,7 @@ class ZoomableImageGallery extends ImageGallery {
       return `<div class="easyzoom"><a href="${zoomImgUrl || standardImgUrl}"></a></div>`;
     } );
     $( ".image-gallery-slide .easyzoom", domNode ).easyZoom( {
+      eventType: "click",
       onShow( ) {
         this.$link.addClass( "easyzoom-zoomed" );
       },
