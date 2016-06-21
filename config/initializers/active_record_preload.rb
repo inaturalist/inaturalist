a4ebf07ebdcc59ab23d97068ff783bad6fbd8143
Patrick Leary <patrick.r.leary@gmail.com>
2016-06-21 15:00:25 -0400

    fixed preload_associations console usability

--- config/initializers/active_record_preload.rb
+++ config/initializers/active_record_preload.rb
@@ -3,6 +3,7 @@ module ActiveRecord
 
     def self.preload_associations(instances, associations)
       ActiveRecord::Associations::Preloader.new.preload(instances, associations)
+      nil
     end
 
   end
