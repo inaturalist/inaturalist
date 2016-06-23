cec118c14bfd111aef72e747717f0e0a8db5a0a0
Patrick Leary <patrick.r.leary@gmail.com>
2016-06-23 15:12:57 -0400

    can cancel submit while photos are uploading

--- app/webpack/observations/uploader/components/status_modal.js
+++ app/webpack/observations/uploader/components/status_modal.js
@@ -1,6 +1,6 @@
 import _ from "lodash";
 import React, { PropTypes, Component } from "react";
-import { Modal } from "react-bootstrap";
+import { Modal, Button } from "react-bootstrap";
 import util from "../models/util";
 
 class StatusModal extends Component {
@@ -11,9 +11,21 @@ class StatusModal extends Component {
     const savingCount = ( saveCounts.saving + saveCounts.saved + saveCounts.failed );
     const totalPhotos = _.size( this.props.files );
     const photoIndex = totalPhotos - util.countPending( this.props.files );
+    let footer;
     if ( totalPhotos !== photoIndex ) {
       modalMessage = I18n.t( "uploading_num_of_count_photos",
         { num: photoIndex || 1, count: totalPhotos } );
+      footer = (
+        <Modal.Footer>
+         <div className="buttons">
+            <Button
+              bsStyle={ "primary" }
+              onClick={ () => this.props.setState( { saveStatus: null } ) }>
+              { I18n.t( "keep_editing" ) }
+            </Button>
+          </div>
+        </Modal.Footer>
+      );
     } else if ( ( saveCounts.pending + saveCounts.saving ) === 0 && savingCount !== 0 ) {
       modalMessage = I18n.t( "going_to_your_observations" );
     } else {
@@ -21,10 +33,11 @@ class StatusModal extends Component {
         { num: savingCount || 1, count: this.props.total } );
     }
     return (
-      <Modal show={ this.props.show } className="status">
+      <Modal show={ this.props.show } className="confirm status">
         <Modal.Body>
           <h3>{ modalMessage }</h3>
         </Modal.Body>
+        { footer }
       </Modal>
     );
   }
@@ -33,6 +46,7 @@ class StatusModal extends Component {
 StatusModal.propTypes = {
   files: PropTypes.object,
   saveCounts: PropTypes.object,
+  setState: PropTypes.func,
   total: PropTypes.number,
   show: PropTypes.bool
 };
