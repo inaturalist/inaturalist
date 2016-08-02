c8aa051f171f0ed98a0478ba9b6cf4f0cddf606c
Patrick Leary <patrick.r.leary@gmail.com>
2016-08-02 11:54:06 -0400

    modified uuid migration script to be faster

--- script/migrate_uuid_columns.rb
+++ script/migrate_uuid_columns.rb
@@ -1,18 +1,26 @@
 psql = User.connection
 
-psql.execute("ALTER TABLE observations ADD new_uuid uuid DEFAULT uuid_generate_v4()")   # new column, no default
-psql.execute("UPDATE observations SET new_uuid=uuid::uuid WHERE uuid IS NOT NULL")      # set values
-psql.execute("ALTER TABLE observations RENAME column uuid TO old_uuid")                 # swap out old
-psql.execute("ALTER TABLE observations RENAME column new_uuid TO uuid")                 # swap in new
-psql.execute("DROP INDEX index_observations_on_uuid")                                   # remove old index
-psql.execute("CREATE INDEX index_observations_on_uuid ON observations(uuid)")           # create new index
+Benchmark.measure{ psql.execute("ALTER TABLE observations ADD new_uuid uuid DEFAULT uuid_generate_v4()") }
 
+Benchmark.measure do
+  Observation.select(:id, :uuid).where("uuid IS NOT NULL").find_in_batches(batch_size: 10000) do |batch|
+    psql.transaction do
+      puts "Batch starting at ID #{batch.first.id} at #{Time.now}"
+      batch.each do |o|
+        Observation.where(id: o.id).update_all(new_uuid: o.uuid)
+      end
+    end
+  end
+end
 
-psql.execute("ALTER TABLE observation_photos ADD new_uuid uuid DEFAULT uuid_generate_v4()")   # new column, no default
-psql.execute("UPDATE observation_photos SET new_uuid=uuid::uuid WHERE uuid IS NOT NULL")      # set values
-psql.execute("ALTER TABLE observation_photos RENAME column uuid TO old_uuid")                 # swap out old
-psql.execute("ALTER TABLE observation_photos RENAME column new_uuid TO uuid")                 # swap in new
-psql.execute("DROP INDEX index_observation_photos_on_uuid")                                   # remove old index
-psql.execute("CREATE INDEX index_observation_photos_on_uuid ON observation_photos(uuid)")     # create new index
+Benchmark.measure{ psql.execute("ALTER TABLE observations RENAME column uuid TO old_uuid") }
+Benchmark.measure{ psql.execute("ALTER TABLE observations RENAME column new_uuid TO uuid") }
+Benchmark.measure{ psql.execute("DROP INDEX index_observations_on_uuid") }
+Benchmark.measure{ psql.execute("CREATE INDEX index_observations_on_uuid ON observations(uuid)") }
 
-
+Benchmark.measure{ psql.execute("ALTER TABLE observation_photos ADD new_uuid uuid DEFAULT uuid_generate_v4()") }
+Benchmark.measure{ psql.execute("UPDATE observation_photos SET new_uuid=uuid::uuid WHERE uuid IS NOT NULL") }
+Benchmark.measure{ psql.execute("ALTER TABLE observation_photos RENAME column uuid TO old_uuid") }
+Benchmark.measure{ psql.execute("ALTER TABLE observation_photos RENAME column new_uuid TO uuid") }
+Benchmark.measure{ psql.execute("DROP INDEX index_observation_photos_on_uuid") }
+Benchmark.measure{ psql.execute("CREATE INDEX index_observation_photos_on_uuid ON observation_photos(uuid)") }
