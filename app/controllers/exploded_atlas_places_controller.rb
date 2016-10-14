16e78bd033bd4f49ede969c1a35b9ad2ad95204a
Scott Loarie <loarie@gmail.com>
2016-10-14 10:04:26 -0700

    exploding/collapsing atlas logs alterations

--- app/controllers/exploded_atlas_places_controller.rb
+++ app/controllers/exploded_atlas_places_controller.rb
@@ -2,6 +2,12 @@ class ExplodedAtlasPlacesController < ApplicationController
   def create
     @exploded_atlas_place = ExplodedAtlasPlace.new(:place_id => params[:place_id].to_i, :atlas_id => params[:atlas_id].to_i)
     @exploded_atlas_place.save
+    AtlasAlteration.create(
+      atlas_id: @exploded_atlas_place.atlas_id,
+      user_id: current_user,
+      place_id: @exploded_atlas_place.place_id,
+      action: "exploded"
+    )
     respond_to do |format|
       format.js
     end
@@ -10,9 +16,15 @@ class ExplodedAtlasPlacesController < ApplicationController
   def destroy
     @exploded_atlas_place = ExplodedAtlasPlace.find(params[:id])
     @exploded_atlas_place.destroy
+    AtlasAlteration.create(
+      atlas_id: @exploded_atlas_place.atlas_id,
+      user_id: current_user,
+      place_id: @exploded_atlas_place.place_id,
+      action: "collapsed"
+    )
     respond_to do |format|
       format.js
     end
   end
-  
+   
 end
