10578206249ed2df0ee303bcee71221657e5d42a
Scott Loarie <loarie@gmail.com>
2016-10-21 17:31:00 -0700

    hold off on index for now

--- db/migrate/20161021233858_add_list_taxon_index_to_listed_taxon.rb
+++
@@ -1,6 +0,0 @@
-class AddListTaxonIndexToListedTaxon < ActiveRecord::Migration
-  def change
-    remove_index :listed_taxa, [:list_id, :taxon_id]
-    add_index :listed_taxa, [:list_id, :taxon_id], unique: true
-  end
-end
