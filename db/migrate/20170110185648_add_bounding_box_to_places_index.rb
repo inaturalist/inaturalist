18e8325650a0aaf259c4ef160e0b90d1ad46edc9
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2017-01-10 12:26:20 -0800

    Added apparently forgotten migration for place index bounding box.

--- db/migrate/20170110185648_add_bounding_box_to_places_index.rb
+++ db/migrate/20170110185648_add_bounding_box_to_places_index.rb
@@ -1,4 +1,21 @@
 class AddBoundingBoxToPlacesIndex < ActiveRecord::Migration
-  def change
+  def up
+    Place.__elasticsearch__.client.indices.put_mapping(
+      index: Place.index_name,
+      type: Place.document_type,
+      body: {
+        Place.document_type => {
+          properties: {
+            bounding_box_geojson: {
+              type: "geo_shape"
+            }
+          }
+        }
+      }
+    )
+  end
+
+  def down
+    # There IS no return
   end
 end
