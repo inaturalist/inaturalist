7a075886afa4ead35cca91fe95ee65a008211836
Scott Loarie <loarie@gmail.com>
2017-01-09 20:08:22 -0800

    added is_active to atlases and complete sets

---
+++ db/migrate/20170110025430_add_is_active_to_atlases.rb
@@ -0,0 +1,5 @@
+class AddActiveToAtlases < ActiveRecord::Migration
+  def change
+    add_column :atlases, :is_active, :boolean, default: false
+  end
+end
---
+++ db/migrate/20170110025450_add_is_active_to_complete_sets.rb
@@ -0,0 +1,5 @@
+class AddActiveToCompleteSets < ActiveRecord::Migration
+  def change
+    add_column :complete_sets, :is_active, :boolean, default: false
+  end
+end
