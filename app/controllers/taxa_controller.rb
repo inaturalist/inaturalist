f5045e08a174dc9c3bda608b956e045210941c9b
Patrick Leary <patrick.r.leary@gmail.com>
2017-01-26 12:19:11 -0500

    make sure to include SSL in taxon show page cache path

--- app/controllers/taxa_controller.rb
+++ app/controllers/taxa_controller.rb
@@ -2,7 +2,10 @@
 class TaxaController < ApplicationController
   caches_page :range, :if => Proc.new {|c| c.request.format == :geojson}
   caches_action :show, :expires_in => 1.day,
-    :cache_path => Proc.new{ |c| { locale: I18n.locale, mobile: c.request.format.mobile? } },
+    :cache_path => Proc.new{ |c| {
+      locale: I18n.locale,
+      mobile: c.request.format.mobile?,
+      ssl: c.request.ssl? } },
     :if => Proc.new {|c|
       !request.format.json? &&
       (c.session.blank? || c.session['warden.user.user.key'].blank?) &&
