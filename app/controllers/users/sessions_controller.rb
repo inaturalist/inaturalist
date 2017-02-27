371961fc655b1dd8aaa39693c88069c90520e41c
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2017-02-27 11:42:49 -0800

    Removed flash notifications when signing in and out.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,11 +1,9 @@
 class Users::SessionsController < Devise::SessionsController
   def create
-    Rails.logger.debug "[DEBUG] I'm actually in this controller"
     # attempt straight db auth first, then warden auth
     resource = legacy_authenticate if CONFIG.legacy && CONFIG.legacy.rest_auth
     resource ||= User.authenticate(params[:login], params[:password]) if params[:login] && params[:password]
     resource ||= warden.authenticate!(auth_options)
-    Rails.logger.debug "[DEBUG] resource: #{resource}"
     throw(:warden) unless resource
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(resource_name, resource)
@@ -13,9 +11,9 @@ class Users::SessionsController < Devise::SessionsController
     last_ip = request.env["HTTP_X_FORWARDED_FOR"] if last_ip.split(".")[0..1].join(".") == "10.183"
     last_ip = request.env["HTTP_X_CLUSTER_CLIENT_IP"] if last_ip.split(".")[0..1].join(".") == "10.183"
     resource.update_attribute(:last_ip, last_ip)
-    Rails.logger.debug "[DEBUG] session[:return_to]: #{session[:return_to]}"
     respond_to do |format|
       format.any(:html, :mobile) do
+        flash.delete(:notice)
         if session[:return_to]
           redirect_to session[:return_to]
         else
@@ -42,6 +40,10 @@ class Users::SessionsController < Devise::SessionsController
         ])
       end
     end
+  end
+
+  def destroy
+    super { flash.delete(:notice) }
   end
 
   private
