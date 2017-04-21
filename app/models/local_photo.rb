ef0d819e82267aa461e2f3dedbc4484c8696fee7
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2017-04-20 20:32:02 -0700

    Fix encoding bug with photo metadata parsing.

--- app/models/local_photo.rb
+++ app/models/local_photo.rb
@@ -112,19 +112,20 @@ class LocalPhoto < Photo
 
   def extract_metadata(path = nil)
     return unless file && (path || !file.queued_for_write.blank?)
-    self.metadata ||= { dimensions: { } }
+    metadata = self.metadata.to_h.clone || {}
+    metadata[:dimensions] ||= { }
     begin
       file.styles.keys.each do |style|
-        self.metadata[:dimensions][style] = extract_dimensions(style)
+        metadata[:dimensions][style] = extract_dimensions(style)
       end
       if file_content_type =~ /jpe?g/i && exif = EXIFR::JPEG.new(path || file.queued_for_write[:original].path)
-        self.metadata.merge!(exif.to_hash)
+        metadata.merge!(exif.to_hash)
         xmp = XMP.parse(exif)
         if xmp && xmp.respond_to?(:dc) && !xmp.dc.nil?
-          self.metadata[:dc] = {}
+          metadata[:dc] = {}
           xmp.dc.attributes.each do |dcattr|
             begin
-              self.metadata[:dc][dcattr.to_sym] = xmp.dc.send(dcattr) unless xmp.dc.send(dcattr).blank?
+              metadata[:dc][dcattr.to_sym] = xmp.dc.send(dcattr) unless xmp.dc.send(dcattr).blank?
             rescue ArgumentError
               # XMP does this for some DC attributes, not sure why
             end
@@ -140,7 +141,8 @@ class LocalPhoto < Photo
       raise e unless e.message =~ /no implicit conversion of Fixnum into String/
       Rails.logger.error "[ERROR #{Time.now}] Failed to parse EXIF for #{self}: #{e}"
     end
-    self.metadata = self.metadata.force_utf8
+    metadata = metadata.force_utf8
+    self.metadata = metadata
   end
 
   def set_urls
