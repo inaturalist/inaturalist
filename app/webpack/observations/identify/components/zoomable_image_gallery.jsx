effdf89eb5448c9c0551a1249b144e272bda014f
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2017-06-23 13:46:18 -0700

    Minor style fix for obs/show, removing some unecessary code.

--- app/webpack/observations/identify/components/zoomable_image_gallery.jsx
+++ app/webpack/observations/identify/components/zoomable_image_gallery.jsx
@@ -29,17 +29,6 @@ class ZoomableImageGallery extends ImageGallery {
       },
       loadingNotice: I18n.t( "loading" )
     } );
-    // Close the zoomed image when mouse is out of the container. This solves an
-    // ipad problem in which the image would get stuck in the zoomed in mode,
-    // and a resolves a request that the image unzoom when you click outside of
-    // it. Maybe it should be optional?
-    easyZoomTarget.on( {
-      "mouseleave.easyzoom touchend.easyzoom": () => {
-        _.each( easyZoomTarget, t => {
-          $( t ).data( "easyZoom" )._onLeave( );
-        } );
-      }
-    } );
   }
 
   componentDidUpdate( prevProps ) {
