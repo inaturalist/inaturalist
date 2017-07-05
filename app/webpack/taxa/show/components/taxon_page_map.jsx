465c5a652e8a44356d50b029f41a0b54789ac6cc
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2017-07-04 21:07:04 -0700

    Restore map bounds on taxon page.

--- app/webpack/taxa/show/components/taxon_page_map.jsx
+++ app/webpack/taxa/show/components/taxon_page_map.jsx
@@ -26,14 +26,10 @@ const TaxonPageMap = ( { taxon, bounds, latitude, longitude, zoomLevel } ) => {
           places: true,
           ranges: true
         }]}
-        // minX={ bounds ? bounds.swlng : null }
-        // minY={ bounds ? bounds.swlat : null }
-        // maxX={ bounds ? bounds.nelng : null }
-        // maxY={ bounds ? bounds.nelat : null }
-        minX={0}
-        minY={0}
-        maxX={1}
-        maxY={1}
+        minX={ bounds ? bounds.swlng : null }
+        minY={ bounds ? bounds.swlat : null }
+        maxX={ bounds ? bounds.nelng : null }
+        maxY={ bounds ? bounds.nelat : null }
         latitude={ latitude }
         longitude={ longitude }
         zoomLevel={ zoomLevel }
