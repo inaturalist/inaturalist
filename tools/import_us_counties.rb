1dc4cabcd87e9eef9ba31e262f8dd6daa8519140
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2017-10-17 13:54:03 -0700

    Added US county import script.

---
+++ tools/import_us_counties.rb
@@ -0,0 +1,28 @@
+# Add country places and boundaries from naturalearthdata.org
+
+def system_call(cmd)
+  puts "Running #{cmd}"
+  system cmd
+  puts
+end
+
+# CONFIGURE
+url = "https://www2.census.gov/geo/tiger/TIGER2012/COUNTY/tl_2012_us_county.zip"
+shapefile_name = "tl_2012_us_county.shp"
+test = false
+#/ CONFIGURE
+
+filename = File.basename(url)
+tmp_path = File.join(Dir::tmpdir, "#{File.basename(__FILE__, ".*")}-#{Time.now.to_i}")
+archive_path = "#{tmp_path}/#{filename}"
+work_path = tmp_path
+FileUtils.mkdir_p tmp_path, :mode => 0755
+
+system_call "curl -Lo #{tmp_path}/#{filename} #{url}"
+system_call "unzip -d #{tmp_path} #{tmp_path}/#{filename}"
+
+Place.import_from_shapefile("#{work_path}/#{shapefile_name}", 
+    :place_type_name => 'County', 
+    :source => 'census',
+    :skip_woeid => true,
+    :test => test)
