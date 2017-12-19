7640f0e91cceed2c66c4dd9e41924e8170829be1
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2017-12-19 13:51:10 -0800

    Replaced C3 histogram with a pure D3 one. Because masochism.

---
+++ app/webpack/stats/year/components/date_histogram.jsx
@@ -0,0 +1,117 @@
+import React, { PropTypes } from "react";
+import ReactDOM from "react-dom";
+import _ from "lodash";
+import * as d3 from "d3";
+import d3tip from "d3-tip";
+import legend from "d3-svg-legend";
+
+class DateHistogram extends React.Component {
+  componentDidMount( ) {
+    this.renderHistogram( );
+  }
+
+  renderHistogram( ) {
+    const mountNode = $( ".chart", ReactDOM.findDOMNode( this ) ).get( 0 );
+    const svg = d3.select( mountNode ).append( "svg" );
+    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    const width = $( "svg", mountNode ).width( ) - margin.left - margin.right;
+    const height = $( "svg", mountNode ).height( ) - margin.top - margin.bottom;
+    const g = svg.append( "g" ).attr( "transform", `translate(${margin.left}, ${margin.top})` );
+
+    const parseTime = d3.timeParse( "%Y-%m-%d" );
+    const series = {};
+    _.forEach( this.props.series, ( s, seriesName ) => {
+      series[seriesName] = _.map( s.data, d => ( {
+        date: parseTime( d.date ),
+        value: d.value
+      } ) );
+    } );
+    const x = d3.scaleTime( ).rangeRound( [0, width] );
+    const y = d3.scaleLinear( ).rangeRound( [height, 0] );
+    const line = d3.line( )
+        .x( d => x( d.date ) )
+        .y( d => y( d.value ) );
+
+    const combinedData = _.flatten( _.values( series ) );
+    x.domain( d3.extent( combinedData, d => d.date ) );
+    y.domain( d3.extent( combinedData, d => d.value ) );
+
+    g.append( "g" )
+        .attr( "transform", `translate(0,${height})` )
+        .call( d3.axisBottom( x ) )
+        .select( ".domain" )
+              .remove();
+
+    g.append( "g" )
+        .call( d3.axisLeft( y ) );
+
+    const dateFormatter = d3.timeFormat( "%d %b" );
+    const tip = d3tip()
+      .attr( "class", "d3-tip" )
+      .offset( [-10, 0] ).
+      html( d => `<strong>${dateFormatter( d.date )}</strong>: ${d.value}` );
+    svg.call( tip );
+
+    const color = d3.scaleOrdinal( d3.schemeCategory10 );
+    const colorForSeries = seriesName => {
+      if ( this.props.series[seriesName] && this.props.series[seriesName].color ) {
+        return this.props.series[seriesName].color;
+      } else {
+        return color( seriesName );
+      }
+    };
+    _.forEach( series, ( seriesData, seriesName ) => {
+      const seriesGroup = g.append( "g" );
+      seriesGroup.classed( _.snakeCase( seriesName ), true );
+      seriesGroup.append( "path" ).datum( seriesData )
+          .attr( "fill", "none" )
+          .attr( "stroke", colorForSeries( seriesName ) )
+          .attr( "stroke-linejoin", "round" )
+          .attr( "stroke-linecap", "round" )
+          .attr( "stroke-width", 1.5 )
+          .attr( "d", line );
+      seriesGroup.selectAll( "path" ).data( seriesData )
+        .enter().append( "circle" )
+          .attr( "cx", d => x( d.date ) )
+          .attr( "cy", d => y( d.value ) )
+          .attr( "r", 2 )
+          .attr( "fill", "white" )
+          .style( "stroke", ( ) => colorForSeries( seriesName ) )
+          .on( "mouseover", tip.show )
+          .on( "mouseout", tip.hide );
+    } );
+
+    svg.append( "g" )
+      .attr( "class", "legendOrdinal" )
+      .attr( "transform", `translate(${width - 20},20)` );
+    const legendScale = d3.scaleOrdinal( )
+      .domain( _.keys( series ) )
+      .range( _.map( series, ( v, k ) => colorForSeries( k ) ) );
+    const legendOrdinal = legend.legendColor()
+      .labels( _.map( this.props.series, ( v, k ) => ( v.title || k ) ) )
+      .classPrefix( "legend" )
+      .shape( "path", d3.symbol( ).type( d3.symbolCircle ).size( 100 )( ) )
+      .shapePadding( 5 )
+      .scale( legendScale );
+    svg.select( ".legendOrdinal" )
+      .call( legendOrdinal );
+  }
+
+  render( ) {
+    return (
+      <div className="Histogram">
+        <div className="chart"></div>
+      </div>
+    );
+  }
+}
+
+DateHistogram.propTypes = {
+  series: PropTypes.object
+};
+
+DateHistogram.defaultProps = {
+  series: {}
+};
+
+export default DateHistogram;
--- app/webpack/stats/year/components/observations.jsx
+++ app/webpack/stats/year/components/observations.jsx
@@ -1,47 +1,22 @@
 import React from "react";
 import _ from "lodash";
-import Histogram from "./histogram";
+import DateHistogram from "./date_histogram";
 
 const Observations = ( { data } ) => {
-  const daysX = ["daysX"].concat( _.keys( data.day_histogram.day ) );
-  const days = ["days"].concat( _.values( data.day_histogram.day ) );
-  const weeksX = ["weeksX"].concat( _.keys( data.week_histogram.week ) );
-  const weeks = ["weeks"].concat( _.values( data.week_histogram.week ) );
   return (
     <div className="Observations">
       <h2>{ I18n.t( "observations" ) }</h2>
-      <Histogram
-        columns={ [daysX, weeksX, days, weeks] }
-        config={ {
-          data: {
-            xs: {
-              weeks: "weeksX",
-              days: "daysX"
-            },
-            colors: {
-              weeks: "#74ac00",
-              days: "#aaaaaa"
-            }
+      <DateHistogram
+        series={ {
+          week: {
+            title: "Per Week",
+            data: _.map( data.week_histogram.week, ( value, date ) => ( { date, value } ) ),
+            color: "#74ac00"
           },
-          axis: {
-            x: {
-              type: "timeseries"
-            },
-            y: {
-              min: 0,
-              show: true,
-              padding: {
-                left: 0,
-                bottom: 0
-              },
-              tick: {
-                outer: false,
-                format: d => I18n.toNumber( d, { precision: 0 } )
-              }
-            }
-          },
-          point: {
-            r: 2
+          day: {
+            title: "Per Day",
+            data: _.map( data.day_histogram.day, ( value, date ) => ( { date, value } ) ),
+            color: "#aaaaaa"
           }
         } }
       />
