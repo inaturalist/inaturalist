76aad1d5c757c6c831bd95435b5c2061b9dcd088
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2017-12-27 19:07:02 -0500

    Fixed generate stats button for other years.

--- app/webpack/stats/year/components/generate_stats_button.jsx
+++ app/webpack/stats/year/components/generate_stats_button.jsx
@@ -12,10 +12,9 @@ class GenerateStatsButton extends React.Component {
 
   generateStats( ) {
     this.setState( { loading: true } );
-    const year = ( new Date( ) ).getYear( ) + 1900;
     const data = new FormData( );
     data.append( "authenticity_token", $( "meta[name=csrf-token]" ).attr( "content" ) );
-    data.append( "year", year );
+    data.append( "year", this.props.year );
     fetch( "/stats/generate_year", {
       method: "POST",
       body: data
