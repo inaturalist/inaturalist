3d10fe007d0abafe2151dde8cc76d9ed17978566
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2018-02-23 17:02:47 -0800

    Title-case names for taxon autocomplete, universal autocomplete.

--- app/webpack/observations/uploader/components/taxon_autocomplete.js
+++ app/webpack/observations/uploader/components/taxon_autocomplete.js
@@ -331,7 +331,7 @@ class TaxonAutocomplete extends React.Component {
     ) {
       name = result.name;
     } else {
-      name = result.preferred_common_name || result.english_common_name || result.name;
+      name = iNatModels.Taxon.titleCaseName( result.preferred_common_name || result.english_common_name ) || result.name;
     }
     return name;
   }
@@ -447,7 +447,7 @@ class TaxonAutocomplete extends React.Component {
     }
     if ( result.title ) {
       if ( scinameFirst ) {
-        result.subtitle = result.preferred_common_name || result.english_common_name;
+        result.subtitle = iNatModels.Taxon.titleCaseName( result.preferred_common_name || result.english_common_name );
       }
       if ( !result.subtitle && result.name !== result.title ) {
         if ( result.rank_level <= 20 ) {
