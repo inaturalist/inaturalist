e19808c859b1d5130918be11387c4c8e789bed01
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2018-03-28 19:17:42 -0700

    Tests that rely on the node API aren't going to fly on Travis.

--- spec/lib/elastic_model/indices/place_index_spec.rb
+++ spec/lib/elastic_model/indices/place_index_spec.rb
@@ -7,8 +7,5 @@ describe "Place Index" do
       json = p.as_indexed_json
       expect( json ).to be_a Hash
     end
-    it "should set observations_count" do
-      expect( make_place_with_geom.as_indexed_json[:observations_count] ).not_to be_nil
-    end
   end
 end
