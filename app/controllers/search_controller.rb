864713fe5f1e53a835250b18fd1c8d678ee4ae33
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2018-04-19 14:25:49 -0700

    Deal with some missing translations and empty states.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,33 +4,38 @@ class SearchController < ApplicationController
   def index
     @sources = [params[:source] || []].flatten
     @q = params[:q]
-    response = INatAPIService.get(
-      "/search",
-      q: @q,
-      page: params[:page],
-      sources: @sources.join( "," )
-    )
-    record_ids_by_type = response.results.inject( {} ) do |memo, result|
-      memo[result["type"]] ||= []
-      memo[result["type"]] << result["record"]["id"]
-      memo
-    end
-    records_by_type_id = record_ids_by_type.inject( {} ) do |memo, ( type, ids )|
-      memo[type] ||= {}
-      scope = Object.const_get( type ).where( id: ids )
-      if type == "Taxon"
-        scope = scope.includes( { taxon_photos: :photo }, :taxon_descriptions, { taxon_names: :place_taxon_names } )
+    if @q.blank?
+      response = {}
+      records = []
+    else
+      response = INatAPIService.get(
+        "/search",
+        q: @q,
+        page: params[:page],
+        sources: @sources.join( "," )
+      )
+      record_ids_by_type = response.results.inject( {} ) do |memo, result|
+        memo[result["type"]] ||= []
+        memo[result["type"]] << result["record"]["id"]
+        memo
       end
-      scope.each do |record|
-        memo[type][record.id] = record
+      records_by_type_id = record_ids_by_type.inject( {} ) do |memo, ( type, ids )|
+        memo[type] ||= {}
+        scope = Object.const_get( type ).where( id: ids )
+        if type == "Taxon"
+          scope = scope.includes( { taxon_photos: :photo }, :taxon_descriptions, { taxon_names: :place_taxon_names } )
+        end
+        scope.each do |record|
+          memo[type][record.id] = record
+        end
+        memo
       end
-      memo
+      records = response.results.map do |result|
+        next unless klass = Object.const_get( result["type"] )
+        records_by_type_id[result["type"]][result["record"]["id"]]
+      end.compact
     end
-    records = response.results.map do |result|
-      next unless klass = Object.const_get( result["type"] )
-      records_by_type_id[result["type"]][result["record"]["id"]]
-    end.compact
-    @results = WillPaginate::Collection.create( response["page"], response["per_page"], response["total_results"] || 0 ) do |pager|
+    @results = WillPaginate::Collection.create( response["page"] || 0, response["per_page"] || 0, response["total_results"] || 0 ) do |pager|
       pager.replace( records )
     end
   end
