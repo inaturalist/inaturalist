5d74a95d13423f8fcf9df4afb6ec10dde130baf4
Patrick Leary <patrick.r.leary@gmail.com>
2018-04-22 12:18:19 -0400

    projects without rules can be added to umbrellas; cannot switch project type after creation

--- app/webpack/projects/form/components/umbrella_form.jsx
+++ app/webpack/projects/form/components/umbrella_form.jsx
@@ -36,7 +36,6 @@ class UmbrellaForm extends React.Component {
                   } }
                   notIDs={ _.map( project.projectRules, rule => rule.project.id ) }
                   notTypes={ ["umbrella"] }
-                  hasParams
                   bootstrapClear
                 />
               </div>
