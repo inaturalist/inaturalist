6d068f1d446dfc38710923a14d2c2923152e5dd6
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2018-05-14 13:17:11 -0700

    Respect obs photo position when choosing og:image (closes #1773)

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -282,10 +282,14 @@ class ObservationsController < ApplicationController
         
         user_viewed_updates(delay: true) if logged_in?
         @observer_provider_authorizations = @observation.user.provider_authorizations
-        @shareable_image_url = if !@observation.photos.blank? && photo = @observation.photos.detect{ |p| p.medium_url =~ /^http/ }
-          FakeView.image_url( photo.best_url(:original) )
-        else
-          FakeView.iconic_taxon_image_url( @observation.taxon, size: 200 )
+        # @shareable_image_url = if !@observation.photos.blank? && photo = @observation.photos.detect{ |p| p.medium_url =~ /^http/ }
+        #   FakeView.image_url( photo.best_url(:original) )
+        # else
+        #   FakeView.iconic_taxon_image_url( @observation.taxon, size: 200 )
+        # end
+        @shareable_image_url = FakeView.iconic_taxon_image_url( @observation.taxon, size: 200 )
+        if op = @observation.observation_photos.sort_by{|op| op.position.to_i || op.id }.first
+          @shareable_image_url = FakeView.image_url( op.photo.best_url(:original) )
         end
         @shareable_title = if @observation.taxon
           if comname = FakeView.common_taxon_name( @observation.taxon, user: current_user, site: @site ).try(:name)
