6dcae0264729cd7efc5730aa85e2191fe5f2295f
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2018-05-24 18:04:32 -0700

    Taxon map legend taxon checkbox should turn things off and on again.

--- app/webpack/taxa/show/components/taxon_page_map.jsx
+++ app/webpack/taxa/show/components/taxon_page_map.jsx
@@ -19,6 +19,7 @@ const TaxonPageMap = ( { taxon, bounds, latitude, longitude, zoomLevel } ) => {
         showAllLayer={false}
         minZoom={2}
         gbifLayerLabel={I18n.t( "maps.overlays.gbif_network" )}
+        reloadKey={ `taxon-map-${t.id}` }
         taxonLayers={[{
           taxon: t,
           observations: {
