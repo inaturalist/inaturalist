4b24dcb8fd546f39ecb44c920be6364bfcd91812
Scott Loarie <loarie@gmail.com>
2018-09-04 16:22:08 -0700

    fixed broken spec

--- spec/models/guide_taxon_spec.rb
+++ spec/models/guide_taxon_spec.rb
@@ -47,9 +47,9 @@ describe GuideTaxon, "deletion" do
   it "should update the guide's taxon id" do
     without_delay do
       g = Guide.make!
-      ancestor = Taxon.make!
-      t1 = Taxon.make!(:parent => ancestor)
-      t2 = Taxon.make!
+      ancestor = Taxon.make!(rank: Taxon::GENUS)
+      t1 = Taxon.make!(parent: ancestor, rank: Taxon::SPECIES)
+      t2 = Taxon.make!(parent: ancestor, rank: Taxon::SPECIES)
       gt1 = GuideTaxon.make!(:guide => g, :taxon => t1)
       gt2 = GuideTaxon.make!(:guide => g, :taxon => t2)
       g.reload
