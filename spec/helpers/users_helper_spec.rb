ae4b2090dbac219217d4b087a59c8269a5f236d6
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2018-10-10 15:15:02 -0700

    Removed some ancient, unused code and updated a spec.

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -4,51 +4,36 @@ include UsersHelper
 include AuthenticatedTestHelper
 
 describe UsersHelper do
-  before do
-    @user = mock_user
-  end
-  
-  describe "if_authorized" do 
-    it "yields if authorized" do
-      should_receive(:authorized?).with('a','r').and_return(true)
-      if_authorized?('a','r'){|action,resource| [action,resource,'hi'] }.should == ['a','r','hi']
-    end
-    it "does nothing if not authorized" do
-      should_receive(:authorized?).with('a','r').and_return(false)
-      if_authorized?('a','r'){ 'hi' }.should be_nil
-    end
-  end
-  
+  let(:user) { User.make! }
   describe "link_to_user" do
     it "should return an error string on a nil user" do
-      link_to_user(nil).should == 'deleted user'
+      expect( link_to_user( nil ) ).to eq 'deleted user'
     end
     it "should link to the given user" do
-      should_receive(:person_url).at_least(:once).and_return('http://test.host/people/user_name')
-      link_to_user(@user).should have_tag("a[href='http://test.host/people/user_name']")
+      expect( link_to_user( user ) ).to have_tag( "a[href='http://test.host/people/#{user.login}']" )
     end
     it "should use given link text if :content_text is specified" do
-      link_to_user(@user, :content_text => 'Hello there!').should have_tag("a", 'Hello there!')
+      expect( link_to_user( user, content_text: "Hello there!" ) ).to have_tag( "a", "Hello there!" )
     end
     it "should use the login as link text with no :content_method specified" do
-      link_to_user(@user).should have_tag("a", 'user_name')
+      expect( link_to_user( user ) ).to have_tag( "a", user.login )
     end
     it "should use the name as link text with :content_method => :name" do
-      link_to_user(@user, :content_method => :name).should have_tag("a", 'U. Surname')
+      expect( link_to_user( user, content_method: :name ) ).to have_tag( "a", user.name )
     end
     it "should use the login as title with no :title_method specified" do
-      link_to_user(@user).should have_tag("a[title='user_name']")
+      expect( link_to_user( user ) ).to have_tag( "a[title='#{user.login}']" )
     end
     it "should use the name as link title with :content_method => :name" do
-      link_to_user(@user, :title_method => :name).should have_tag("a[title='U. Surname']")
+      expect( link_to_user( user , title_method: :name ) ).to have_tag( "a[title='#{user.name}']" )
     end
     it "should have nickname as a class by default" do
-      link_to_user(@user).should have_tag("a.nickname")
+      expect( link_to_user( user ) ).to have_tag( "a.nickname" )
     end
     it "should take other classes and no longer have the nickname class" do
-      result = link_to_user(@user, :class => 'foo bar')
-      result.should have_tag("a.foo")
-      result.should have_tag("a.bar")
+      result = link_to_user( user , class: "foo bar" )
+      expect( result ).to have_tag( "a.foo" )
+      expect( result ).to have_tag( "a.bar" )
     end
   end
 
