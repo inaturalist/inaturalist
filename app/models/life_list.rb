a3c2a7755bd11248b231c34bc30d9b4c419e3e72
Scott Loarie <loarie@gmail.com>
2018-10-12 09:49:31 -0700

    roll infrasp to parent (not sp) in case taxonomy wack

--- app/models/life_list.rb
+++ app/models/life_list.rb
@@ -24,11 +24,11 @@ class LifeList < List
   def add_taxon(taxon, options = {})
     taxon_id = taxon.is_a?(Taxon) ? taxon.id : taxon
     
-    # Make sure the parent species is added for infraspecies
+    # Make sure the parent is added for infraspecies
     taxon = taxon.is_a?(Taxon) ? taxon : Taxon.find(taxon_id)
-    if !taxon.rank_level.nil? && taxon.rank_level < Taxon::SPECIES_LEVEL && taxon.species
-      unless listed_taxon = listed_taxa.find_by_taxon_id(taxon.species.id)
-        lt = ListedTaxon.create(options.merge(list: self, taxon_id: taxon.species.id))
+    if !taxon.rank_level.nil? && taxon.rank_level < Taxon::SPECIES_LEVEL && taxon.parent && taxon.parent.rank_level >= Taxon::SPECIES_LEVEL
+      unless listed_taxon = listed_taxa.find_by_taxon_id(taxon.parent.id)
+        lt = ListedTaxon.create(options.merge(list: self, taxon_id: taxon.parent.id))
       end
     end
     
