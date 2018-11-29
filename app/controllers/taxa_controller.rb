4936f84a1a9f08db50a026ee1df190351fd0b8b5
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2018-11-28 17:12:17 -0800

    Several minor fixes (closes #2060, #2059, #2058, and #2052)

--- app/controllers/taxa_controller.rb
+++ app/controllers/taxa_controller.rb
@@ -208,7 +208,13 @@ class TaxaController < ApplicationController
       format.html do
         options = {}
         options[:api_token] = current_user.api_token if current_user
-        @node_taxon_json = INatAPIService.get_json( "/taxa/#{@taxon.id}", options )
+        site_place = @site && @site.place
+        user_place = current_user && current_user.place
+        preferred_place = user_place || site_place
+        @node_taxon_json = INatAPIService.get_json(
+          "/taxa/#{@taxon.id}?preferred_place_id=#{preferred_place.try(:id)}&place_id=#{@place.try(:id)}&locale=#{I18n.locale}",
+          options
+        )
         place_id = current_user.preferred_taxon_page_place_id if logged_in?
         place_id = session[:prefers_taxon_page_place_id] if place_id.blank?
         @place = Place.find_by_id( place_id )
