e037eae5da2be2f548785ea6685bb0d0f38feb57
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2018-12-11 11:08:50 -0800

    Release donate button in web and email footer.

--- app/controllers/donate_controller.rb
+++ app/controllers/donate_controller.rb
@@ -5,4 +5,10 @@ class DonateController < ApplicationController
     @footless = true
     @no_footer_gap = true
   end
+
+  def index
+    if Site.default && @site && @site.id != Site.default.id
+      return redirect_to donate_url( host: Site.default.domain )
+    end
+  end
 end
