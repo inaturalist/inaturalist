a156f1dad4a62220cf6c80752621736ba85a54d7
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2018-12-21 16:32:52 -0500

    Forgot about using faves_count in YIR front end.

--- app/models/year_statistic.rb
+++ app/models/year_statistic.rb
@@ -363,7 +363,7 @@ class YearStatistic < ActiveRecord::Base
     # )
     es_params_with_sort = es_params.merge(
       sort: [
-        { "cached_votes_total": "desc" },
+        { "faves_count": "desc" },
         { "comments_count": "desc" }
       ]
     )
@@ -392,10 +392,10 @@ class YearStatistic < ActiveRecord::Base
       
     JSON.
         parse( INatAPIService.get_json( "/observations", api_params, { timeout: 30 } ) )["results"].
-        sort_by{|o| [0 - o["cached_votes_total"].to_i, 0 - o["comments_count"].to_i] }.
+        sort_by{|o| [0 - o["faves_count"].to_i, 0 - o["comments_count"].to_i] }.
         each_with_index.map do |o,i|
       if i < 10
-        o.select{|k,v| %w(id taxon community_taxon user photos comments_count cached_votes_total observed_on).include?( k ) }
+        o.select{|k,v| %w(id taxon community_taxon user photos comments_count faves_count observed_on).include?( k ) }
       elsif !o["photos"].blank?
         {
           "id": o["id"],
