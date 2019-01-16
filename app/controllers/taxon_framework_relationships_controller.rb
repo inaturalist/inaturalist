36d395dc812a59554798babda56a3ccb4e9f59ab
Scott Loarie <loarie@gmail.com>
2019-01-15 18:02:10 -0800

    sort taxon_frameworks

--- app/controllers/taxon_framework_relationships_controller.rb
+++ app/controllers/taxon_framework_relationships_controller.rb
@@ -11,7 +11,7 @@ class TaxonFrameworkRelationshipsController < ApplicationController
     @relationships ||= TaxonFrameworkRelationship::RELATIONSHIPS.map{ |r| filter_params[r] == "1" ? r : nil }
     @relationships.delete_if{ |r| r.blank? }
     @taxon_framework = TaxonFramework.find_by_id( filter_params[:taxon_framework_id] ) unless filter_params[:taxon_framework_id].blank?    
-    @taxon_frameworks = TaxonFramework.all.limit( 100 )
+    @taxon_frameworks = TaxonFramework.includes( :taxon ).where( "taxon_frameworks.rank_level IS NOT NULL" ).order('taxa.name').limit( 100 )
     @taxon = Taxon.find_by_id( filter_params[:taxon_id].to_i ) unless filter_params[:taxon_id].blank?
     user_id = filter_params[:user_id] || params[:user_id]
     @user = User.find_by_id(user_id) || User.find_by_login(user_id) unless user_id.blank?
@@ -69,7 +69,7 @@ class TaxonFrameworkRelationshipsController < ApplicationController
         order( "taxa.rank_level ASC" ).first]
     else
       @taxon_framework_relationship.taxa.new
-      @taxon_frameworks = TaxonFramework.all.limit( 100 )
+      @taxon_frameworks = TaxonFramework.includes( :taxon ).all.order('taxa.name').limit( 100 )
     end
   end
   
@@ -85,7 +85,7 @@ class TaxonFrameworkRelationshipsController < ApplicationController
         if taxon = Taxon.where( id: row["id"] ).first
          if !taxon.taxon_framework_relationship_id.nil? && row["unlink"] == "false"
             flash[:error] = "#{ taxon.name } is already represented in a Taxon Framework Relationship"
-            @taxon_frameworks = TaxonFramework.all.limit( 100 )
+            @taxon_frameworks = TaxonFramework.includes( :taxon ).all.order('taxa.name').limit( 100 )
             render action: :new
             return
           end
@@ -111,7 +111,7 @@ class TaxonFrameworkRelationshipsController < ApplicationController
         redirect_to @taxon_framework_relationship
       end
     else
-      @taxon_frameworks = TaxonFramework.all.limit( 100 )
+      @taxon_frameworks = TaxonFramework.includes( :taxon ).all.order('taxa.name').limit( 100 )
       render action: :new
     end
   end
@@ -123,7 +123,7 @@ class TaxonFrameworkRelationshipsController < ApplicationController
           where( "taxon_frameworks.rank_level <= ? AND taxon_id IN (?)", taxon.rank_level, taxon.ancestor_ids ).
           order( "taxa.rank_level ASC" ).first]
     else
-      @taxon_frameworks = TaxonFramework.all.limit( 100 )
+      @taxon_frameworks = TaxonFramework.includes( :taxon ).all.order('taxa.name').limit( 100 )
     end
   end
 
@@ -145,7 +145,7 @@ class TaxonFrameworkRelationshipsController < ApplicationController
                   where( "taxon_frameworks.rank_level <= ? AND taxon_id IN (?)", taxon.rank_level, taxon.ancestor_ids ).
                   order( "taxa.rank_level ASC" ).first]
             else
-              @taxon_frameworks = TaxonFramework.all.limit( 100 )
+              @taxon_frameworks = TaxonFramework.includes( :taxon ).all.order('taxa.name').limit( 100 )
             end
             render action: :edit
             return
@@ -177,7 +177,7 @@ class TaxonFrameworkRelationshipsController < ApplicationController
         @taxon_frameworks = [TaxonFramework.joins( "JOIN taxa ON taxa.id = taxon_frameworks.taxon_id" ).
             where( "taxon_frameworks.rank_level <= ? AND taxon_id IN (?)", taxon.rank_level, taxon.ancestor_ids ).order( "taxa.rank_level ASC" ).first]
       else
-        @taxon_frameworks = TaxonFramework.all.limit( 100 )
+        @taxon_frameworks = TaxonFramework.includes( :taxon ).all.order('taxa.name').limit( 100 )
       end
       render action: :edit
     end
