e9e1801a646a8ccf7065dd0811b4c13b38949b39
Patrick Leary <patrick.r.leary@gmail.com>
2019-01-20 17:44:11 -0500

    also use names fields for names-specific searches

--- app/es_indices/observation_index.rb
+++ app/es_indices/observation_index.rb
@@ -324,6 +324,10 @@ class Observation < ActiveRecord::Base
     end
   end
 
+  def self.elasticsearch_taxon_names_fields
+    @@taxon_names_fields ||= TaxonName::LOCALES.values.map{ |l| "taxon.names_#{l}" }.sort
+  end
+
   def self.params_to_elastic_query(params, options = {})
     current_user = options[:current_user] || params[:viewer]
     p = params[:_query_params_set] ? params : query_params(params)
@@ -339,7 +343,7 @@ class Observation < ActiveRecord::Base
     if q
       fields = case search_on
       when "names"
-        [ "taxon.names_*" ]
+        elasticsearch_taxon_names_fields
       when "tags"
         [ :tags ]
       when "description"
@@ -347,42 +351,7 @@ class Observation < ActiveRecord::Base
       when "place"
         [ :place_guess ]
       else
-        [
-          "taxon.names_ar",
-          "taxon.names_bg",
-          "taxon.names_br",
-          "taxon.names_ca",
-          "taxon.names_cs",
-          "taxon.names_da",
-          "taxon.names_de",
-          "taxon.names_el",
-          "taxon.names_en",
-          "taxon.names_es",
-          "taxon.names_et",
-          "taxon.names_eu",
-          "taxon.names_fi",
-          "taxon.names_fr",
-          "taxon.names_gl",
-          "taxon.names_haw",
-          "taxon.names_id",
-          "taxon.names_it",
-          "taxon.names_iw",
-          "taxon.names_ja",
-          "taxon.names_ko",
-          "taxon.names_lb",
-          "taxon.names_mi",
-          "taxon.names_mk",
-          "taxon.names_myn",
-          "taxon.names_nl",
-          "taxon.names_oc",
-          "taxon.names_pt",
-          "taxon.names_ru",
-          "taxon.names_sci",
-          "taxon.names_sq",
-          "taxon.names_zh",
-          "taxon.names_zh-CN",
-          :tags, :description, :place_guess
-        ]
+        elasticsearch_taxon_names_fields + [ :tags, :description, :place_guess ]
       end
       search_filters << { multi_match: { query: q, operator: "and", fields: fields } }
     end
