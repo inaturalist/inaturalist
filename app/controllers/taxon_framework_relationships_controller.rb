550f5dfb58c1212aaedf4909a20b0835181e9e10
Scott Loarie <loarie@gmail.com>
2019-01-25 15:57:50 -0800

    partially opened up taxon_framework_relationships to taxon_curators

--- app/controllers/taxon_framework_relationships_controller.rb
+++ app/controllers/taxon_framework_relationships_controller.rb
@@ -1,7 +1,6 @@
 class TaxonFrameworkRelationshipsController < ApplicationController
   before_filter :find_taxon_framework_relationship, only: [:show, :edit]
   before_filter :authenticate_user!, except: [:index, :show]
-  before_filter :admin_required, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_taxon_framework_relationship, except: [:index, :new, :create]
   
   layout "bootstrap"
