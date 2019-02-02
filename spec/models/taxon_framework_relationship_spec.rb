921f23f2f46e8044f6ad75f3c7a7f5a736b28fa4
Scott Loarie <loarie@gmail.com>
2019-02-01 21:59:11 -0800

    fix broken specs

--- spec/models/taxon_framework_relationship_spec.rb
+++ spec/models/taxon_framework_relationship_spec.rb
@@ -3,12 +3,12 @@ require File.dirname(__FILE__) + '/../spec_helper.rb'
 describe "relationship" do
   it "should update relationship when external taxon destroyed" do
     tfr = TaxonFrameworkRelationship.make!
-    p = Taxon.make!(name: "Taricha", rank: "genus")
-    t = Taxon.make!(name: "Taricha torosa", rank: "species", taxon_framework_relationship_id: tfr.id)
-    t.parent = p
+    t = Taxon.make!(name: "Taricha torosa", rank: "species")
+    t.parent = tfr.taxon_framework.taxon
     t.save
+    t.update_attributes( taxon_framework_relationship_id: tfr.id )
     t.reload
-    et = ExternalTaxon.new(name: "Taricha torosa", rank: "species", parent_name: "Taricha", parent_rank: "genus", taxon_framework_relationship_id: tfr.id)
+    et = ExternalTaxon.new(name: "Taricha torosa", rank: "species", parent_name: t.parent.name, parent_rank: t.parent.rank, taxon_framework_relationship_id: tfr.id)
     et.save
     tfr.reload
     expect(tfr.relationship).to eq "match"
