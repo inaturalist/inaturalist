b5ad82447192175cf00b67bc43e6fc60ae0a6737
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2019-02-08 15:22:34 -0800

    Removed Picasa spec that no longer applies, added one with lowered expectations.

--- spec/models/picasa_photo_spec.rb
+++ spec/models/picasa_photo_spec.rb
@@ -3,24 +3,18 @@ require File.dirname(__FILE__) + '/../spec_helper.rb'
 describe PicasaPhoto do
   describe "to_observation" do
     before(:all) do
-      f = open(File.dirname(__FILE__) + '/../fixtures/picasa_photo.xml', 'r')
-      @xml = f.read
-      f.close
-      @nxml = Nokogiri::XML(@xml)
+      open( File.join( File.dirname(__FILE__), "..", "fixtures", "google_photos_library_photo.json" ) ) do |f|
+        @fixture = JSON.parse( f.read )
+      end
     end
 
     before(:each) { enable_elastic_indexing( Observation, Place ) }
     after(:each) { disable_elastic_indexing( Observation, Place ) }
     
-    it "should set coordinates" do
-      expect(@nxml.at('//gml:pos')).not_to be_blank
-      plat, plon = @nxml.at('//gml:pos').inner_text.split
-      api_response = RubyPicasa::Photo.new(@xml)
-      photo = PicasaPhoto.new(:user => User.make)
-      photo.api_response = api_response
+    it "should set description" do
+      photo = PicasaPhoto.new_from_api_response( @fixture )
       obs = photo.to_observation
-      expect(obs.latitude.to_f).to eq plat.to_f
-      expect(obs.longitude.to_f).to eq plon.to_f
+      expect( obs.description ).to eq @fixture["description"]
     end
   end
 end
