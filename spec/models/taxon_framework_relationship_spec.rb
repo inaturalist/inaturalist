92a47321ce90f50019ce0370e0ab0e600a503921
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2019-02-07 16:27:46 -0800

    Ensure spec runs consistently without relying on externally created data.

--- spec/models/taxon_framework_relationship_spec.rb
+++ spec/models/taxon_framework_relationship_spec.rb
@@ -2,18 +2,25 @@ require File.dirname(__FILE__) + '/../spec_helper.rb'
 
 describe "relationship" do
   it "should update relationship when external taxon destroyed" do
-    tfr = TaxonFrameworkRelationship.make!
-    t = Taxon.make!(name: "Taricha torosa", rank: "species")
-    t.parent = tfr.taxon_framework.taxon
-    t.save
-    t.update_attributes( taxon_framework_relationship_id: tfr.id )
-    t.reload
-    et = ExternalTaxon.new(name: "Taricha torosa", rank: "species", parent_name: t.parent.name, parent_rank: t.parent.rank, taxon_framework_relationship_id: tfr.id)
+    genus = Taxon.make!( rank: Taxon::GENUS )
+    species = Taxon.make!( rank: Taxon::SPECIES )
+    species.update_attributes( parent: genus )
+    tf = TaxonFramework.make!( taxon: genus )
+    tfr = TaxonFrameworkRelationship.make!( taxon_framework: tf )
+    species.update_attributes( taxon_framework_relationship_id: tfr.id )
+    species.reload
+    et = ExternalTaxon.new(
+      name: species.name,
+      rank: "species",
+      parent_name: species.parent.name,
+      parent_rank: species.parent.rank,
+      taxon_framework_relationship_id: tfr.id
+    )
     et.save
     tfr.reload
-    expect(tfr.relationship).to eq "match"
+    expect( tfr.relationship ).to eq "match"
     et.destroy
     tfr.reload
-    expect(tfr.relationship).to eq "not_external"
+    expect( tfr.relationship ).to eq "not_external"
   end
 end
