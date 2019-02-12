9d6b3f91998b8b779c5d8060175c31c5689ba72c
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2019-02-11 16:26:32 -0800

    Map he locale to iw (closes #2143)

--- app/controllers/shared/filters_module.rb
+++ app/controllers/shared/filters_module.rb
@@ -12,6 +12,9 @@ module Shared::FiltersModule
     locale = locale_from_header if locale.blank?
     locale = I18n.default_locale if locale.blank?
     I18n.locale = locale
+    if I18n.locale.to_s == "he"
+      I18n.locale = I18n.locale.to_s.sub( "he", "iw" ).to_sym
+    end
     unless I18N_SUPPORTED_LOCALES.include?( I18n.locale.to_s )
       I18n.locale = I18n.default_locale
     end
