b408cb842b27427ffcc506111f55cbe79bbb2438
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2019-04-09 14:31:33 -0700

    Another reminder that I don't understand / don't like materialized paths.

--- app/models/taxon_change.rb
+++ app/models/taxon_change.rb
@@ -46,8 +46,10 @@ class TaxonChange < ActiveRecord::Base
   scope :taxon, lambda{|taxon|
     joins(TAXON_JOINS).
     where(
-      "t1.id = ? OR t2.id = ? OR t1.ancestry LIKE ? OR t1.ancestry LIKE ?",
-      taxon, taxon, "#{taxon.ancestry}/#{taxon.id}/%", "#{taxon.ancestry}/#{taxon.id}/%"
+      "t1.id = ? OR t2.id = ? OR t1.ancestry = ? OR t1.ancestry = ? OR t1.ancestry LIKE ? OR t1.ancestry LIKE ?",
+      taxon, taxon,
+      "#{taxon.ancestry}/#{taxon.id}", "#{taxon.ancestry}/#{taxon.id}",
+      "#{taxon.ancestry}/#{taxon.id}/%", "#{taxon.ancestry}/#{taxon.id}/%"
     )
   }
   
