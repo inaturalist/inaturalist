9ac7e448a0df52e1692a0eab0478007597592c1b
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2019-04-10 15:51:18 -0700

    Bugfix: flags could not be resolved if the flaggable owner blocked the flagger after the flag was created.

--- config/initializers/acts_as_blockable.rb
+++ config/initializers/acts_as_blockable.rb
@@ -4,11 +4,12 @@ module ActsAsBlockable
 
   class_methods do
     def blockable_by( user_id_finder, options = {} )
-      validate do
+      blockable_user_finder = options.delete(:blockable_user_id)
+      validate( options ) do
         blocking_user_id = user_id_finder.call( self )
         blocking_user_id = blocking_user_id.id if blocking_user_id.is_a?( User )
-        blockable_user_id = if options[:blockable_user_id]
-          options[:blockable_user_id].call( self )
+        blockable_user_id = if blockable_user_finder
+          blockable_user_finder.call( self )
         else
           try(:user_id) || try(:user).try(:id)
         end
