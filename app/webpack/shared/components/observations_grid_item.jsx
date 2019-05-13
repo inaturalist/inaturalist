564e19ceca0f419ad09afa3641e0cebbe7e3711a
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2019-05-13 09:55:24 -0700

    Abhas's photo count redesign

--- app/webpack/shared/components/observations_grid_item.jsx
+++ app/webpack/shared/components/observations_grid_item.jsx
@@ -48,8 +48,7 @@ const ObservationsGridItem = ( {
             className="photo-count"
             title={I18n.t( "x_photos", { count: o.photos.length } )}
           >
-            <i className="fa fa-picture-o" />
-            { o.photos.length }
+            { o.photos.length > 9 ? "+" : o.photos.length }
           </span>
         ) }
         { o.photos && o.photos.length > 1 && (
