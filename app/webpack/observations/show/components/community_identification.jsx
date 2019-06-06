20deb05246f8c8d651dcfdc50cceaf4885f6e739
Scott Loarie <loarie@gmail.com>
2019-06-05 23:27:22 -0700

    experiment with ancestor disagreements only disagreeing with previous_observed_taxon, not the rest of the branch between it and the id taxon

--- app/webpack/observations/show/components/community_identification.jsx
+++ app/webpack/observations/show/components/community_identification.jsx
@@ -219,7 +219,10 @@ class CommunityIdentification extends React.Component {
       _.each( sortedIdents, i => {
         const idAncestry = `${i.taxon.ancestry}/${i.taxon.id}`;
         if ( obsTaxonAncestry.includes( idAncestry ) || idAncestry.includes( obsTaxonAncestry ) ) {
-          if ( obsTaxonAncestry.includes( idAncestry ) && obsTaxonAncestry !== idAncestry && i.disagreement ) {
+          if ( obsTaxonAncestry.includes( i.previous_observation_taxon_id  ) && obsTaxonAncestry !== idAncestry && i.disagreement ) {
+            if( i.previous_observation_taxon_id == 31 ){
+              console.log( obsTaxonAncestry );
+            }
             votesAgainst.push( i );
           } else if ( first_ident_of_taxon && obsTaxonAncestry.includes( idAncestry ) && obsTaxonAncestry !== idAncestry && i.disagreement == null && i.id > first_ident_of_taxon.id ) {
             votesAgainst.push( i );
