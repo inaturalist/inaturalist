db64b493ca56c577e00b707f8f5725bde02fe979
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2019-08-21 18:32:00 -0700

    Add iNat auto summaries as the taxon describer of last resort (closes #2373)

---
+++ lib/taxon_describers/lib/taxon_describers/inaturalist.rb
@@ -0,0 +1,19 @@
+# Trivial taxon describer that uses Taxon#auto_summary to provide a bare bones
+# summary of any iNat taxon
+module TaxonDescribers
+  class Inaturalist < Base
+    def describe( taxon )
+      taxon.auto_summary
+    end
+
+    def page_url( taxon )
+      fake_view.taxon_url( taxon )
+    end
+
+    def name
+      "iNaturalist"
+    end
+    alias :describer_name :name
+
+  end
+end
