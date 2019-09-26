698a8858cbdcb3628e31552511367fb2a3921955
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2019-09-25 17:22:00 -0700

    Attempting to deal with CVE 2015 9284
    Mostly following guidance from
    https://github.com/omniauth/omniauth/wiki/Resolving-CVE-2015-9284

--- app/controllers/taxa_controller.rb
+++ app/controllers/taxa_controller.rb
@@ -1585,8 +1585,7 @@ class TaxaController < ApplicationController
       if e.message =~ /Insufficient permissions/ || e.message =~ /signature_invalid/
         auth_url = auth_url_for('flickr', :scope => 'write')
         flash[:error] = ("#{@site.site_name_short} can't add tags to your photos until " +
-          "Flickr knows you've given us permission.  " + 
-          "<a href=\"#{auth_url}\">Click here to authorize #{@site.site_name_short} to add tags</a>.").html_safe
+          "Flickr knows you've given us permission.").html_safe
       else
         flash[:error] = "Something went wrong trying to to post those tags: #{e.message}"
       end
@@ -1655,7 +1654,7 @@ class TaxaController < ApplicationController
     @provider_authorization = current_user.provider_authorizations.where(provider_name: "flickr").first
     if @provider_authorization.blank? || @provider_authorization.scope != 'write'
       session[:return_to] = request.get? ? request.fullpath : request.env['HTTP_REFERER']
-      redirect_to auth_url_for('flickr', :scope => 'write')
+      redirect_to url_for( controller: :flickr, action: :options, scope: "write" )
       return false
     end
   end
