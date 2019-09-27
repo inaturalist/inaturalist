908025f3e6e8a0d5c82b94b1f864857f685d8164
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2019-09-27 15:59:05 -0700

    Attempt to get custom pluralizer to deal with numbers with delimiters

--- config/initializers/i18n_pluralization_default.rb
+++ config/initializers/i18n_pluralization_default.rb
@@ -15,7 +15,18 @@ module I18n
         if pluralizer.respond_to?(:call)
           return entry[:zero] if count == 0 && entry.has_key?(:zero)
 
-          plural_key = pluralizer.call(count)
+          # Sometimes the pluralizer is going to get a string that's a number
+          # with delimiters, so we need to parse that into an actual number
+          if count.is_a?( String )
+            count_num = count.to_s.
+              gsub( I18n.t( "number.format.delimiter" ), "" ).
+              gsub( I18n.t( "number.format.separator" ), "." ).
+              to_f
+          else
+            count_num = count
+          end
+
+          plural_key = pluralizer.call( count_num )
           return entry[plural_key] if entry.has_key?(plural_key)
 
           # fallback to the default gettext plural forms if real entry is missing (for example :few)
