726f6c0dc6c8ca6c32067f570f7b8420f7c39a79
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2019-10-11 11:46:26 -0700

    Added script to export user data; added ETA and avg time to script_find_each

--- config/initializers/script_find_each.rb
+++ config/initializers/script_find_each.rb
@@ -4,10 +4,38 @@ module ActiveRecord
     def script_find_each(options = {})
       total = self.count
       current = 1
+      recent_times = []
+      flush = options.delete(:flush)
+      max_count_width = Math::log( total ) + 2
       find_each(options) do |record|
-        puts ["#{record.class.name} #{record.id}", "#{current.to_s.rjust(10)} / #{total}", "#{(current.to_f / total * 100).round(2)}%"].join("\t")
+        eta = "??????????????????"
+        avg = 0
+        unless recent_times.blank?
+          avg = recent_times.sum.to_f / recent_times.size
+          eta = FakeView.time_ago_in_words( (
+            avg * ( total - current )
+          ).seconds.from_now )
+        end
+        pieces = [
+          "#{record.class.name} #{record.id}".ljust( max_count_width + record.class.name.size + 5 ),
+          "#{current.to_s.rjust(max_count_width)} / #{total}",
+          "#{(current.to_f / total * 100).round(2)}%",
+          "Avg: #{"#{avg.to_f.round( 4 ).to_s}s".ljust(10)}",
+          "ETA #{eta}"
+        ]
+        col_size = pieces.sort.last.size + 4
+        msg = pieces.map{|p| p.ljust( col_size )}.join
+        if flush
+          print msg + "\r"
+          $stdout.flush
+        else
+          puts msg
+        end
         current += 1
+        start = Time.now
         yield record
+        recent_times << Time.now - start
+        recent_times.unshift if recent_times.size > 10
       end
     end
   end
