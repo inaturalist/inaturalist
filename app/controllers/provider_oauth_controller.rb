80d9671ff25dcb52585219ff23e0539c86980ff5
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2020-01-30 16:44:30 -0800

    Ensure that if no oauth scopes are requested via the assertion flow, a token still gets the default scopes

--- app/controllers/provider_oauth_controller.rb
+++ app/controllers/provider_oauth_controller.rb
@@ -239,7 +239,8 @@ class ProviderOauthController < ApplicationController
   def scopes_from_params( params, client )
     allowed_scopes = client.scopes.try(:to_a) || []
     requested_scopes = params[:scopes].to_s.split( /\s/ ).compact.uniq
-    scopes = ( allowed_scopes & requested_scopes ).join( " " )
-    scopes.blank? ? nil : scopes
+    scopes = allowed_scopes & requested_scopes
+    scopes = Doorkeeper.configuration.default_scopes.to_a if scopes.blank?
+    scopes.join( " " )
   end
 end
