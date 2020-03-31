10aee213d3d47bd373b0ba6c0c01351b54435cf7
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2020-03-30 19:19:50 -0700

    Tried to refactor some site stats calcs in ES; added observers and identifiers

--- app/assets/javascripts/stats.js
+++ app/assets/javascripts/stats.js
@@ -1,34 +1,39 @@
 /* global _ */
 /* global google */
 /* global I18n */
+/* global STATS_JSON */
+/* global d3 */
 
 var Stats = { };
 
-Stats.dateForStat = function( stat ) {
+Stats.dateForStat = function ( stat ) {
   var date = new Date( stat.created_at );
   var timezoneOffsetInMilliseconds = date.getTimezoneOffset( ) * 60 * 1000;
   var startOfLocalDay = new Date( date.getTime() + timezoneOffsetInMilliseconds );
-  return startOfLocalDay
-}
+  return startOfLocalDay;
+};
 
-Stats.loadCharts = function( ) {
-  var prefetched_stats;
-  try { prefetched_stats = STATS_JSON; }
-  catch( err ) { prefetched_stats = null; }
-  if( prefetched_stats ) {
-    Stats.loadChartsFromJSON( prefetched_stats );
+Stats.loadCharts = function ( ) {
+  var prefetchedStats;
+  try {
+    prefetchedStats = STATS_JSON;
+  } catch ( err ) {
+    prefetchedStats = null;
+  }
+  if ( prefetchedStats ) {
+    Stats.loadChartsFromJSON( prefetchedStats );
   } else {
     Stats.loadAjaxCharts( );
   }
 };
 
-Stats.loadAjaxCharts = function( ) {
-  $.getJSON("/stats.json?start_date=" + Stats.yearAgoDate( ), function( json ) {
+Stats.loadAjaxCharts = function ( ) {
+  $.getJSON( "/stats.json?start_date=" + Stats.yearAgoDate( ), function ( json ) {
     Stats.loadChartsFromJSON( json );
-  });
+  } );
 };
 
-Stats.loadChartsFromJSON = function( json ) {
+Stats.loadChartsFromJSON = function ( json ) {
   Stats.loadObsSpark( json );
   Stats.loadPercentIdSpark( json );
   Stats.loadPercentCIDToGenusSpark( json );
@@ -39,7 +44,9 @@ Stats.loadChartsFromJSON = function( json ) {
   Stats.loadObservations7Days( json );
   Stats.loadPlatforms( json );
   Stats.loadTTID( json );
-  Stats.loadUsers( json );
+  Stats.loadActiveUsers( json );
+  Stats.loadRecentUsers( json );
+  Stats.loadDailyUsers( json );
   Stats.loadObservations( json );
   Stats.loadCumulativeUsers( json );
   Stats.loadCumulativePlatforms( json );
@@ -49,84 +56,87 @@ Stats.loadChartsFromJSON = function( json ) {
 };
 
 Stats.loadObsSpark = function ( json ) {
-  google.setOnLoadCallback(Stats.sparkline({
+  const data = _.map( json, function ( stat ) {
+    return [Stats.dateForStat( stat ), stat.data.observations.today];
+  } );
+  google.setOnLoadCallback( Stats.sparkline( {
     element_id: "obsspark",
     series: [
       { label: "Today" }
     ],
-    data: _.map( json, function( stat ) {
-      return [ Stats.dateForStat( stat ), stat.data.observations.today ]
-    })
-  }));
-}
+    data
+  } ) );
+};
 
 Stats.loadPercentIdSpark = function ( json ) {
-  google.setOnLoadCallback(Stats.sparkline({
+  const data = _.map( json, function ( stat ) {
+    if ( stat.data.identifier ) {
+      return [Stats.dateForStat( stat ), stat.data.identifier.percent_id];
+    }
+    return [Stats.dateForStat( stat ), 0];
+  } );
+  google.setOnLoadCallback( Stats.sparkline( {
     element_id: "percentidspark",
     series: [
       { label: "% ID" }
     ],
-    data: _.map( json, function( stat ) {
-      if (stat.data.identifier) {
-        return [ Stats.dateForStat( stat ), stat.data.identifier.percent_id]
-      } else {
-        return [ Stats.dateForStat( stat ), 0]
-      }
-    })
-  }));
-}
+    data
+  } ) );
+};
 
 Stats.loadPercentCIDToGenusSpark = function ( json ) {
-  google.setOnLoadCallback(Stats.sparkline({
+  google.setOnLoadCallback( Stats.sparkline( {
     element_id: "percentcidtogenusspark",
     series: [
       { label: "% ID" }
     ],
-    data: _.map( json, function( stat ) {
-      if (stat.data.identifier) {
-        return [ Stats.dateForStat( stat ), stat.data.identifier.percent_cid_to_genus ]
-      } else {
-        return [ Stats.dateForStat( stat ), 0 ]
+    data: _.map( json, function ( stat ) {
+      if ( stat.data.identifier ) {
+        return [Stats.dateForStat( stat ), stat.data.identifier.percent_cid_to_genus];
       }
-    })
-  }));
-}
+      return [Stats.dateForStat( stat ), 0];
+    } )
+  } ) );
+};
+
 Stats.loadActiveUsersSpark = function ( json ) {
-  google.setOnLoadCallback(Stats.sparkline({
+  google.setOnLoadCallback( Stats.sparkline( {
     element_id: "activeusersspark",
     series: [
       { label: "% ID" }
     ],
-    data: _.map( json, function( stat ) {
-      return [ Stats.dateForStat( stat ), stat.data.users.active ]
-    })
-  }));
-}
+    data: _.map( json, function ( stat ) {
+      return [Stats.dateForStat( stat ), stat.data.users.active];
+    } )
+  } ) );
+};
+
 Stats.loadNewUsersSpark = function ( json ) {
-  google.setOnLoadCallback(Stats.sparkline({
+  google.setOnLoadCallback( Stats.sparkline( {
     element_id: "newusersspark",
     series: [
       { label: "% ID" }
     ],
-    data: _.map( json, function( stat ) {
-      return [ Stats.dateForStat( stat ), stat.data.users.last_7_days ]
-    })
-  }));
-}
+    data: _.map( json, function ( stat ) {
+      return [Stats.dateForStat( stat ), stat.data.users.last_7_days];
+    } )
+  } ) );
+};
+
 Stats.load7ObsUsersSpark = function ( json ) {
-  google.setOnLoadCallback(Stats.sparkline({
+  google.setOnLoadCallback( Stats.sparkline( {
     element_id: "new7obsusersspark",
     series: [
       { label: "% ID" }
     ],
-    data: _.map( json, function( stat ) {
-      return [ Stats.dateForStat( stat ), stat.data.users.recent_7_obs ]
-    })
-  }));
-}
+    data: _.map( json, function ( stat ) {
+      return [Stats.dateForStat( stat ), stat.data.users.recent_7_obs];
+    } )
+  } ) );
+};
 
-Stats.loadObservations = function( json ) {
-  google.setOnLoadCallback(Stats.simpleChart({
+Stats.loadObservations = function ( json ) {
+  google.setOnLoadCallback( Stats.simpleChart( {
     chartOptions: {
       legend: { position: "bottom" }
     },
@@ -135,19 +145,19 @@ Stats.loadObservations = function( json ) {
       { label: I18n.t( "total" ) },
       { label: I18n.t( "research_grade" ) }
     ],
-    data: _.map( json, function( stat ) {
+    data: _.map( json, function ( stat ) {
       stat.data.platforms_cumulative = stat.data.platforms_cumulative || { };
       return [
         Stats.dateForStat( stat ),
         stat.data.observations.count,
         stat.data.observations.research_grade
       ];
-    })
-  }));
+    } )
+  } ) );
 };
 
-Stats.loadCumulativePlatforms = function( json ) {
-  google.setOnLoadCallback(Stats.simpleChart({
+Stats.loadCumulativePlatforms = function ( json ) {
+  google.setOnLoadCallback( Stats.simpleChart( {
     element_id: "cumulative-platforms",
     chartOptions: { isStacked: true },
     series: [
@@ -156,7 +166,7 @@ Stats.loadCumulativePlatforms = function( json ) {
       { label: I18n.t( "android" ) },
       { label: I18n.t( "other" ) }
     ],
-    data: _.map( json, function( stat ) {
+    data: _.map( json, function ( stat ) {
       stat.data.platforms_cumulative = stat.data.platforms_cumulative || { };
       return [
         Stats.dateForStat( stat ),
@@ -165,12 +175,12 @@ Stats.loadCumulativePlatforms = function( json ) {
         stat.data.platforms_cumulative.android,
         stat.data.platforms_cumulative.other
       ];
-    })
-  }));
+    } )
+  } ) );
 };
 
-Stats.loadObservations7Days = function( json ) {
-  google.setOnLoadCallback(Stats.simpleChart({
+Stats.loadObservations7Days = function ( json ) {
+  google.setOnLoadCallback( Stats.simpleChart( {
     element_id: "obs_7",
     chartType: google.visualization.AnnotationChart,
     series: [
@@ -180,7 +190,7 @@ Stats.loadObservations7Days = function( json ) {
       { label: I18n.t( "views.stats.index.obs_cid_d_to_genus" ) },
       { label: I18n.t( "views.stats.index.obs_1_day" ) }
     ],
-    data: _.map( json, function( stat ) {
+    data: _.map( json, function ( stat ) {
       return [
         Stats.dateForStat( stat ),
         stat.data.observations.last_7_days,
@@ -189,12 +199,12 @@ Stats.loadObservations7Days = function( json ) {
         stat.data.observations.community_identified_to_genus,
         stat.data.observations.today
       ];
-    })
-  }));
+    } )
+  } ) );
 };
 
-Stats.loadPlatforms = function( json ) {
-  google.setOnLoadCallback(Stats.simpleChart({
+Stats.loadPlatforms = function ( json ) {
+  google.setOnLoadCallback( Stats.simpleChart( {
     element_id: "platforms",
     chartType: google.visualization.AnnotationChart,
     series: [
@@ -203,7 +213,7 @@ Stats.loadPlatforms = function( json ) {
       { label: I18n.t( "android" ) },
       { label: I18n.t( "other" ) }
     ],
-    data: _.map( json, function( stat ) {
+    data: _.map( json, function ( stat ) {
       stat.data.platforms = stat.data.platforms || { };
       return [
         Stats.dateForStat( stat ),
@@ -212,16 +222,16 @@ Stats.loadPlatforms = function( json ) {
         stat.data.platforms.android,
         stat.data.platforms.other
       ];
-    })
-  }));
+    } )
+  } ) );
 };
 
-Stats.loadTTID = function( json ) {
-  var dodgerblue = d3.rgb('dodgerblue'),
-      ldodgerblue = d3.rgb(dodgerblue.r + 75, dodgerblue.g + 75, dodgerblue.b + 75),
-      pink = d3.rgb('deeppink'),
-      lpink = d3.rgb(pink.r + 100, pink.g + 100, pink.b + 100)
-  google.setOnLoadCallback(Stats.simpleChart({
+Stats.loadTTID = function ( json ) {
+  var dodgerblue = d3.rgb( "dodgerblue" );
+  var ldodgerblue = d3.rgb( dodgerblue.r + 75, dodgerblue.g + 75, dodgerblue.b + 75);
+  var pink = d3.rgb( "deeppink" );
+  var lpink = d3.rgb( pink.r + 100, pink.g + 100, pink.b + 100 );
+  google.setOnLoadCallback( Stats.simpleChart( {
     element_id: "ttid",
     chartType: google.visualization.AnnotationChart,
     series: [
@@ -230,8 +240,8 @@ Stats.loadTTID = function( json ) {
       { label: I18n.t( "views.stats.index.med_ttcid" ) },
       { label: I18n.t( "views.stats.index.avg_ttcid" ) }
     ],
-    data: _.map( json, function( stat ) {
-      if (stat.data.identifier) {
+    data: _.map( json, function ( stat ) {
+      if ( stat.data.identifier ) {
         return [
           Stats.dateForStat( stat ),
           stat.data.identifier.med_ttid / 60,
@@ -239,12 +249,11 @@ Stats.loadTTID = function( json ) {
           stat.data.identifier.med_ttcid / 60,
           stat.data.identifier.avg_ttcid / 60
         ];
-      } else {
-        return [ Stats.dateForStat( stat ), null, null, null, null];
       }
-    }),
+      return [Stats.dateForStat( stat ), null, null, null, null];
+    } ),
     chartOptions: {
-      scaleType: 'allfixed',
+      scaleType: "allfixed",
       colors: [
         dodgerblue.toString(),
         ldodgerblue.toString(),
@@ -252,23 +261,23 @@ Stats.loadTTID = function( json ) {
         lpink.toString()
       ]
     }
-  }));
+  } ) );
 };
 
-Stats.loadProjects = function( json ) {
-  google.setOnLoadCallback(Stats.simpleChart({
+Stats.loadProjects = function ( json ) {
+  google.setOnLoadCallback( Stats.simpleChart( {
     chartOptions: {
       legend: { position: "none" }
     },
     element_id: "projects",
-    series: [ { label: I18n.t( "total" ) } ],
-    data: _.map( json, function( stat ) {
-      return [ Stats.dateForStat( stat ), stat.data.projects.count ]
-    })
-  }));
+    series: [{ label: I18n.t( "total" ) }],
+    data: _.map( json, function ( stat ) {
+      return [Stats.dateForStat( stat ), stat.data.projects.count];
+    } )
+  } ) );
 };
 
-Stats.loadCumulativeUsers = function( json ) {
+Stats.loadCumulativeUsers = function ( json ) {
   google.setOnLoadCallback( Stats.simpleChart( {
     chartOptions: {
       legend: { position: "bottom" }
@@ -290,14 +299,47 @@ Stats.loadCumulativeUsers = function( json ) {
   } ) );
 };
 
-Stats.loadUsers = function ( json ) {
+Stats.loadDailyUsers = function ( json ) {
   google.setOnLoadCallback( Stats.simpleChart( {
-    element_id: "users",
+    element_id: "daily-users",
     chartType: google.visualization.AnnotationChart,
     series: [
-      { label: I18n.t( "active" ) },
       { label: I18n.t( "new" ) },
-      { label: I18n.t( "identifiers" ) },
+      { label: I18n.t( "observers" ) },
+      { label: I18n.t( "identifiers" ) }
+    ],
+    data: _.map( json, function ( stat ) {
+      return [
+        Stats.dateForStat( stat ),
+        stat.data.users.today,
+        stat.data.users.observers,
+        stat.data.users.identifiers
+      ];
+    } )
+  } ) );
+};
+
+Stats.loadActiveUsers = function ( json ) {
+  google.setOnLoadCallback( Stats.simpleChart( {
+    element_id: "active-users",
+    chartType: google.visualization.AnnotationChart,
+    series: [
+      { label: I18n.t( "active" ) }
+    ],
+    data: _.map( json, function ( stat ) {
+      return [
+        Stats.dateForStat( stat ),
+        stat.data.users.active
+      ];
+    } )
+  } ) );
+};
+
+Stats.loadRecentUsers = function ( json ) {
+  google.setOnLoadCallback( Stats.simpleChart( {
+    element_id: "recent-users",
+    chartType: google.visualization.AnnotationChart,
+    series: [
       { label: I18n.t( "recent" ) },
       { label: I18n.t( "views.stats.index.recent_w_7_obs" ) },
       { label: I18n.t( "views.stats.index.recent_w_0_obs" ) }
@@ -305,9 +347,6 @@ Stats.loadUsers = function ( json ) {
     data: _.map( json, function ( stat ) {
       return [
         Stats.dateForStat( stat ),
-        stat.data.users.active,
-        stat.data.users.today,
-        stat.data.users.identifiers,
         stat.data.users.last_7_days,
         stat.data.users.recent_7_obs,
         stat.data.users.recent_0_obs
@@ -342,23 +381,23 @@ Stats.loadRanks = function ( json ) {
   } ) );
 };
 
-Stats.loadRanksPie = function( json ) {
-  google.setOnLoadCallback(Stats.simpleChart({
+Stats.loadRanksPie = function ( json ) {
+  google.setOnLoadCallback( Stats.simpleChart( {
     element_id: "ranks_pie",
-    data: _.map( json[0].data.taxa.count_by_rank, function( value, rank ) {
-      return [ I18n.t( "ranks." + rank, { defaultValue: rank } ), parseInt( value ) ];
-    }),
+    data: _.map( json[0].data.taxa.count_by_rank, function ( value, rank ) {
+      return [I18n.t( "ranks." + rank, { defaultValue: rank } ), parseInt( value, 0 )];
+    } ),
     chartType: google.visualization.PieChart
-  }));
+  } ) );
 };
 
-Stats.yearAgoDate = function( ) {
+Stats.yearAgoDate = function ( ) {
   var date = new Date( );
-  return ( date.getFullYear( ) - 4 ) + "-" +
-    (date.getMonth( ) + 1) + "-" + date.getDate( );
+  return ( date.getFullYear( ) - 4 ) + "-"
+    + ( date.getMonth( ) + 1 ) + "-" + date.getDate( );
 };
 
-Stats.monthAgoDate = function( ) {
+Stats.monthAgoDate = function ( ) {
   var date = new Date( );
   var year = date.getFullYear( );
   var month = ( ( date.getMonth( ) + 11 ) % 12 ) + 1;
@@ -369,12 +408,12 @@ Stats.monthAgoDate = function( ) {
   return monthAgo;
 };
 
-Stats.simpleChart = function( options ) {
+Stats.simpleChart = function ( options ) {
   options.chartType = options.chartType || google.visualization.AreaChart;
   var chartOptions = options.chartOptions || { };
   var data = new google.visualization.DataTable( );
-  if( options.chartType === google.visualization.AreaChart ) {
-    data.addColumn( 'date', 'Date' );
+  if ( options.chartType === google.visualization.AreaChart ) {
+    data.addColumn( "date", "Date" );
     chartOptions.vAxis = { minValue: 0 };
     chartOptions.height = 300;
     chartOptions.chartArea = { height: "80%" };
@@ -383,41 +422,39 @@ Stats.simpleChart = function( options ) {
       keepInBounds: false,
       zoomDelta: 1.05
     };
-  } else if( options.chartType === google.visualization.PieChart ) {
-    data.addColumn( 'string', 'Key' );
-    data.addColumn( 'number', 'Value' );
-  } else if (options.chartType = google.visualization.AnnotationChart) {
-    data.addColumn( 'date', 'Date' );
-    chartOptions.min = 0
-    chartOptions.zoomStartTime = new Date(Stats.monthAgoDate())
+  } else if ( options.chartType === google.visualization.PieChart ) {
+    data.addColumn( "string", "Key" );
+    data.addColumn( "number", "Value" );
+  } else if ( options.chartType === google.visualization.AnnotationChart ) {
+    data.addColumn( "date", "Date" );
+    chartOptions.min = 0;
+    chartOptions.zoomStartTime = new Date( Stats.monthAgoDate( ) );
   }
-  _.each( options.series, function( s ) {
-    data.addColumn( 'number', s.label );
-  });
+  _.each( options.series, function ( s ) {
+    data.addColumn( "number", s.label );
+  } );
   data.addRows( options.data );
-  var chart = new options.chartType(
-    document.getElementById( options.element_id ));
+  var chart = new options.chartType( document.getElementById( options.element_id ) );
   chart.draw( data, chartOptions );
 };
 
-Stats.sparkline = function( options ) {
-  var element = options.element_id,
-      series = options.series,
-      data = options.data,
-      graph = d3.select("#"+element).append("svg:svg").attr("width", "100%").attr("height", "100%"),
-      numDays = 100;
-  data = _.map( data.slice(0, numDays).reverse(), function( stat ) {
-    return stat[1] || 0
-  })
-  var x = d3.scale.linear().domain([0, data.length]).range([0, $('#'+element).width()]);
-  var y = d3.scale.linear().domain([0, _.max(data)]).range([$('#'+element).height(), 0]);
+Stats.sparkline = function ( options ) {
+  var element = options.element_id;
+  var series = options.series;
+  var data = options.data;
+  var graph = d3.select( "#" + element ).append( "svg:svg" ).attr( "width", "100%" ).attr( "height", "100%" );
+  var numDays = 100;
+  data = _.map( data.slice( 0, numDays ).reverse(), function ( stat ) {
+    return stat[1] || 0;
+  } );
+  var x = d3.scale.linear().domain( [0, data.length] ).range( [0, $( "#" + element ).width()] );
+  var y = d3.scale.linear().domain( [0, _.max( data )] ).range( [$( "#" + element ).height(), 0] );
   var line = d3.svg.line()
-    .x(function(d,i) {
-      return x(i);
-    })
-    .y(function(d) {
-      return y(d);
-    });
-  graph.append("svg:path").attr("d", line(data));
+    .x( function ( d, i ) {
+      return x( i );
+    } )
+    .y( function ( d ) {
+      return y( d );
+    } );
+  graph.append( "svg:path" ).attr( "d", line( data ) );
 };
-
