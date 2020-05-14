f49d7cb67f0beb74a760bd4042d74b0c431a8406
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2020-05-14 12:53:12 -0700

    Added tools to donorbox scripts to answer questions about API responses

--- tools/donorbox-plans.rb
+++ tools/donorbox-plans.rb
@@ -1,3 +1,22 @@
+require "rubygems"
+require "optimist"
+
+opts = Optimist::options do
+    banner <<-EOS
+Sync donorbox users with iNat User and UserParent records
+
+Usage:
+
+  rails runner tools/calflora_observation_links.rb
+
+where [options] are:
+EOS
+  opt :debug, "Print debug statements", type: :boolean, short: "-d"
+  opt :dry, "Dry run, don't actually change anything", type: :boolean
+  opt :email, "Filter results by Donorbox donor email", type: :string
+  opt :donor_id, "Filter results by Donorbox donor ID", type: :string
+end
+
 start = Time.now
 if !CONFIG.donorbox || !CONFIG.donorbox.email || !CONFIG.donorbox.key
   raise "Donorbox email an API key haven't been added to config"
@@ -6,14 +25,13 @@ donorbox_email = CONFIG.donorbox.email
 donorbox_key = CONFIG.donorbox.key
 page = 1
 per_page = 100
-debug = true
 num_plans = 0
 num_updated_users = 0
 num_invalid_users = 0
 active_user_ids = []
 while true
   url = "https://donorbox.org/api/v1/plans?page=#{page}&per_page=#{per_page}"
-  puts url if debug
+  puts url if opts.debug
   response = RestClient.get( url, {
     "Authorization" => "Basic #{Base64.strict_encode64( "#{donorbox_email}:#{donorbox_key}" ).strip}",
     "User-Agent" => "iNaturalist/Donorbox"
@@ -23,7 +41,16 @@ while true
   json.each do |plan|
     num_plans += 1
     next unless donor = plan["donor"]
+    if opts.donor_id && opts.donor_id.to_i != donor["id"].to_i
+      next
+    end
+    if opts.email && donor["email"] !~ /#{opts.email}/i
+      next
+    end
     puts "Donor #{donor["id"]}"
+    if opts.debug
+      puts plan
+    end
     unless user = User.find_by_email( donor["email"] )
       puts "\tNo user"
       next
@@ -50,7 +77,8 @@ while true
       if user.donorbox_plan_status_changed? && user.donorbox_plan_status != "active"
         user.prefers_monthly_supporter_badge = false
       end
-      if user.save
+      user_updated = opts.dry || user.save
+      if user_updated
         puts "\tUpdated #{user}"
         num_updated_users += 1
       else
