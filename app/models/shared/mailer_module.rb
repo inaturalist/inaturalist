07700c22019802aa9c71027670a32bef4783cafa
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2020-06-16 16:19:51 -0700

    Bugfix: mail sends mail, message is the message sent

--- app/models/shared/mailer_module.rb
+++ app/models/shared/mailer_module.rb
@@ -5,9 +5,10 @@ module Shared::MailerModule
 
   private
   def set_sendgrid_headers
+    return if message.blank? || message.to.blank?
     mailer = self.class.name
     headers "X-SMTPAPI" => {
-      to: mail.to,
+      to: message.to,
       category:    [ mailer, "#{mailer}##{action_name}" ],
       unique_args: { environment: Rails.env }
     }.merge( @x_smtpapi_headers || {} ).to_json
