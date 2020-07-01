66f23d417c78eb4a9e20f56b0888f87a2435c2bd
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2020-07-01 11:37:05 -0700

    Allow opting in to the CID even if you don't have an ID

--- app/webpack/observations/show/components/community_identification.jsx
+++ app/webpack/observations/show/components/community_identification.jsx
@@ -58,7 +58,14 @@ class CommunityIdentification extends React.Component {
   }
 
   communityIDOverridePanel( ) {
-    if ( !( this.userIsObserver && this.ownerID && this.communityIDIsRejected ) ) {
+    // We're not including the owner ID requirement here b/c if you want to
+    // opt-in to the CID, that shouldn't require an opinion of your own, e.g.
+    // you opted out, withdrew your ID, and now you want to cede to the
+    // community again
+    if ( !(
+      this.userIsObserver
+      && this.communityIDIsRejected
+    ) ) {
       return ( <div /> );
     }
     return (
@@ -120,6 +127,9 @@ class CommunityIdentification extends React.Component {
     // must be observer, IDer, must not have opted out already
     if ( !(
       this.userIsObserver
+      // The observer must have an ID b/c we don't want people rejecting the CID
+      // to make the obs not associated with *any* taxon. If you want to reject
+      // it, you need to have an opinion.
       && this.ownerID
       && observation.taxon
       && !this.observationOptedOut
@@ -592,7 +602,7 @@ class CommunityIdentification extends React.Component {
           { I18n.t( "community_id_heading" ) }
           <span className="header-actions pull-right">
             { this.optOutPopover( ) }
-            { loggedIn && !observation.communityTaxon ? (
+            { loggedIn && !observation.communityTaxon && (
               <a
                 href={compareLink}
                 className="linky compare-link"
@@ -605,7 +615,7 @@ class CommunityIdentification extends React.Component {
               >
                 { I18n.t( "compare" ) }
               </a>
-            ) : null }
+            ) }
             <button
               type="button"
               className="btn btn-nostyle linky"
@@ -638,7 +648,9 @@ class CommunityIdentification extends React.Component {
                     type="button"
                     className="btn btn-default"
                   >
-                    <i className="fa fa-exchange" /> { I18n.t( "compare" ) }
+                    <i className="fa fa-exchange" />
+                    { " " }
+                    { I18n.t( "compare" ) }
                   </button>
                 </a>
               </div>
@@ -649,7 +661,9 @@ class CommunityIdentification extends React.Component {
                 className="btn btn-default"
                 onClick={this.showCommunityIDModal}
               >
-                <i className="fa fa-info-circle" /> { I18n.t( "about" ) }
+                <i className="fa fa-info-circle" />
+                { " " }
+                { I18n.t( "about" ) }
               </button>
             </div>
           </div>
