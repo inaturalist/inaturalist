16d1a7950c2aa52d6406ca9f2dd633f47b7683fb
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2020-07-22 13:39:04 -0700

    sites/affiliation for everyone, typo fix

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,7 +2,8 @@ class SitesController < ApplicationController
   before_filter :authenticate_user!, except: %w(network affiliation)
   before_filter :load_site, only: [:show, :edit, :update, :destroy]
   before_filter :admin_or_any_site_admin_required, only: [:index, :show]
-  before_filter :require_admin_of_viewed_site, except: [:index, :show, :network]
+  before_filter :require_admin_of_viewed_site, except: [:index, :show,
+    :network, :affiliation]
   before_filter :setup_pref_groups, only: [:new, :create, :edit, :update, :show]
 
   layout "bootstrap"
