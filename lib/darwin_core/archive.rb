4e3a98a1110f4aac3ff6e9867d5b23126c0fa7d9
Patrick Leary <patrick.r.leary@gmail.com>
2020-08-28 15:45:17 -0400

    query smaller batches when generating dwca; vision export can take array of taxon_ids

--- lib/darwin_core/archive.rb
+++ lib/darwin_core/archive.rb
@@ -547,28 +547,39 @@ module DarwinCore
 
     def observations_in_batches(params, preloads, options = {}, &block)
       batch_times = []
-      Observation.search_in_batches( params, logger: logger ) do |batch|
-        start = Time.now
-        avg_batch_time = if batch_times.size > 0
-          (batch_times.inject{|sum, num| sum + num}.to_f / batch_times.size).round(3)
-        else
-          0
-        end
-        avg_observation_time = avg_batch_time / 500
-        msg = "Observation batch #{batch_times.size}"
-        msg += " for #{options[:label]}" if options[:label]
-        msg += " (avg batch: #{avg_batch_time}s, avg obs: #{avg_observation_time}s)"
-        logger.debug msg
-        try_and_try_again( [PG::ConnectionBad, ActiveRecord::StatementInvalid], logger: logger ) do
-          Observation.preload_associations(batch, preloads)
-        end
-        batch.each do |observation|
-          if @opts[:community_taxon] && observation.community_taxon.blank?
-            next
+      max_id = Observation.maximum( :id )
+      search_chunk_size = 1000000
+      chunk_start_id = 1
+      # initial loop splits queries into batches by setting the search param
+      # `id_below`. This make queries with very large result sets faster overall
+      # whith little overhead for small queries.
+      while chunk_start_id <= max_id
+        params[:min_id] = chunk_start_id
+        params[:id_below] = chunk_start_id + search_chunk_size
+        Observation.search_in_batches( params, logger: logger ) do |batch|
+          start = Time.now
+          avg_batch_time = if batch_times.size > 0
+            (batch_times.inject{|sum, num| sum + num}.to_f / batch_times.size).round(3)
+          else
+            0
           end
-          yield observation
+          avg_observation_time = avg_batch_time / 500
+          msg = "Observation batch #{batch_times.size}"
+          msg += " for #{options[:label]}" if options[:label]
+          msg += " (avg batch: #{avg_batch_time}s, avg obs: #{avg_observation_time}s)"
+          logger.debug msg
+          try_and_try_again( [PG::ConnectionBad, ActiveRecord::StatementInvalid], logger: logger ) do
+            Observation.preload_associations(batch, preloads)
+          end
+          batch.each do |observation|
+            if @opts[:community_taxon] && observation.community_taxon.blank?
+              next
+            end
+            yield observation
+          end
+          batch_times << (Time.now - start)
         end
-        batch_times << (Time.now - start)
+        chunk_start_id += search_chunk_size
       end
     end
 
