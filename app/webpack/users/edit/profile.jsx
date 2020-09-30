4e39051d3ec08399f5ade0c2431eab7a60f6a47c
Amanda Bullington <albullington@gmail.com>
2020-09-29 17:16:54 -0700

    Style buttons for profile picture component

--- app/webpack/users/edit/profile.jsx
+++ app/webpack/users/edit/profile.jsx
@@ -2,7 +2,22 @@ import React from "react";
 
 const Profile = () => (
   <div id="SettingsContainer">
-    <div>Profile Picture</div>
+    <div id="SettingsItem">
+      <text id="ProfileHeader">
+        Profile Picture
+      </text>
+      <div id="row">
+        <image />
+        <div className="centered-column">
+          <button className="blue-button" type="button">
+            <text className="blue-button-text">Upload new photo</text>
+          </button>
+          <button className="gray-button" type="button">
+            <text className="gray-button-text">Remove photo</text>
+          </button>
+        </div>
+      </div>
+    </div>
     <div>Username</div>
     <div>Email</div>
     <div>Change Password</div>
