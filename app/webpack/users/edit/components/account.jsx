d0c3b561d20f3108a40e575d6ba090b53f9ec338
Amanda Bullington <albullington@gmail.com>
2020-10-08 14:42:09 -0700

    Text styling for Account screen

--- app/webpack/users/edit/components/account.jsx
+++ app/webpack/users/edit/components/account.jsx
@@ -14,35 +14,53 @@ const Account = ( { profile, setUserData } ) => {
         <div className="col-md-5 col-xs-10">
           <div className="profile-setting">
             <h5>{I18n.t( "place_geo.geo_planet_place_types.Time_Zone" )}</h5>
-            <div>{I18n.t( "all_your_observations_will_default_this_time_zone" )}</div>
+            <div className="account-subheader-text">{I18n.t( "all_your_observations_will_default_this_time_zone" )}</div>
           </div>
           <div className="profile-setting">
             <h5>{I18n.t( "language_slash_locale" )}</h5>
-            <div>{I18n.t( "language_slash_locale_description" )}</div>
+            <div className="account-subheader-text">{I18n.t( "language_slash_locale_description" )}</div>
           </div>
           <div className="profile-setting">
             <h5>{I18n.t( "default_search_place" )}</h5>
-            <div>{I18n.t( "default_search_place_description" )}</div>
+            <div className="account-subheader-text">{I18n.t( "default_search_place_description" )}</div>
           </div>
           <div className="profile-setting">
             <h5>{I18n.t( "privacy" )}</h5>
-            <div>{I18n.t( "views.users.edit.prefers_no_tracking_label" )}</div>
+            <div className="row">
+              <div className="col-xs-1">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  value={profile ? profile.prefers_no_tracking : ""}
+                  name="prefers_no_tracking"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="col-xs-9">
+                <label>{I18n.t( "views.users.edit.prefers_no_tracking_label" )}</label>
+                <div className="blue-text italic-text">{I18n.t( "learn_about_third_party_tracking" )}</div>
+              </div>
+            </div>
           </div>
           <div className="profile-setting">
             <h5>{I18n.t( "danger_zone" )}</h5>
-            <div>{I18n.t( "delete_your_account" )}</div>
+            <button className="btn gray-button" type="button">
+              <div className="blue-button-text">{I18n.t( "delete_your_account" )}</div>
+            </button>
           </div>
         </div>
         <div className="col-md-1" />
         <div className="col-md-6 col-xs-10">
           <div className="profile-setting">
             <h5>{I18n.t( "inaturalist_network_affiliation" )}</h5>
-            {/* eslint-disable-next-line react/no-danger */}
-            <span dangerouslySetInnerHTML={{
-              __html: I18n.t( "views.users.edit.inaturalist_network_affiliation_desc_html", {
-                url: "https://www.inaturalist.org/sites/network"
-              } )
-            }}
+            <span
+              className="account-subheader-text"
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: I18n.t( "views.users.edit.inaturalist_network_affiliation_desc_html", {
+                  url: "https://www.inaturalist.org/sites/network"
+                } )
+              }}
             />
           </div>
         </div>
