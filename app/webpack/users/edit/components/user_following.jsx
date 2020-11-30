0de3395a2ecc63aba19ad6deccbecbe2228772a3
Amanda Bullington <albullington@gmail.com>
2020-11-30 13:00:15 -0800

    Increase size of profile icon

--- app/webpack/users/edit/components/user_following.jsx
+++ app/webpack/users/edit/components/user_following.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import UserImage from "../../../shared/components/user_image";
 
 const UserFollowing = ( { user } ) => (
-  <div className="flex-no-wrap user-profile-image">
+  <div className="flex-no-wrap relationship-profile-image">
     <UserImage user={user} />
     <div>
       <a href={`/people/${user.login}`}>{user.login}</a>
