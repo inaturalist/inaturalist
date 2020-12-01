d24d92d7945e858f50221170674ead9b296754d9
Amanda Bullington <albullington@gmail.com>
2020-11-30 16:10:00 -0800

    Show save button on left side of screen when using mobile screen size

--- app/webpack/users/edit/components/save_button.jsx
+++ app/webpack/users/edit/components/save_button.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import moment from "moment";
 
 const SaveButton = ( { saveUserSettings, profile } ) => (
-  <div className="flex-no-wrap flex-end">
+  <div className="flex-no-wrap flex-start-xs flex-end">
     <div className={profile.saved_status === "saved" ? "text-muted underline margin-right-medium" : "collapse"}>
       {I18n.t( "saved_at" )}
       {` ${moment( profile.updated_at ).format( "h:mm a" )}`}
