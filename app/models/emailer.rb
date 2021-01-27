48b03902fed312963df75646d5d3a032859f5aa2
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2021-01-27 11:24:24 -0800

    Send curator application email to an address that exists

--- app/models/emailer.rb
+++ app/models/emailer.rb
@@ -250,7 +250,8 @@ class Emailer < ActionMailer::Base
   def curator_application( user, application )
     set_site
     opts = set_site_specific_opts
-    opts[:to] = CONFIG.help_email.to_s.sub( "@", "+curator@" )
+    # Always send this email to iNat staff
+    opts[:to] = Site.default.email_help.to_s.sub( "@", "+curator@" )
     opts[:from] = user.email || opts[:from]
     opts[:subject] = "Curator Application from #{user.login} (#{user.id})"
     @user = user
