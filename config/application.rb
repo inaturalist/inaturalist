aa747fe9cf05fb72de8e459c60fe96db8acf6962
Patrick Leary <patrick.r.leary@gmail.com>
2021-05-11 12:45:29 -0400

    bump assets version

--- config/application.rb
+++ config/application.rb
@@ -51,7 +51,7 @@ module Inaturalist
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '3.0'
+    config.assets.version = '3.1'
 
     # Compile localized CSS:
     config.assets.precompile += ['*.css', '*.js']
