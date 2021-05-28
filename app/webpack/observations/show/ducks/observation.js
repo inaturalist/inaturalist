221e289eb39a7157f7119f25aa224350a9c9207a
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2021-05-27 17:38:05 -0700

    Several changes for v2 obs/show
    * Load full observation when clicking on obs of relatives
    * Include observation field attributes when loading project observation fields
    * Load flags
    * Load prefers_observation_fields_by
    * Fixed broken obs loading when popping from the history stack (back button)

--- app/webpack/observations/show/ducks/observation.js
+++ app/webpack/observations/show/ducks/observation.js
@@ -73,8 +73,13 @@ const PROJECT_FIELDS = {
   },
   icon: true,
   project_observation_fields: {
+    id: true,
     observation_field: {
-      id: true
+      allowed_values: true,
+      datatype: true,
+      description: true,
+      id: true,
+      name: true
     }
   },
   slug: true,
@@ -110,6 +115,9 @@ const FIELDS = {
   description: true,
   faves: {
     user: USER_FIELDS
+  },
+  flags: {
+    id: true
   },
   geojson: true,
   geoprivacy: true,
@@ -212,7 +220,8 @@ const FIELDS = {
     name: true,
     observations_count: true,
     preferences: {
-      prefers_community_taxa: true
+      prefers_community_taxa: true,
+      prefers_observation_fields_by: true
     }
   } ),
   votes: {
@@ -271,6 +280,7 @@ export function windowStateForObservation( observation, state, opts = { } ) {
   const observationState = {
     observation: {
       id: observation.id,
+      uuid: observation.uuid,
       observed_on: observation.observed_on,
       user: {
         login: observation.user.login
