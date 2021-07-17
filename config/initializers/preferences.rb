2fe12d6de3a349e414ed696e851c593a9e6de783
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2021-07-16 17:25:42 -0700

    Restored Preference initializer (closes #3095)

--- config/initializers/preferences.rb
+++ config/initializers/preferences.rb
@@ -1,9 +1,11 @@
-# TODO Rails 5: this is something we actually need, so figure it out
-# class Preference
-#   after_save :touch_owner
+# In Rails 5 this initializer can run before the Preference class is loaded from
+# the gem, so we need to explicitly load it here before mixing in new stuff
+require_dependency "preference"
+class Preference
+  after_save :touch_owner
 
-#   private
-#   def touch_owner
-#     owner.touch
-#   end
-# end
+  private
+  def touch_owner
+    owner.touch
+  end
+end
