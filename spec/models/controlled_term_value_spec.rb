5ec91c266e92fdc2d647799cb7b929230df8d34c
todtb <todtb@users.noreply.github.com>
2021-08-05 21:41:36 -0400

    Add shoulda matchers "c"

--- spec/models/controlled_term_value_spec.rb
+++ spec/models/controlled_term_value_spec.rb
@@ -1,9 +1,7 @@
 require "spec_helper.rb"
 
 describe ControlledTermValue do
-
   it { is_expected.to validate_presence_of :controlled_value_id }
   it { is_expected.to validate_presence_of :controlled_attribute_id }
   it { is_expected.to validate_uniqueness_of(:controlled_value_id).scoped_to(:controlled_attribute_id) }
-
 end
