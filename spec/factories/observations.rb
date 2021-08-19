fdd6f3bb395d3ff758d49c7ea0e6359bed96917a
todtb <todtb@users.noreply.github.com>
2021-08-18 21:33:00 -0400

    Update observation creation specs

--- spec/factories/observations.rb
+++ spec/factories/observations.rb
@@ -1,7 +1,10 @@
 FactoryBot.define do
   factory :observation do
     user
+    taxon
     license { Observation::CC_BY }
     description { Faker::Lorem.sentence }
+    uri { Faker::Internet.url }
+    observed_on_string { 'yesterday at noon' }
   end
 end
