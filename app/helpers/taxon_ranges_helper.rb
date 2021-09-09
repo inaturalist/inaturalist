2f7aaf699ac171cc19757238f8e1ebd24019e737
Scott Loarie <loarie@gmail.com>
2021-09-09 10:02:38 -0700

    minor changes to display

--- app/helpers/taxon_ranges_helper.rb
+++ app/helpers/taxon_ranges_helper.rb
@@ -11,4 +11,27 @@ module TaxonRangesHelper
       t( :unknown )
     end
   end
+
+  def taxon_range_file_display_name( tr )
+    if tr.range_file_size.is_a? Numeric
+      filesize = "(#{ tr.range_file_size / 1000.to_f.round } KB)"
+    else
+      filesize = nil
+    end
+    if tr.range_file_name
+      filename = tr.range_file_name
+    else
+      filename = t( :range_file )
+    end
+    if filesize
+      display_name = [filename, filesize].join(" ")
+    else
+      display_name = filename
+    end
+    if tr.range.url
+      return link_to display_name, tr.range.url
+    else
+      return display_name
+    end
+  end
 end
