5a3245aaba6628020aa5b188533676bc17ede4e4
Patrick Leary <patrick.r.leary@gmail.com>
2021-10-05 10:30:14 -0400

    comment out part of migration

--- db/migrate/20210930182050_modify_place_admin_levels.rb
+++ db/migrate/20210930182050_modify_place_admin_levels.rb
@@ -1,15 +1,17 @@
 class ModifyPlaceAdminLevels < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
   def up
-    max_id = User.maximum( :id )
-    batch_size = 1000
-    batch_count = ( max_id / batch_size )
-    for batch_index in ( 0..batch_count ) do
-      start_id = batch_index * batch_size
-      puts start_id
-      User.where( "id > ? AND id <= ?", start_id, start_id + batch_size )
-        .update_all( "lat_lon_acc_admin_level = lat_lon_acc_admin_level * 10" )
-    end
+    # Removing user updates from migration to run separately
+    #
+    # max_id = User.maximum( :id )
+    # batch_size = 1000
+    # batch_count = ( max_id / batch_size )
+    # for batch_index in ( 0..batch_count ) do
+    #   start_id = batch_index * batch_size
+    #   puts start_id
+    #   User.where( "id > ? AND id <= ?", start_id, start_id + batch_size )
+    #     .update_all( "lat_lon_acc_admin_level = lat_lon_acc_admin_level * 10" )
+    # end
 
     max_id = Place.maximum( :id )
     batch_size = 10000
@@ -23,15 +25,15 @@ class ModifyPlaceAdminLevels < ActiveRecord::Migration[5.2]
   end
 
   def down
-    max_id = User.maximum( :id )
-    batch_size = 1000
-    batch_count = ( max_id / batch_size )
-    for batch_index in ( 0..batch_count ) do
-      start_id = batch_index * batch_size
-      puts start_id
-      User.where( "id > ? AND id <= ?", start_id, start_id + batch_size )
-        .update_all( "lat_lon_acc_admin_level = lat_lon_acc_admin_level / 10" )
-    end
+    # max_id = User.maximum( :id )
+    # batch_size = 1000
+    # batch_count = ( max_id / batch_size )
+    # for batch_index in ( 0..batch_count ) do
+    #   start_id = batch_index * batch_size
+    #   puts start_id
+    #   User.where( "id > ? AND id <= ?", start_id, start_id + batch_size )
+    #     .update_all( "lat_lon_acc_admin_level = lat_lon_acc_admin_level / 10" )
+    # end
 
     max_id = Place.maximum( :id )
     batch_size = 10000
