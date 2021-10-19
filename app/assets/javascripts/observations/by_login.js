inaturalist/inaturalist

commit 415f2a91ecf963f57c36e6460227f59d472c13c9

Move observations/by_login inline js to js file

app/assets/javascripts/observations/by_login.js
@@ -57,3 +57,62 @@ function redoSearchInMapArea() {
   $('#filters input[name=nelng]').val(bounds.getNorthEast().lng());
   $('#submit_filters_button').click();
 }
+
+function editSelected() {
+  actOnSelected("/observations/edit/batch");
+}
+
+function actOnSelected(baseURL, options) {
+  var options = $.extend({}, options);
+  var obs_inputs = $.makeArray($('.observation .control input:checkbox:checked'));
+  var obs_ids = $.map(obs_inputs, function(input) { return $(input).val(); });
+  if (obs_ids.length > 0) {
+    if (options.method == "post") {
+      var token = $( "meta[name=csrf-param]" ).attr( "content" );
+      var form = $('<form method="post" style="display:none"></form>').attr('action', baseURL).append(
+        $('<input type="hidden" name="o">').val(obs_ids.join(',')),
+        $('<input type="hidden" name="authenticity_token">').val( token )
+      );
+      $('body').append(form);
+      $(form).submit();
+    } else {
+      window.location = baseURL + "?o=" + obs_ids.join(',');
+    }
+  } else {
+    alert(I18n.t('you_need_to_select_some_observations_first'));
+  }
+}
+
+function flickrTagger() {
+  if (!confirm(I18n.t('this_will_try_to_add_tags_to_your_flickr'))) return;
+
+  actOnSelected(
+    '/taxa/tag_flickr_photos_from_observations',
+    {method: "post"}
+  );
+}
+
+function deleteSelected() {
+  var obs_inputs = $.makeArray($('.observation .control input:checkbox:checked'));
+  var obs_ids = $.map(obs_inputs, function(input) { return $(input).val(); });
+  if (obs_ids.length > 0) {
+    iNaturalist.restfulDelete("/observations/delete_batch", {
+      plural: true,
+      data: {
+        o: obs_ids.join(',')
+      }, 
+      complete: function() {
+        $.each(obs_inputs, function() {
+          $(this).parents('.observation').fadeOut('normal', function() {
+            $(this).remove()
+          });
+        });
+
+        $('#delete_selected_button').show();
+        $('#delete_selected_button').next('.loading.status').remove();
+      }
+    }, $('#delete_selected_button'));
+  } else {
+    alert(I18n.t('you_need_to_select_some_observations_first'));
+  }
+}

app/views/observations/by_login.html.haml
@@ -16,64 +16,6 @@
       // Reveal columns
       $('.created_at').show();
     });
-
-    function editSelected() {
-      actOnSelected("<%= url_for(:action => 'edit_batch') %>");
-    }
-
-    function actOnSelected(baseURL, options) {
-      var options = $.extend({}, options);
-      var obs_inputs = $.makeArray($('.observation .control input:checkbox:checked'));
-      var obs_ids = $.map(obs_inputs, function(input) { return $(input).val(); });
-      if (obs_ids.length > 0) {
-        if (options.method == "post") {
-          var form = $('<form method="post" style="display:none"></form>').attr('action', baseURL).append(
-            $('<input type="hidden" name="o">').val(obs_ids.join(',')),
-            $('<input type="hidden" name="authenticity_token">').val('<%= form_authenticity_token.html_safe %>')
-          );
-          $('body').append(form);
-          $(form).submit();
-        } else {
-          window.location = baseURL + "?o=" + obs_ids.join(',');
-        }
-      } else {
-        alert('<%=t :you_need_to_select_some_observations_first %>');
-      }
-    }
-
-    function flickrTagger() {
-      if (!confirm('<%=t :this_will_try_to_add_tags_to_your_flickr %>')) return;
-
-      actOnSelected(
-        '<%= url_for :controller => "taxa", :action => "tag_flickr_photos_from_observations" %>',
-        {method: "post"}
-      );
-    }
-
-    function deleteSelected() {
-      var obs_inputs = $.makeArray($('.observation .control input:checkbox:checked'));
-      var obs_ids = $.map(obs_inputs, function(input) { return $(input).val(); });
-      if (obs_ids.length > 0) {
-        iNaturalist.restfulDelete("<%= url_for(:action => 'delete_batch') %>", {
-          plural: true,
-          data: {
-            o: obs_ids.join(',')
-          }, 
-          complete: function() {
-            $.each(obs_inputs, function() {
-              $(this).parents('.observation').fadeOut('normal', function() {
-                $(this).remove()
-              });
-            });
-
-            $('#delete_selected_button').show();
-            $('#delete_selected_button').next('.loading.status').remove();
-          }
-        }, $('#delete_selected_button'));
-      } else {
-        alert('<%=t :you_need_to_select_some_observations_first %>');
-      }
-    }
   = javascript_include_tag 'observations/by_login_bundle', 'map_bundle', 'observations/by_login'
 
 - content_for(:extrahead) do
@@ -130,7 +72,7 @@
 
         = link_to_function t(:flickr_tagger), "flickrTagger()", :id => "flickr_tagger_button", :class => "minor button", :title => t(:add_taxonomic_tags_to_the_fickr_photos)
         - unless @project_users.blank?
-          = link_to_toggle_menu( t(:add_to_project), 
+          - link_to_toggle_menu( t(:add_to_project), 
             :id => "add_to_project_button",
             :class => "minor button", 
             :wrapper => {:class => "inter toggle_menu_list", :style => "padding: 0"} ) do

commit f1f9c4fcdbabd905c1803476d64ca5e2ccaa0400

Fix Edit Observations add to project
And Flickr Tagger
I broke it in commit 415f2a91ecf963f57c36e6460227f59d472c13c9
Issue was that it was sending the param name rather than the token,
so failed authentication on POST

app/assets/javascripts/observations/by_login.js
@@ -68,11 +68,16 @@ function actOnSelected(baseURL, options) {
   var obs_ids = $.map(obs_inputs, function(input) { return $(input).val(); });
   if (obs_ids.length > 0) {
     if (options.method == "post") {
-      var token = $( "meta[name=csrf-param]" ).attr( "content" );
+      const csrfParam = $( "meta[name=csrf-param]" ).attr( "content" );
+      const csrfToken = $( "meta[name=csrf-token]" ).attr( "content" );
       var form = $('<form method="post" style="display:none"></form>').attr('action', baseURL).append(
-        $('<input type="hidden" name="o">').val(obs_ids.join(',')),
-        $('<input type="hidden" name="authenticity_token">').val( token )
+        $('<input type="hidden" name="o">').val(obs_ids.join(','))
       );
+      $( "<input>" ).attr( {
+        type: "hidden",
+        name: csrfParam,
+        value: csrfToken
+      } ).appendTo( form );
       $('body').append(form);
       $(form).submit();
     } else {