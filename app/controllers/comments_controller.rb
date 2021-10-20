7a8121ac1efcf40be5f477a85100fe46db12da8f
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2021-10-20 12:09:08 -0700

    No more curator or site admin; if site admins need to do curator things, they should be curators

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!, :except => [:index], :unless => lambda { authenticated_with_oauth? }
   before_action :load_record, :only => [:show, :edit, :update, :destroy]
   before_action :owner_required, :only => [:edit, :update]
+  before_action :curator_required, only: [:user]
   check_spam only: [:create, :update], instance: :comment
   
   def index
@@ -49,7 +50,6 @@ class CommentsController < ApplicationController
   def user
     @display_user = User.find_by_id( params[:id] ) || User.find_by_login( params[:login] )
     return render_404 unless @display_user
-    return unless curator_or_site_admin_of_user?( @display_user )
 
     @comments = @display_user.comments.order( id: :desc ).page( params[:page] )
   end
