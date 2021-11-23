df7d684613ebb9ef8527fe4b9ba06619871618ab
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2021-11-23 21:38:30 +1300

    date_time_wrapper: fix linting errors
    Also removed unused function pickerState

--- app/webpack/observations/uploader/components/date_time_wrapper.jsx
+++ app/webpack/observations/uploader/components/date_time_wrapper.jsx
@@ -3,15 +3,15 @@ import { Glyphicon } from "react-bootstrap";
 import PropTypes from "prop-types";
 import Datetime from "react-datetime";
 import moment from "moment-timezone";
-import { DATE_ONLY, TIME_WITH_TIMEZONE, DATETIME_WITH_TIMEZONE, DATETIME_WITH_TIMEZONE_OFFSET } from "../models/util";
+import {
+  DATE_ONLY, TIME_WITH_TIMEZONE, DATETIME_WITH_TIMEZONE, DATETIME_WITH_TIMEZONE_OFFSET
+} from "../models/util";
 
 class DateTimeWrapper extends Component {
-
   constructor( props ) {
     super( props );
     this.onClick = this.onClick.bind( this );
     this.onChange = this.onChange.bind( this );
-    this.pickerState = this.pickerState.bind( this );
     this.valid = this.valid.bind( this );
     this.renderInputWithOpenButton = this.renderInputWithOpenButton.bind( this );
     this.toggleCalendar = this.toggleCalendar.bind( this );
@@ -31,12 +31,12 @@ class DateTimeWrapper extends Component {
   onChange( fieldValue ) {
     const { timeZone, dateFormat, timeFormat } = this.props;
     let value = fieldValue;
-    if ( moment.isMoment(value) ) {
+    if ( moment.isMoment( value ) ) {
       const pickedDate = value.toDate();
       if ( pickedDate ) {
         let format = dateFormat || timeFormat;
         if ( dateFormat && timeFormat ) {
-          format = `${ format } ${ timeFormat }`
+          format = `${format} ${timeFormat}`;
         }
         if ( timeZone ) {
           value = value.tz( timeZone ).format( format || DATETIME_WITH_TIMEZONE );
@@ -48,57 +48,63 @@ class DateTimeWrapper extends Component {
     this.props.onChange( value );
   }
 
-  pickerState( ) {
-    if ( this.datetime ) { return this.datetime.state; }
-    return undefined;
-  }
-
   valid( current ) {
-    return this.props.allowFutureDates || current.isBefore( ); //isBefore( ) checks for before the current date/time.
+    // isBefore( ) checks for before the current date/time.
+    return this.props.allowFutureDates || current.isBefore( );
   }
 
   toggleCalendar( openCalendar, closeCalendar ) {
-    this.datetime.isOpen() ? closeCalendar() : openCalendar();
+    if ( this.datetime.isOpen() ) {
+      closeCalendar();
+    } else {
+      openCalendar();
+    }
   }
 
   renderInputWithOpenButton( props, openCalendar, closeCalendar ) {
-    const openButtonOnClick = () => { this.toggleCalendar( openCalendar, closeCalendar ) };
+    const openButtonOnClick = () => { this.toggleCalendar( openCalendar, closeCalendar ); };
     return (
       <div className="input-group date">
-        { this.renderOpenButton("before", openButtonOnClick) }
+        {this.renderOpenButton( "before", openButtonOnClick )}
         <input {...props} />
-        { this.renderOpenButton("after", openButtonOnClick) }
+        {this.renderOpenButton( "after", openButtonOnClick )}
       </div>
     );
   }
 
-  renderOpenButton( position, onClick )
-  {
-    if (position === this.props.openButton) {
+  renderOpenButton( position, onClick ) {
+    if ( position === this.props.openButton ) {
       return (
-        <span className={`input-group-addon ${this.props.openButtonClassName}`} onClick={ onClick }>
+        <span
+          className={`input-group-addon ${this.props.openButtonClassName}`}
+          role="button"
+          onClick={onClick}
+          onKeyPress={onClick}
+          tabIndex="0"
+        >
           <Glyphicon glyph="calendar" />
         </span>
       );
     }
+    return "";
   }
 
   render( ) {
     return (
-      <Datetime 
-        ref={ ref => this.datetime = ref }
+      <Datetime
+        ref={ref => { this.datetime = ref; }}
         key="datetime"
         className="datetime"
-        inputProps={ this.props.inputProps }
-        renderInput={ this.props.openButton ? this.renderInputWithOpenButton : undefined }
-        locale={ I18n.locale }
-        initialValue={ this.props.dateTime }
-        dateFormat={ this.props.dateFormat }
-        timeFormat={ this.props.timeFormat }
-        displayTimeZone={ this.props.timeZone }
-        closeOnSelect={ true }
-        isValidDate={ this.valid }
-        onChange={ this.onChange }
+        inputProps={this.props.inputProps}
+        renderInput={this.props.openButton ? this.renderInputWithOpenButton : undefined}
+        locale={I18n.locale}
+        initialValue={this.props.dateTime}
+        dateFormat={this.props.dateFormat}
+        timeFormat={this.props.timeFormat}
+        displayTimeZone={this.props.timeZone}
+        closeOnSelect
+        isValidDate={this.valid}
+        onChange={this.onChange}
       />
     );
   }
@@ -135,6 +141,6 @@ DateTimeWrapper.defaultProps = {
   dateFormat: DATE_ONLY,
   timeFormat: TIME_WITH_TIMEZONE,
   openButton: false
-}
+};
 
 export default DateTimeWrapper;
