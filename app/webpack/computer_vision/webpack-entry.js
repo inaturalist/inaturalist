2827159ec0bd6117a65414f7ce2f7cfaa42accde
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2022-01-05 05:39:11 +1300

    Fix react-bootstrap-datetimepicker internationalization  (#3342)
    * Set moment locale in webpack-entry
    
    These react entry points each use the datetime wrapper control.
    This needs to have the moment locale initialized before it's created,
    or the days get rendered in the wrong columns to the headers.
    
    Fixes datetime controls in:
    - project create
    - uploader (just the left menu was broken)
    - computer vision demo
    
    Other areas had moment locale initialized on webpack-entry:
    - identify filters
    - observation show (observation fields)
    
    * Fix react-bootstrap-datetimepicker hash to be main
    
    It was pointing to the head of a branch instead of main.
    Thought it best to switch it over in case the branch is ever deleted.
    No functional change expected.
    
    Closes #3276

--- app/webpack/computer_vision/webpack-entry.js
+++ app/webpack/computer_vision/webpack-entry.js
@@ -2,11 +2,14 @@ import "@babel/polyfill";
 import thunkMiddleware from "redux-thunk";
 import { createStore, compose, applyMiddleware, combineReducers } from "redux";
 import React from "react";
+import moment from "moment";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 
 import computerVisionDemoReducer from "./ducks/computer_vision_demo";
 import ComputerVisionDemo from "./containers/computer_vision_demo";
+
+moment.locale( I18n.locale );
 
 const rootReducer = combineReducers( {
   computerVisionDemo: computerVisionDemoReducer
