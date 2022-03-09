57d3902a4812f3715137696e1db8522ecc1598fc
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2022-03-09 15:26:28 -0800

    Added notice about pre-nullifying cols before running migration

--- db/migrate/20220127195113_nullify_users_confirmation_sent_at.rb
+++ db/migrate/20220127195113_nullify_users_confirmation_sent_at.rb
@@ -1,6 +1,14 @@
 class NullifyUsersConfirmationSentAt < ActiveRecord::Migration[6.1]
   def up
-    execute "UPDATE users SET confirmation_token = NULL, confirmation_sent_at = NULL, confirmed_at = NULL"
+    say "You might want to perform this nullification before running this migration if there are a lot of users"
+    execute <<~SQL
+      UPDATE users
+      SET confirmation_token = NULL, confirmation_sent_at = NULL, confirmed_at = NULL
+      WHERE
+        confirmation_token IS NOT NULL
+        OR confirmation_sent_at IS NOT NULL
+        OR confirmed_at IS NOT NULL
+    SQL
   end
 
   def down
