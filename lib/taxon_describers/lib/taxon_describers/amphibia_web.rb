35ac05d60b52098f52cd13310f79e9426a643953
Patrick Leary <patrick.r.leary@gmail.com>
2022-08-22 11:28:21 -0400

    bug fixes

--- lib/taxon_describers/lib/taxon_describers/amphibia_web.rb
+++ lib/taxon_describers/lib/taxon_describers/amphibia_web.rb
@@ -13,7 +13,8 @@ module TaxonDescribers
     def data_for_name(name)
       genus_name, species_name = name.split
       url = "https://amphibiaweb.org/cgi/amphib_ws?where-genus=#{genus_name}&where-species=#{species_name}&src=eol"
-      xml = Nokogiri::XML(open(url))
+      response = Net::HTTP.get_response( URI.parse( url ) )
+      xml = Nokogiri::XML( response.body )
       return nil if xml.blank? || xml.children.blank? || xml.at('error')
       [xml, genus_name, species_name]
     end
