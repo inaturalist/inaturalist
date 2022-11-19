55768890daa4514cf7428255941352161b320973
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2022-11-18 19:00:43 -0800

    Oh yeah, no transpilation here

--- app/assets/javascripts/i18n/default_value.js
+++ app/assets/javascripts/i18n/default_value.js
@@ -1,13 +1,16 @@
+/* global I18n */
+
 // Override default behavior so that defaultValue gets returned even if there
 // is a fallback value (i.e. how it works in the Rails gem)
 ( function ( ) {
-  const originalImplementation = I18n.t;
+  var originalImplementation = I18n.t;
   I18n.t = function ( key, params ) {
     var opts = params || {};
     if (
       opts.defaultValue
       && (
         originalImplementation( key, opts )
+        // eslint-disable-next-line prefer-object-spread
         === originalImplementation( key, Object.assign( {}, opts, { locale: "en" } ) )
       )
     ) {
