73841ce00cf80e64f3764e50a011f25ef772d64a
Ken-ichi <kenichi.ueda@gmail.com>
2022-11-28 13:22:14 -0800

    Support rel=me attribute in profile links (#3568)

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,40 +1,50 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-include ApplicationHelper
+# frozen_string_literal: true
+
+require "#{File.dirname( __FILE__ )}/../spec_helper"
 
 describe ApplicationHelper do
-
   describe "hyperlink_mentions" do
     it "links known user mentions in text" do
-      User.make!(login: "testmention")
+      User.make!( login: "testmention" )
       str = "Hello @testmention!"
-      expect(hyperlink_mentions(str)).to eq(
-        "Hello <a href=\"http://test.host/people/testmention\">@testmention</a>!")
+      expect( hyperlink_mentions( str ) ).to eq(
+        "Hello <a href=\"http://test.host/people/testmention\">@testmention</a>!"
+      )
     end
 
     it "does not link unknown logins" do
       str = "Hello @testmention!"
-      expect(hyperlink_mentions(str)).to eq str
+      expect( hyperlink_mentions( str ) ).to eq str
     end
 
     it "links mentions at the start and end of strings" do
-      User.make!(login: "alpha")
-      User.make!(login: "beta")
+      User.make!( login: "alpha" )
+      User.make!( login: "beta" )
       str = "@alpha, @beta"
-      expect(hyperlink_mentions(str)).to eq(
-        "<a href=\"http://test.host/people/alpha\">@alpha</a>, " +
-        "<a href=\"http://test.host/people/beta\">@beta</a>")
+      expect( hyperlink_mentions( str ) ).to eq(
+        "<a href=\"http://test.host/people/alpha\">@alpha</a>, " \
+          "<a href=\"http://test.host/people/beta\">@beta</a>"
+      )
     end
 
     it "properly links logins that are substrings of each other" do
-      User.make!(login: "alpha")
-      User.make!(login: "alphabeta")
-      User.make!(login: "alphabetagamma")
+      User.make!( login: "alpha" )
+      User.make!( login: "alphabeta" )
+      User.make!( login: "alphabetagamma" )
       str = "Hello @alpha, @alphabeta, @alphabetagamma"
-      expect(hyperlink_mentions(str)).to eq(
-        "Hello <a href=\"http://test.host/people/alpha\">@alpha</a>, " +
-        "<a href=\"http://test.host/people/alphabeta\">@alphabeta</a>, " +
-        "<a href=\"http://test.host/people/alphabetagamma\">@alphabetagamma</a>")
+      expect( hyperlink_mentions( str ) ).to eq(
+        "Hello <a href=\"http://test.host/people/alpha\">@alpha</a>, " \
+          "<a href=\"http://test.host/people/alphabeta\">@alphabeta</a>, " \
+          "<a href=\"http://test.host/people/alphabetagamma\">@alphabetagamma</a>"
+      )
+    end
+
+    it "ignores links that look like mentions" do
+      user = create :user
+      txt = <<~TXT
+        Hey, it's <a rel="me" href="https://foo.bar/@#{user.login}">me</a>
+      TXT
+      expect( hyperlink_mentions( txt ) ).to eq txt
     end
   end
-
 end
