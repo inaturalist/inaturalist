288ff7c941625ce65c8298790fe91600189d7590
Peter Abrahamsen <rainhead@gmail.com>
2022-12-13 22:01:24 -0800

    Ensure our translations override devise's

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 # tell the I18n library where to find your translations
-I18n.load_path += Dir[Rails.root.join( "config", "locales", "*.{rb,yml}" )]
-I18n.load_path += Dir[Rails.root.join( "config", "locales", "extra", "*.{rb,yml}" )]
 I18N_LOCALES = Dir[Rails.root.join( "config", "locales", "*.{rb,yml}" )].map do | p |
   p[%r{/([\w\-]+?)\.yml}, 1]
 end.compact.uniq
