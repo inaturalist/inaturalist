1fd110bcdf80b5444636989d581ae23f33302265
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2022-12-18 20:29:18 -0800

    WIP: not much, but just getting it out

--- app/webpack/stats/year/components/monthly_supporters.jsx
+++ app/webpack/stats/year/components/monthly_supporters.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const UserWithIcon = "../../../observations/show/components/user_with_icon";
+import UserImage from "../../../shared/components/user_image";
+import UserLink from "../../../shared/components/user_link";
 
-const MonthlySupporters = ( { year, data } ) => (
-  <div className="Donors">
-    <h4>
-      <a name="monthly-supporters" href="#monthly-supporters">
-        <span>{I18n.t( "views.stats.year.monthly_supporters" )}</span>
-      </a>
-    </h4>
-    { data.map( user => <UserWithIcon user={user} key={`monthly-supporter-${user.id}`} /> ) }
-  </div>
-);
+const MonthlySupporters = ( { year, data } ) => {
+  console.log( "data: ", data );
+  const slicedData = data.slice( 25 ).map(
+    user => ( { ...user, icon_url: user.icon_url.replace( "staticdev", "static" ) } )
+  );
+  console.log( "slicedData: ", slicedData );
+  return (
+    <div className="Donors">
+      <h4>
+        <a name="monthly-supporters" href="#monthly-supporters">
+          <span>{I18n.t( "views.stats.year.monthly_supporters" )}</span>
+        </a>
+      </h4>
+      { slicedData.map( user => (
+        <div key={`monthly-supporters-user-${user.login}`}>
+          <UserImage user={user} />
+          <UserLink user={user} />
+        </div>
+      ) ) }
+    </div>
+  );
+};
 
 MonthlySupporters.propTypes = {
   year: PropTypes.number,
