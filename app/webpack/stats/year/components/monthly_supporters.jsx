5e7a8f671066c9cecc5e66233d5d64be0d73420e
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2022-12-21 20:56:27 -0500

    Shuffle monthly supporters; adjust breakpoint clips

--- app/webpack/stats/year/components/monthly_supporters.jsx
+++ app/webpack/stats/year/components/monthly_supporters.jsx
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import React from "react";
 import PropTypes from "prop-types";
 
@@ -5,21 +6,33 @@ import UserImage from "../../../shared/components/user_image";
 import UserLink from "../../../shared/components/user_link";
 
 const MonthlySupporters = ( { data } ) => {
-  const slicedData = data.slice( 0, 22 ).map(
+  const slicedData = _.shuffle( data ).slice( 0, 22 ).map(
     user => ( { ...user, icon_url: user.icon_url.replace( "staticdev", "static" ) } )
   );
   return (
     <div className="MonthlySupporters">
       <div className="supporters">
-        { slicedData.map( ( user, i ) => (
-          <div
-            className={`monthly-supporter ${i >= 9 ? "hidden-xs" : ""}`}
-            key={`monthly-supporters-user-${user.login}`}
-          >
-            <UserImage user={user} />
-            <UserLink user={user} useName />
-          </div>
-        ) ) }
+        { slicedData.map( ( user, i ) => {
+          let hiddenClass = "";
+          if ( i >= 18 ) {
+            hiddenClass += " hidden-md";
+          }
+          if ( i >= 16 ) {
+            hiddenClass += " hidden-sm";
+          }
+          if ( i >= 9 ) {
+            hiddenClass += " hidden-xs";
+          }
+          return (
+            <div
+              className={`monthly-supporter ${hiddenClass}`}
+              key={`monthly-supporters-user-${user.login}`}
+            >
+              <UserImage user={user} />
+              <UserLink user={user} useName />
+            </div>
+          );
+        } ) }
       </div>
     </div>
   );
