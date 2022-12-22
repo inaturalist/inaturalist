6ca19d846d6760f147accdd8e49a4fcd30d61fc8
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2022-12-22 11:04:14 -0500

    Added lead-in text; hide supporters if text not translated.

--- app/webpack/stats/year/components/monthly_supporters.jsx
+++ app/webpack/stats/year/components/monthly_supporters.jsx
@@ -6,11 +6,24 @@ import UserImage from "../../../shared/components/user_image";
 import UserLink from "../../../shared/components/user_link";
 
 const MonthlySupporters = ( { data } ) => {
+  // Just don't show anything if the text isn't translated
+  if (
+    !I18n.locale.match( /^en/ )
+    && (
+      I18n.t( "yir_monthly_supporters_random_selection" )
+      === I18n.t( "yir_monthly_supporters_random_selection", { locale: "en" } )
+    )
+  ) {
+    return <span />;
+  }
   const slicedData = _.shuffle( data ).slice( 0, 22 ).map(
     user => ( { ...user, icon_url: user.icon_url.replace( "staticdev", "static" ) } )
   );
   return (
     <div className="MonthlySupporters">
+      <p>
+        { I18n.t( "yir_monthly_supporters_random_selection" ) }
+      </p>
       <div className="supporters">
         { slicedData.map( ( user, i ) => {
           let hiddenClass = "";
