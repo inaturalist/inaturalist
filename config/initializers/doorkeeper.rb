c048f6d285ee6112a018de2b2bb4719aa0eab463
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2022-12-29 15:13:06 -0500

    Bugfix: unconfirmed existing users could not authenticate via the API after sending confirmation

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -15,7 +15,7 @@ Doorkeeper.configure do
       raise INat::Auth::BadUsernamePasswordError unless user.valid_password?( params[:password] )
       raise INat::Auth::SuspendedError if user.suspended?
       raise INat::Auth::ChildWithoutPermissionError if user.child_without_permission?
-      raise INat::Auth::UnconfirmedError if !user.confirmed? && !user.confirmation_sent_at.blank?
+      raise INat::Auth::UnconfirmedError if !user.confirmed? && !user.active_for_authentication?
       user
     elsif defined?( warden )
       warden.authenticate!(auth_options)
