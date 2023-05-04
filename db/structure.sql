5163da4febbfdbdb0a53c5a044ebd454b8fcb1d5
Patrick Leary <patrick.r.leary@gmail.com>
2023-05-04 11:54:23 -0400

    add and modify some indices

--- db/structure.sql
+++ db/structure.sql
@@ -7295,6 +7295,13 @@ CREATE INDEX index_annotations_on_controlled_value_id ON public.annotations USIN
 
 
 --
+-- Name: index_annotations_on_observation_field_value_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_annotations_on_observation_field_value_id ON public.annotations USING btree (observation_field_value_id);
+
+
+--
 -- Name: index_annotations_on_resource_id_and_resource_type; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -7456,10 +7463,17 @@ CREATE INDEX index_colors_taxa_on_taxon_id_and_color_id ON public.colors_taxa US
 
 
 --
--- Name: index_comments_on_parent_type_and_parent_id; Type: INDEX; Schema: public; Owner: -
+-- Name: index_comments_on_parent_id_and_parent_type; Type: INDEX; Schema: public; Owner: -
 --
 
-CREATE INDEX index_comments_on_parent_type_and_parent_id ON public.comments USING btree (parent_type, parent_id);
+CREATE INDEX index_comments_on_parent_id_and_parent_type ON public.comments USING btree (parent_id, parent_type);
+
+
+--
+-- Name: index_comments_on_parent_type; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_comments_on_parent_type ON public.comments USING btree (parent_type);
 
 
 --
@@ -8132,6 +8146,13 @@ CREATE INDEX index_lists_on_user_id ON public.lists USING btree (user_id);
 --
 
 CREATE INDEX index_messages_on_user_id_and_from_user_id ON public.messages USING btree (user_id, from_user_id);
+
+
+--
+-- Name: index_messages_on_user_id_and_thread_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_messages_on_user_id_and_thread_id ON public.messages USING btree (user_id, thread_id);
 
 
 --
@@ -9171,6 +9192,13 @@ CREATE INDEX index_subscriptions_on_user_id ON public.subscriptions USING btree 
 
 
 --
+-- Name: index_subscriptions_on_user_id_and_resource_type; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_subscriptions_on_user_id_and_resource_type ON public.subscriptions USING btree (user_id, resource_type);
+
+
+--
 -- Name: index_taggings_on_taggable_id_and_taggable_type_and_context; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -9626,6 +9654,13 @@ CREATE INDEX index_users_on_place_id ON public.users USING btree (place_id);
 
 
 --
+-- Name: index_users_on_reset_password_token; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_users_on_reset_password_token ON public.users USING btree (reset_password_token);
+
+
+--
 -- Name: index_users_on_site_id; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -9644,6 +9679,13 @@ CREATE INDEX index_users_on_spammer ON public.users USING btree (spammer);
 --
 
 CREATE INDEX index_users_on_state ON public.users USING btree (state);
+
+
+--
+-- Name: index_users_on_unconfirmed_email; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_users_on_unconfirmed_email ON public.users USING btree (unconfirmed_email);
 
 
 --
@@ -10253,6 +10295,12 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20220407173712'),
 ('20221214192739'),
 ('20221219015021'),
-('20230224230316');
+('20230224230316'),
+('20230504154134'),
+('20230504154207'),
+('20230504154224'),
+('20230504154236'),
+('20230504154248'),
+('20230504154302');
 
 
