0274b305ba51e3751f9251f603fefa59ad1cf340
sylvain-morin <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>
2023-06-27 18:41:02 +0200

    Migrate photos between buckets based on license

--- tools/migrate_photo_to_odp.rb
+++ tools/migrate_photo_to_odp.rb
@@ -1,20 +1,23 @@
-# Migrate all photos with open licenses (not 0,8,10) from the Static bucket to the Open bucket
+# Migrate all photos with open licenses from the Static bucket to the Open bucket
 
 max_photo_id = LocalPhoto.last.id
 batch_size = 100000
 index = 0
 
+static_bucket_id = FilePrefix.where( prefix: "https://static.inaturalist.org/photos" ).first.id
+
 while index < max_photo_id
-	LocalPhoto.joins("LEFT JOIN flags ON (photos.id = flags.flaggable_id)").
-  where("flags.id IS NULL").
-  where("photos.file_prefix_id=2")
-  where("photos.license not in (0,8,10)").
-  where("photos.id between ? and ?", index, index + batch_size -1).
-	find_each do |photo|
-	  LocalPhoto.delay(
-	      queue: "photos",
-	      unique_hash: { "LocalPhoto::migrate_open_photo_to_odp_bucket": photo.id }
-	    ).change_photo_bucket_if_needed( photo )
-	end
-	index = index + batch_size
+  puts "Starting batch #{index}"
+  LocalPhoto.joins( "LEFT JOIN flags ON (photos.id = flags.flaggable_id)" ).
+  where( "flags.id IS NULL" ).
+  where( "photos.file_prefix_id=?", static_bucket_id ).
+  where( "photos.license not in (?)", Shared::LicenseModule::LICENSE_NUMBERS - Shared::LicenseModule::ODP_LICENSES).
+  where( "photos.id between ? and ?", index, index + batch_size - 1 ).
+  each do |photo|
+    LocalPhoto.delay(
+      queue: "photos",
+      unique_hash: { "LocalPhoto::change_photo_bucket_if_needed": photo.id }
+    ).change_photo_bucket_if_needed( photo )
+  end
+  index += batch_size
 end
