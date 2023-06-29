fc24fd99f688fe7e6ee558c0039be111db653870
Patrick Leary <patrick.r.leary@gmail.com>
2023-06-29 11:15:58 -0400

    minor cleanup

--- app/webpack/geo_model/index/containers/app_container.js
+++ app/webpack/geo_model/index/containers/app_container.js
@@ -1,6 +1,5 @@
 import { connect } from "react-redux";
 import App from "../components/app";
-// import { setConfig } from "../../../shared/ducks/config";
 import { setOrderBy } from "../ducks/geo_model";
 
 function mapStateToProps( state ) {
