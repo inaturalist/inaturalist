c6dd681eb71f009d8bcbcdea675b5e90d46a0e96
Ken-ichi <kenichi.ueda@gmail.com>
2023-09-07 10:55:40 -0700

    Localize doorkeeper devise auth failure (#3861)
    This handles situations where auth fails before it gets to our application
    code, e.g. when Doorkeeper checks an auth token using Devise. It uses the
    Accept-Language HTTP header, so it isn't dependent on any particular endpoint
    or request body.

---
+++ lib/inat_devise_failure_app.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# Override of the Devise class to localize error responses based on HTTP headers
+class InatDeviseFailureApp < Devise::FailureApp
+  def i18n_options( options )
+    I18n.locale = normalize_locale( request_accept_language )
+    options
+  end
+
+  def request_accept_language
+    if request.respond_to?( :headers ) && request.headers.respond_to?( :[] )
+      return request.headers["Accept-Language"]
+    end
+
+    if request.respond_to? :get_header
+      return request.get_header( "Accept-Language" )
+    end
+
+    request.env["Accept-Language"]
+  end
+end
