e97bfde0cc2a34569ae98c57c6b2809db6d97da6
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2023-09-08 15:44:22 -0700

    Update apple-app-site-association to include applinks from CONFIG

--- app/controllers/apple_app_site_association_controller.rb
+++ app/controllers/apple_app_site_association_controller.rb
@@ -1,16 +1,28 @@
+# frozen_string_literal: true
+
 # https://developer.apple.com/documentation/xcode/supporting-associated-domains
 class AppleAppSiteAssociationController < ApplicationController
   def index
-    unless @site && !@site.preferred_ios_app_webcredentials.blank?
-      render_404
-      return
-    end
-    render :json => {
-      webcredentials: {
-        apps: [
-          @site.preferred_ios_app_webcredentials
-        ]
+    response = {}
+    unless @site.preferred_ios_app_webcredentials.blank?
+      response[:webcredentials] = {
+        apps: @site.preferred_ios_app_webcredentials.to_s.split( "," )
       }
-    }
+    end
+    if ( applinks = CONFIG&.apple&.applinks ) && ( team_id = CONFIG&.apple&.team_id )
+      details = applinks.to_h.map do | bundle_id, paths |
+        {
+          appID: "#{team_id}.#{bundle_id}",
+          paths: paths
+        }
+      end
+      response[:applinks] = {
+        apps: [],
+        details: details
+      }
+    end
+    return render_404 if response.blank?
+
+    render json: response
   end
 end
--- spec/controllers/apple_app_site_association_controller_spec.rb
+++ spec/controllers/apple_app_site_association_controller_spec.rb
@@ -1,14 +1,38 @@
-require File.dirname(__FILE__) + "/../spec_helper"
+# frozen_string_literal: true
+
+require "#{File.dirname( __FILE__ )}/../spec_helper"
 
 describe AppleAppSiteAssociationController do
   it "should 404 if preference not set for site" do
-    s = Site.make!
-    get :index, params: { inat_site_id: s.id }
+    allow( CONFIG ).to receive( :apple ).and_return OpenStruct.new
+    site = Site.make!
+    expect( site.preferred_ios_app_webcredentials ).to be_blank
+    expect( CONFIG.apple.team_id ).to be_blank
+    get :index, params: { inat_site_id: site.id }
     expect( response.status ).to eq 404
   end
+
   it "should return the webcredentials if set for site" do
     s = Site.make!( preferred_ios_app_webcredentials: "foo" )
     get :index, params: { inat_site_id: s.id }
     expect( JSON.parse( response.body )["webcredentials"]["apps"][0] ).to eq "foo"
+  end
+
+  it "should return the applinks if set in config" do
+    allow( CONFIG ).to receive_message_chain( :apple, :team_id ) { "inat-team-ID" }
+    allow( CONFIG ).to receive_message_chain( :apple, :applinks ) do
+      {
+        "org.inaturalist.bundle": [
+          "/path/to/something"
+        ]
+      }
+    end
+    get :index
+    expect(
+      JSON.parse( response.body )["applinks"]["details"][0]["appID"]
+    ).to eq "#{CONFIG.apple.team_id}.#{CONFIG.apple.applinks.keys[0]}"
+    expect(
+      JSON.parse( response.body )["applinks"]["details"][0]["paths"]
+    ).to include CONFIG.apple.applinks.values[0][0]
   end
 end
