5240170dfa177aaa0892e1c26160982c419b323a
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2023-09-13 14:42:06 -0700

    Support more applinks properties

--- app/controllers/apple_app_site_association_controller.rb
+++ app/controllers/apple_app_site_association_controller.rb
@@ -9,33 +9,46 @@ class AppleAppSiteAssociationController < ApplicationController
         apps: @site.preferred_ios_app_webcredentials.to_s.split( "," )
       }
     end
-    if ( applinks = CONFIG&.apple&.applinks2 ) && ( team_id = CONFIG&.apple&.team_id )
-      details = applinks.map do | applink |
+    unless ( team_id = CONFIG&.apple&.team_id )
+      return render_404 if response.blank?
+
+      return render json: response
+    end
+
+    applinks_details = []
+
+    # Newer-style way of specifying paths
+    if ( applinks = CONFIG&.apple&.applinks2 )
+      applinks_details += applinks.map do | applink |
         {
           appIDs: applink["bundle_ids"].map {| bundle_id | "#{team_id}.#{bundle_id}" },
-          components: applink["components"].map do | component |
-            {
-              "/": component["path"],
-              exclude: component["exclude"].yesish?,
-              comment: component["comment"]
-            }
+          components: applink["components"].map do | config_component |
+            component = { "/": config_component["path"] }
+            component["?"] = config_component["query"].as_json unless config_component["query"].blank?
+            component["#"] = config_component["fragment"].as_json unless config_component["fragment"].blank?
+            component[:exclude] = true if config_component["exclude"].yesish?
+            component[:comment] = config_component["comment"] unless config_component["comment"].blank?
+            component
           end
         }
       end
-      response[:applinks] = { details: details }
-    elsif ( applinks = CONFIG&.apple&.applinks ) && ( team_id = CONFIG&.apple&.team_id )
-      details = applinks.to_h.map do | bundle_id, paths |
+    end
+
+    # Older-style way of specifying paths, maybe totally obsolete
+    if ( applinks = CONFIG&.apple&.applinks )
+      applinks_details += applinks.to_h.map do | bundle_id, paths |
         {
           appID: "#{team_id}.#{bundle_id}",
           paths: paths
         }
       end
-      response[:applinks] = {
-        apps: [],
-        details: details
-      }
     end
-    return render_404 if response.blank?
+    return render_404 if applinks_details.blank?
+
+    response[:applinks] = {
+      apps: [],
+      details: applinks_details
+    }
 
     render json: response
   end
