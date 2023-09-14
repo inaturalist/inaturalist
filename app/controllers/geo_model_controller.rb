9b2b568642a675e6db6e18f594d701027ab80e2f
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2023-09-14 16:02:35 -0700

    Added some translation context for geomodel strings

--- app/controllers/geo_model_controller.rb
+++ app/controllers/geo_model_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class GeoModelController < ApplicationController
   before_action :authenticate_user!
   before_action :admin_required
@@ -31,7 +33,7 @@ class GeoModelController < ApplicationController
         geo_model_taxa = GeoModelTaxon.
           joins( :taxon ).includes( :taxon ).limit( per_page )
         geo_model_taxa = geo_model_taxa.order( "#{sort_column} #{sort_order} NULLS LAST" )
-        render json: geo_model_taxa.map{ |gmt|
+        render json: geo_model_taxa.map {| gmt |
           {
             taxon_id: gmt.taxon_id,
             prauc: gmt.prauc,
@@ -51,23 +53,26 @@ class GeoModelController < ApplicationController
     @asynchronous_google_maps_loading = true
     return render_404 unless @taxon
     return render_404 unless @taxon.geo_model_taxon
-    site_place = @site && @site.place
-    user_place = current_user && current_user.place
+
+    site_place = @site&.place
+    user_place = current_user&.place
     preferred_place = user_place || site_place
 
     @raw_env_data = INatAPIService.get( "/computervision/taxon_h3_cells/#{@taxon.id}",
       { authenticate: current_user }, json: true )
     raise unless @raw_env_data
 
-    taxon_range_data_path = File.join(
+    taxon_range_data_path = CONFIG&.geo_model_data_path && File.join(
       CONFIG.geo_model_data_path, "taxon_range_csvs/#{@taxon.id}.csv"
     )
-    @taxon_range_data = File.exist?( taxon_range_data_path ) ?
-      Hash[File.readlines( taxon_range_data_path, chomp: true ).map{ |v| [v, 1]}] : { }
-    api_url = "/taxa/#{@taxon.id}?preferred_place_id=#{preferred_place.try(:id)}&locale=#{I18n.locale}"
+    @taxon_range_data = if taxon_range_data_path && File.exist?( taxon_range_data_path )
+      File.readlines( taxon_range_data_path, chomp: true ).map {| v | [v, 1] }.to_h
+    else
+      {}
+    end
+    api_url = "/taxa/#{@taxon.id}?preferred_place_id=#{preferred_place.try( :id )}&locale=#{I18n.locale}"
     @node_taxon_json = INatAPIService.get_json( api_url )
     @geo_model_taxon = GeoModelTaxon.where( taxon_id: @taxon ).first
     render layout: "bootstrap", action: "explain"
   end
-
 end
