a5fe0177cf05f3ba63faba95bc74da0c9724de3a
thatkaiguy <kai438@gmail.com>
2023-10-11 16:12:54 -0700

    Configure vcr to allow http requests when no cassette exists

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -224,6 +224,7 @@ LocalPhoto.attachment_definitions[:file].tap do | d |
 end
 
 VCR.configure do |config|
+  config.allow_http_connections_when_no_cassette = true
   config.cassette_library_dir = "fixtures/vcr_cassettes"
   config.hook_into(:webmock)
   config.ignore_localhost = true
