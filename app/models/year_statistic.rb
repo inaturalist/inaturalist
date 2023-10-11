f149d920d3e71ae63789852fdc2db6a4f41a9662
thatkaiguy <kai438@gmail.com>
2023-10-11 13:18:38 -0700

    Use official gbif literature search api

--- app/models/year_statistic.rb
+++ app/models/year_statistic.rb
@@ -877,6 +877,71 @@ class YearStatistic < ApplicationRecord
     if options[:debug]
       puts "[#{Time.now}] publications, year: #{year}, options: #{options}"
     end
+
+    gbif_endpoint = "https://api.gbif.org/v1/literature/search"
+    gbif_params = {
+      # iNat RG Observations dataset identifier on GBIF We don't publish site-
+      # specific datasets, so there's no reason not to hard-code it here.
+      gbifDatasetKey: "50c9509d-22c7-4a22-a47d-8c48425ef4a7",
+      literatureType: "journal",
+      year: year,
+      limit: 50
+    }
+    gbif_url = "#{gbif_endpoint}?#{gbif_params.to_query}"
+    response = JSON.parse( RestClient.get( gbif_url ) )
+
+    new_results = []
+    response["results"].each do | result |
+      altmetric_score = nil
+
+      if ( doi = result.dig( "identifiers", "doi" ) )
+        url = "https://api.altmetric.com/v1/doi/#{doi}"
+        begin
+          am_response = RestClient.get( url )
+          altmetric_score = JSON.parse( am_response )["score"]
+        rescue RestClient::NotFound => e
+          Rails.logger.debug "[DEBUG] Request failed for #{url}: #{e}"
+        end
+        sleep( 1 )
+      end
+
+      new_result = result.slice(
+        "title",
+        "authors",
+        "year",
+        "source",
+        "websites",
+        "publisher",
+        "id",
+        "identifiers"
+      )
+
+      new_result["altmetric_score"] = altmetric_score
+
+      gbif_dois = result["tags"].select do | tag |
+        tag.start_with?( "gbifDOI:" )
+      end
+      gbif_dois = gbif_dois.take( 10 )
+      new_result["_gbifDOIs"] = gbif_dois.map {| tag | tag.delete_prefix( "gbifDOI:" ) }
+
+      if new_result["authors"].size == 1 && new_result["authors"][0]["lastName"] =~ /doesn't match/
+        new_result["authors"] = []
+      end
+
+      new_results << new_result
+    end
+
+    {
+      results: new_results.sort_by {| result | result["altmetric_score"].to_f * -1 }[0..5],
+      url: gbif_url,
+      count: response["count"]
+    }
+  end
+
+  def self.publications_old( year, options = {} )
+    if options[:debug]
+      puts "[#{Time.now}] publications, year: #{year}, options: #{options}"
+    end
     # TODO: replace this with https://www.gbif.org/developer/literature
     gbif_endpont = "https://www.gbif.org/api/resource/search"
     gbif_params = {
--- spec/models/year_statistic_spec.rb
+++ spec/models/year_statistic_spec.rb
@@ -1,7 +1,29 @@
-require "spec_helper.rb"
+# frozen_string_literal: true
+
+require "spec_helper"
 
 describe YearStatistic do
   it { is_expected.to belong_to :user }
   it { is_expected.to belong_to :site }
   it { is_expected.to have_many( :year_statistic_localized_shareable_images ) }
+
+  describe ".publications" do
+    it "should match the YearStatistic.publications method" do
+      publications = YearStatistic.publications( 2022 )
+      old_publications = YearStatistic.publications_old( 2022 )
+
+      publications[:results].each.with_index do | result, i |
+        expect( result.except( "_gbifDOIs", "title" ) ).to eq(
+          old_publications[:results][i].except( "_gbifDOIs", "title" )
+        )
+        expect( result["_gbifDOIs"] ).to eq(
+          old_publications[:results][i]["_gbifDOIs"].map {| doi | doi.delete_prefix( "doi:" ) }
+        )
+      end
+
+      expect( publications[:count] ).to eq( old_publications[:count] )
+
+      expect( publications[:url] ).not_to eq( old_publications[:url] )
+    end
+  end
 end
