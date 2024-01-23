9aeb281c9715a1049dc7791ca976d25f28ab4d65
loarie <loarie@gmail.com>
2024-01-23 01:54:25 -0800

    bugfix

--- app/controllers/observation_quality_experiments_controller.rb
+++ app/controllers/observation_quality_experiments_controller.rb
@@ -6,7 +6,7 @@ class ObservationAccuracyExperimentsController < ApplicationController
   def get_more_validators
     experiment_id = params[:experimentId]
     @experiment = ObservationAccuracyExperiment.find( experiment_id )
-    @more_validators = get_validator_names( limit: nil, offset: 20 )
+    @more_validators = @experiment.get_validator_names( limit: nil, offset: 20 )
     respond_to do | format |
       format.html { render partial: "additional_validators", locals: { validators: @more_validators } }
     end
@@ -15,7 +15,7 @@ class ObservationAccuracyExperimentsController < ApplicationController
   def show
     @experiment = ObservationAccuracyExperiment.find( params[:id] )
     keys = ["quality_grade", "continent", "year", "iconic_taxon_name", "taxon_observations_count", "taxon_rank_level"]
-    @validators = get_validator_names( limit: 20, offset: 0 )
+    @validators = @experiment.get_validator_names( limit: 20, offset: 0 )
     @stats = @experiment.get_top_level_stats
     @data = {}
     keys.each do | key |
