78077dd729dbb25dff7f87f716ab2e436203bc75
loarie <loarie@gmail.com>
2024-01-24 11:53:39 -0800

    minot changes

--- app/controllers/observation_quality_experiments_controller.rb
+++ app/controllers/observation_quality_experiments_controller.rb
@@ -16,32 +16,43 @@ class ObservationAccuracyExperimentsController < ApplicationController
     @experiment = ObservationAccuracyExperiment.find( params[:id] )
     @explorable = ( @experiment.validator_deadline_date > Time.now ) ||
       ( logged_in? && ( current_user.is_admin? || is_curator_or_site_admin ) )
+
     @validators = @experiment.get_validator_names( limit: 20, offset: 0 )
+
     @tab = params[:tab] || "research_grade_results"
     @stats, @data, @precision_data = get_data_for_tab unless @tab == "methods"
+
     if @tab == "methods"
       @candidate_validators = @experiment.observation_accuracy_validators.count
+
       @mean_validators_per_sample = @experiment.observation_accuracy_samples.
-        map( &:reviewers ).sum / @experiment.observation_accuracy_samples.count
-      @mean_validator_count = @experiment.observation_accuracy_samples.includes( :observation_accuracy_validators ).
-        average( "observation_accuracy_validators.id" )
-      @mean_sample_count = @experiment.observation_accuracy_validators.includes( :observation_accuracy_samples ).
-        average( "observation_accuracy_samples.id" )
+        average( :reviewers )
+
       grouped_observation_ids = @experiment.observation_accuracy_samples.
         group( :reviewers ).pluck( :reviewers, "ARRAY_AGG(observation_id)" )
-      @validators_per_sample = { "0": [], "1": [], "2-5": [], ">5": [] }
+      @validators_per_sample = { "0": [], "1": [], "2": [], "3-4": [], ">4": [] }
       grouped_observation_ids.each do | reviewers, observation_ids |
         case reviewers
         when 0
           @validators_per_sample[:"0"] = observation_ids
         when 1
           @validators_per_sample[:"1"] = observation_ids
-        when 2..5
-          @validators_per_sample[:"2-5"] += observation_ids
+        when 2
+          @validators_per_sample[:"2"] = observation_ids
+        when 3..4
+          @validators_per_sample[:"3-4"] += observation_ids
         else
-          @validators_per_sample[:">5"] += observation_ids
+          @validators_per_sample[:">4"] += observation_ids
         end
       end
+
+      samples_by_validators = @experiment.observation_accuracy_validators.joins( :observation_accuracy_samples ).
+        group( "observation_accuracy_validators.id" ).count
+      @mean_validator_count = samples_by_validators.values.sum / samples_by_validators.count.to_f
+
+      validators_by_samples = @experiment.observation_accuracy_samples.joins( :observation_accuracy_validators ).
+        group( "observation_accuracy_samples.id" ).count
+      @mean_sample_count = validators_by_samples.values.sum / validators_by_samples.count.to_f
     end
     render "show"
   end
