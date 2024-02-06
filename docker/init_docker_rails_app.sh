571dc5ac55f418c4b49066d2315712ca9f7518f3
sylvain-morin <sylvain.morin@inaturalist.org>
2024-02-06 17:07:29 +0100

    Precompile assets in Docker image

--- docker/init_docker_rails_app.sh
+++ docker/init_docker_rails_app.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-if [ ! -f "/code/app/assets/javascripts/i18n/translations/en.js" ]; then
-	rake inaturalist:generate_translations_js
-fi
+rake inaturalist:generate_translations_js
 
 rake assets:precompile
