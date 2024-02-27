2d7fe1b63f1c1d915275c4538be833fa0331c9cc
sylvain-morin <sylvain.morin@inaturalist.org>
2024-02-27 18:36:03 +0100

    Configure docker image for k8s

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,5 +55,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.hosts << "host.docker.internal"
 end
 
