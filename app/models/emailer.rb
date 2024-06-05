92af157a9e4b4840bf4032c14785c09944f8b85a
loarie <loarie@gmail.com>
2024-06-05 16:08:55 -0700

    bugfix

--- app/models/emailer.rb
+++ app/models/emailer.rb
@@ -373,7 +373,7 @@ class Emailer < ActionMailer::Base
 
     latitude = observation.latitude || observation.private_latitude || nil
     longitude = observation.longitude || observation.private_longitude || nil
-    @search_params = { radius: 50, verifiable: true, lat: latitude, lng: longitude,
+    @search_params = { radius: 50, quality_grade: "research", lat: latitude, lng: longitude,
                        d1: 1.week.ago.to_s, d2: Time.now, subview: "grid" }
     follower_ids = Observation.
       elastic_user_observation_counts( Observation.params_to_elastic_query( @search_params ), 4 )[:counts]
