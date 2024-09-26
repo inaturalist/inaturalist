98dd80c05b720248dac6e200f946dc6b14189c4c
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2024-09-26 11:23:11 -0700

    typo

--- app/models/taxon_description.rb
+++ app/models/taxon_description.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TaxonDescription < ApplicationRecord
-  # locale can potential have a region, since the different localized
-  # wikipedias do support regional "variants"
+  # locale can potentially have a region, since the different localized
+  # wikipedias support regional "variants"
   belongs_to :taxon
 end
