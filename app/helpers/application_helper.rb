52dd2d841e64a3ffd0ccc3fd0ec801ffe9fae6ae
sylvain-morin <sylvain.morin@inaturalist.org>
2024-10-23 18:50:59 +0200

    Matomo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1727,10 +1727,10 @@ module ApplicationHelper
       _paq.push(['enableLinkTracking']);
       (function() {
         var u="#{CONFIG.matomo_url}";
-        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setTrackerUrl', u+'/matomo.php']);
         _paq.push(['setSiteId', '1']);
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        g.async=true; g.src=u+'/matomo.js'; s.parentNode.insertBefore(g,s);
       })();
       </script>
       <!-- End Matomo Code -->
