3b3abea386909a7429c5b489d236c9126ed80157
Patrick Leary <patrick.r.leary@gmail.com>
2024-11-11 17:06:32 -0500

    Paperclip path versioning (#3957)
    * add paperclip path versioning module and implement for YIR, users, and projects
    
    * catch some potential paperclip versioning configuration errors; update project cover versioned path
    
    * add more descriptive exception text
    
    * fix specs

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -25,10 +25,39 @@ Paperclip.interpolates( "root_url" ) do | _attachment, _style |
   UrlHelper.root_url.chomp( "/" )
 end
 
-Paperclip.interpolates( "locale" ) do | attachment, _style |
-  attachment&.instance&.locale || ":locale"
-end
-
 Paperclip::UploadedFileAdapter.content_type_detector = InatContentTypeDetector
 Paperclip::UriAdapter.register
 Paperclip::DataUriAdapter.register
+
+module Paperclip
+  module Interpolations
+    # Perform the actual interpolation. Takes the pattern to interpolate
+    # and the arguments to pass, which are the attachment and style name.
+    # You can pass a method name on your record as a symbol, which should turn
+    # an interpolation pattern for Paperclip to use.
+    def self.interpolate( pattern, *args )
+      pattern = args.first.instance.send( pattern ) if pattern.is_a? Symbol
+      result = pattern.dup
+      interpolators_cache.each do | method, token |
+        result.gsub!( token ) { send( method, *args ) } if result.include?( token )
+      end
+
+      # START OF MODIFICATION
+      #
+      # for any symbol at least two characters, starting and ending with a letter,
+      # and composed of letters or underscopes, if the instance responds to a method
+      # with that name, interpolate the pattern replacing that symbol with the result
+      # of that method. Allows for dynamic interpolation without having to declare
+      # every interpolation pattern individually
+      matches = result.scan( /:([a-z][a-z_]*[a-z])/ )
+      matches.flatten.uniq.each do | token |
+        if args.first.instance.respond_to?( token )
+          result.gsub!( ":#{token}" ) { args.first.instance.send( token ) }
+        end
+      end
+      # END OF MODIFICATION
+
+      result
+    end
+  end
+end
---
+++ lib/paperclip_path_versioning.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+module PaperclipPathVersioning
+  extend ActiveSupport::Concern
+
+  included do
+    def paperclip_versioned_path( attachment_name )
+      attachment_paths_constant = "PAPERCLIP_#{attachment_name.upcase}_PATHS"
+      unless self.class.const_defined?( attachment_paths_constant )
+        raise "`#{self.class.name}` has not implemented `PaperclipPathVersioning` on attachment " \
+          "`#{attachment_name}`. The attachment must configure `paperclip_path_versioning` " \
+          "for attachment `#{attachment_name}`."
+      end
+      attachment_path_versions = self.class.const_get( attachment_paths_constant )
+      path_version = send( "#{attachment_name}_path_version" )
+      versioned_path = attachment_path_versions[path_version]
+      if versioned_path.nil?
+        raise "`#{self.class.name}` implemented `PaperclipPathVersioning` but does not have a " \
+          "path version `#{path_version}` on attachment `#{attachment_name}`. The attachment " \
+          "`#{attachment_name}` needs more paths configured with `paperclip_path_versioning`."
+      end
+      versioned_path
+    end
+
+    class << self
+      # path_patterns should be an array of strings, where the string at each
+      # position is the path_pattern for that attachment version.
+      # e.g. path_patterns[0] is the pattern for version 0 of `attachment_name`,
+      # path_patterns[1] is the pattern for version 1 of `attachment_name`, etc.
+      def paperclip_path_versioning( attachment_name, path_patterns = [] )
+        attachment_updated_at_method = "#{attachment_name}_updated_at"
+        attachment_path_version_method = "#{attachment_name}_path_version"
+        unless column_names.include?( attachment_updated_at_method )
+          raise "`#{name}` cannot implement `PaperclipPathVersioning` on attachment " \
+            "`#{attachment_name}` as it is missing column `#{attachment_updated_at_method}`"
+        end
+        unless column_names.include?( attachment_path_version_method )
+          raise "`#{name}` cannot implement `PaperclipPathVersioning` on attachment " \
+            "`#{attachment_name}` as it is missing column `#{attachment_path_version_method}`"
+        end
+        unless path_patterns.is_a?( Array ) && path_patterns.size >= 2
+          raise "`#{name}` cannot implement `PaperclipPathVersioning` on attachment " \
+            "`#{attachment_name}` as `path_patterns` needs to be an array of at least 2 paths"
+        end
+
+        const_set( "PAPERCLIP_#{attachment_name.upcase}_PATHS", path_patterns )
+
+        before_save -> do
+          attachment_changed_method = "#{attachment_updated_at_method}_changed?"
+          return unless send( attachment_changed_method )
+
+          send( "#{attachment_path_version_method}=", path_patterns.size - 1 )
+        end
+
+        define_method( "#{attachment_name}_version" ) do
+          Digest::MD5.hexdigest( send( attachment_updated_at_method ).to_i.to_s )
+        end
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.include( PaperclipPathVersioning )
