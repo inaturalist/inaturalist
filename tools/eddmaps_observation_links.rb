ae621a02633bd429d6131df6ba54628410a53336
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2024-12-10 17:44:36 -0800

    fix: fetch EDDMapS records and make obs links again

--- tools/eddmaps_observation_links.rb
+++ tools/eddmaps_observation_links.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 require "rubygems"
 require "optimist"
 
-opts = Optimist::options do
-    banner <<-EOS
-Create ObservationLinks for observations that have been integrated into 
-EDDMapS.
+opts = Optimist.options do
+  banner <<~DESC
+    Create ObservationLinks for observations that have been integrated into#{' '}
+    EDDMapS.
 
-Usage:
+    Usage:
 
-  rails runner tools/eddmaps_observation_links.rb
+      rails runner tools/eddmaps_observation_links.rb
 
-where [options] are:
-EOS
+    where [options] are:
+  DESC
   opt :debug, "Print debug statements", type: :boolean, short: "-d"
   opt :log_task_name, "Log with the specified task name", type: :string
 end
@@ -25,30 +27,32 @@ task_logger&.start
 start_time = Time.now
 new_count = 0
 old_count = 0
-delete_count = 0
 href_name = "EDDMapS"
-# according_to = opts.according_to
-reporter = 105332 # identifier for iNat on EDDMapS
-limit = 1000
-start = 0
+reporter = 105_332 # identifier for iNat on EDDMapS
+limit = 500
+page = 1
 
-while true do
-  url = "https://api.bugwood.org/rest/api/occurrence.json?includeonly=objectid,URL&reporter=#{reporter}&draw=1&length=#{limit}&start=#{start}"
+while true
+  # url = "https://api.bugwood.org/rest/api/occurrence.json?includeonly=objectid,URL&reporter=#{reporter}&draw=1&length=#{limit}&start=#{start}"
+  # The above no longer works; there's no new equivalent of the start param, so we might have to do it by date
+  url = "https://api.bugwoodcloud.org/v2/occurrence?" \
+    "reporter=#{reporter}&pagesize=#{limit}&page=#{page}&paging=true&sort=objectid&sortorder=asc"
   puts
   puts url
   puts
-  data = JSON.parse( Net::HTTP.get( URI( url ) ) )
-  records = data['data']
-  break if records.size == 0
+  response = JSON.parse( Net::HTTP.get( URI( url ) ) )
+  records = response["data"]
+  break if records.size.zero?
+
   observation_ids = []
-  records.each do |record|
-    observation_id = record[1].to_s[/\/(\d+)$/, 1]
-    observation = Observation.find_by_id(observation_id)
+  records.each do | record |
+    observation_id = record["url"].to_s[%r{/(\d+)$}, 1]
+    observation = Observation.find_by_id( observation_id )
     if observation.blank?
       puts "\tobservation #{observation_id} doesn't exist, skipping..."
       next
     end
-    href = "https://www.eddmaps.org/distribution/point.cfm?id=#{record[0]}"
+    href = "https://www.eddmaps.org/distribution/point.cfm?id=#{record['objectid']}"
     existing = ObservationLink.where( observation_id: observation_id, href: href ).first
     if existing
       existing.touch unless opts[:debug]
@@ -62,18 +66,18 @@ while true do
       puts "\tCreated #{ol}"
     end
   end
-  if observation_ids.size > 0
+  if observation_ids.size.positive?
     puts "Re-indexing #{observation_ids.size} observations..."
     Observation.elastic_index!( ids: observation_ids, wait_for_index_refresh: true ) unless opts[:debug]
   end
-  start += limit
+  page += 1
 end
 
-delete_scope = ObservationLink.where( href_name: href_name ).where("updated_at < ?", start_time)
+delete_scope = ObservationLink.where( href_name: href_name ).where( "updated_at < ?", start_time )
 delete_count = delete_scope.count
 puts "Deleting #{delete_count} ObservationLinks"
-if !opts[:debug] && delete_count > 0
-  observation_ids = delete_scope.pluck(:observation_id)
+if !opts[:debug] && delete_count.positive?
+  observation_ids = delete_scope.pluck( :observation_id )
   delete_scope.delete_all
   puts "Re-indexing observations with deleted ObservationLinks"
   Observation.elastic_index!( ids: observation_ids, wait_for_index_refresh: true )
