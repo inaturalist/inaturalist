88b63e0edbc1ff9b52adacce98f2ce07db905f2e
sylvain-morin <sylvain.morin@inaturalist.org>
2024-12-20 09:51:48 +0100

    Uncomment EOL tests

--- spec/models/guide_taxon_spec.rb
+++ spec/models/guide_taxon_spec.rb
@@ -116,139 +116,139 @@ describe GuideTaxon do
     end
   end
 
-  # describe "sync_eol" do
-  #   let(:gt) { GuideTaxon.make! }
-  #   elastic_models( Observation )
-  #   before(:all) do
-  #     Site.make!
-  #     eol = EolService.new(:timeout => 30)
-  #     @mflagellum_page ||= eol.page( 47046719,
-  #                                    common_names: true,
-  #                                    images_per_page: 5,
-  #                                    maps_per_page: 5,
-  #                                    texts_per_page: 5,
-  #                                    details: true,
-  #                                    cache_ttl: -1
-  #     )
-  #   end
+  describe "sync_eol" do
+    let(:gt) { GuideTaxon.make! }
+    elastic_models( Observation )
+    before(:all) do
+      Site.make!
+      eol = EolService.new(:timeout => 30)
+      @mflagellum_page ||= eol.page( 47046719,
+                                     common_names: true,
+                                     images_per_page: 5,
+                                     maps_per_page: 5,
+                                     texts_per_page: 5,
+                                     details: true,
+                                     cache_ttl: -1
+      )
+    end
 
-  #   it "should update the display_name" do
-  #     expect( gt.display_name.to_s.downcase ).not_to eq "coachwhip"
-  #     gt.sync_eol(:page => @mflagellum_page, :replace => true)
-  #     expect( gt.display_name.to_s.downcase ).to eq "coachwhip"
-  #   end
+    it "should update the display_name" do
+      expect( gt.display_name.to_s.downcase ).not_to eq "coachwhip"
+      gt.sync_eol(:page => @mflagellum_page, :replace => true)
+      expect( gt.display_name.to_s.downcase ).to eq "coachwhip"
+    end
 
-  #   it "should allow replacement of existing content" do
-  #     gp = GuidePhoto.make!(:guide_taxon => gt)
-  #     gr = GuideRange.make!(:guide_taxon => gt)
-  #     gs = GuideSection.make!(:guide_taxon => gt)
-  #     gt.sync_eol(:page => @mflagellum_page, :replace => true, :photos => true, :ranges => true, :overview => true)
-  #     expect(GuidePhoto.find_by_id(gp.id)).to be_blank
-  #     expect(GuideRange.find_by_id(gr.id)).to be_blank
-  #     expect(GuideSection.find_by_id(gs.id)).to be_blank
-  #   end
+    it "should allow replacement of existing content" do
+      gp = GuidePhoto.make!(:guide_taxon => gt)
+      gr = GuideRange.make!(:guide_taxon => gt)
+      gs = GuideSection.make!(:guide_taxon => gt)
+      gt.sync_eol(:page => @mflagellum_page, :replace => true, :photos => true, :ranges => true, :overview => true)
+      expect(GuidePhoto.find_by_id(gp.id)).to be_blank
+      expect(GuideRange.find_by_id(gr.id)).to be_blank
+      expect(GuideSection.find_by_id(gs.id)).to be_blank
+    end
 
-  #   it "should not replace existing content if not requested" do
-  #     original_name = "foo1"
-  #     gt.update(:display_name => original_name)
-  #     gp = GuidePhoto.make!(:guide_taxon => gt)
-  #     gr = GuideRange.make!(:guide_taxon => gt)
-  #     gs = GuideSection.make!(:guide_taxon => gt)
-  #     gt.sync_eol(:page => @mflagellum_page, :photos => true, :ranges => true, :overview => true)
-  #     gt.reload
-  #     expect(gt.display_name).to eq original_name
-  #     expect(GuidePhoto.find_by_id(gp.id)).not_to be_blank
-  #     expect(GuideRange.find_by_id(gr.id)).not_to be_blank
-  #     expect(GuideSection.find_by_id(gs.id)).not_to be_blank
-  #   end
+    it "should not replace existing content if not requested" do
+      original_name = "foo1"
+      gt.update(:display_name => original_name)
+      gp = GuidePhoto.make!(:guide_taxon => gt)
+      gr = GuideRange.make!(:guide_taxon => gt)
+      gs = GuideSection.make!(:guide_taxon => gt)
+      gt.sync_eol(:page => @mflagellum_page, :photos => true, :ranges => true, :overview => true)
+      gt.reload
+      expect(gt.display_name).to eq original_name
+      expect(GuidePhoto.find_by_id(gp.id)).not_to be_blank
+      expect(GuideRange.find_by_id(gr.id)).not_to be_blank
+      expect(GuideSection.find_by_id(gs.id)).not_to be_blank
+    end
 
-  #   it "should not add content for categories that weren't selected" do
-  #     gt.guide_photos.destroy_all
-  #     gt.guide_sections.destroy_all
-  #     gt.guide_ranges.destroy_all
-  #     expect(gt.guide_photos.count).to eq 0
-  #     expect(gt.guide_sections.count).to eq 0
-  #     expect(gt.guide_ranges.count).to eq 0
-  #     gt.sync_eol(:page => @mflagellum_page, :photos => "0", :ranges => "0", :overview => "0")
-  #     gt.reload
-  #     expect(gt.guide_photos.count).to eq 0
-  #     expect(gt.guide_sections.count).to eq 0
-  #     expect(gt.guide_ranges.count).to eq 0
-  #   end
+    it "should not add content for categories that weren't selected" do
+      gt.guide_photos.destroy_all
+      gt.guide_sections.destroy_all
+      gt.guide_ranges.destroy_all
+      expect(gt.guide_photos.count).to eq 0
+      expect(gt.guide_sections.count).to eq 0
+      expect(gt.guide_ranges.count).to eq 0
+      gt.sync_eol(:page => @mflagellum_page, :photos => "0", :ranges => "0", :overview => "0")
+      gt.reload
+      expect(gt.guide_photos.count).to eq 0
+      expect(gt.guide_sections.count).to eq 0
+      expect(gt.guide_ranges.count).to eq 0
+    end
 
-  #   it "should add at least one secion if overview requested" do
-  #     expect(gt.guide_sections).to be_blank
-  #     gt.sync_eol(:page => @mflagellum_page, :overview => true)
-  #     gt.reload
-  #     expect(gt.guide_sections).not_to be_blank
-  #   end
+    it "should add at least one secion if overview requested" do
+      expect(gt.guide_sections).to be_blank
+      gt.sync_eol(:page => @mflagellum_page, :overview => true)
+      gt.reload
+      expect(gt.guide_sections).not_to be_blank
+    end
 
-  #   # this doesn't pass b/c EOL's page api seems to return a GeneralDescription
-  #   # when you request a Description. Could hack around it, of course, but would
-  #   # rather the EOL API just behaved reasonably
-  #   # it "should add a description section if requested" do
-  #   #   expect(gt.guide_sections).to be_blank
-  #   #   eol = EolService.new(:timeout => 30)
-  #   #   page = eol.page(577775, :subjects => "Description")
-  #   #   gt.sync_eol(:page => page, :subjects => %w(Description))
-  #   #   gt.reload
-  #   #   expect(gt.guide_sections).not_to be_blank
-  #   # end
-  # end
+    # this doesn't pass b/c EOL's page api seems to return a GeneralDescription
+    # when you request a Description. Could hack around it, of course, but would
+    # rather the EOL API just behaved reasonably
+    # it "should add a description section if requested" do
+    #   expect(gt.guide_sections).to be_blank
+    #   eol = EolService.new(:timeout => 30)
+    #   page = eol.page(577775, :subjects => "Description")
+    #   gt.sync_eol(:page => page, :subjects => %w(Description))
+    #   gt.reload
+    #   expect(gt.guide_sections).not_to be_blank
+    # end
+  end
 
-  # describe "sync_eol_photos" do
-  #   let(:gt) { GuideTaxon.make! }
-  #   elastic_models( Observation )
-  #   before(:all) do
-  #     eol = EolService.new(:timeout => 30)
-  #     @mflagellum_page ||= eol.page( 47046719,
-  #                                    common_names: true,
-  #                                    images_per_page: 5,
-  #                                    maps_per_page: 5,
-  #                                    texts_per_page: 5,
-  #                                    details: true,
-  #                                    cache_ttl: -1
-  #     )
-  #   end
+  describe "sync_eol_photos" do
+    let(:gt) { GuideTaxon.make! }
+    elastic_models( Observation )
+    before(:all) do
+      eol = EolService.new(:timeout => 30)
+      @mflagellum_page ||= eol.page( 47046719,
+                                     common_names: true,
+                                     images_per_page: 5,
+                                     maps_per_page: 5,
+                                     texts_per_page: 5,
+                                     details: true,
+                                     cache_ttl: -1
+      )
+    end
 
-  #   it "should add new photos" do
-  #     expect(gt.guide_photos).to be_blank
-  #     gt.sync_eol_photos(:page => @mflagellum_page)
-  #     gt.save!
-  #     gt.reload
-  #     expect(gt.guide_photos).not_to be_blank
-  #   end
+    it "should add new photos" do
+      expect(gt.guide_photos).to be_blank
+      gt.sync_eol_photos(:page => @mflagellum_page)
+      gt.save!
+      gt.reload
+      expect(gt.guide_photos).not_to be_blank
+    end
 
-  #   it "should not add duplicate photos" do
-  #     gt.sync_eol_photos(:page => @mflagellum_page)
-  #     gt.save!
-  #     s1 = gt.guide_photos.size
-  #     gt.sync_eol_photos(:page => @mflagellum_page)
-  #     gt.save!
-  #     gt.reload
-  #     s2 = gt.guide_photos.size
-  #     expect(s2).to eq s1
-  #   end
+    it "should not add duplicate photos" do
+      gt.sync_eol_photos(:page => @mflagellum_page)
+      gt.save!
+      s1 = gt.guide_photos.size
+      gt.sync_eol_photos(:page => @mflagellum_page)
+      gt.save!
+      gt.reload
+      s2 = gt.guide_photos.size
+      expect(s2).to eq s1
+    end
 
-  #   it "should position new photos after existing ones" do
-  #     gp = GuidePhoto.make!(:guide_taxon => gt)
-  #     gt.sync_eol_photos(:page => @mflagellum_page)
-  #     gt.save!
-  #     gt.reload
-  #     guide_photos = gt.guide_photos.sort_by(&:position)
-  #     expect(guide_photos.first).to eq gp
-  #     expect(guide_photos.last.position).to be > gp.position
-  #   end
+    it "should position new photos after existing ones" do
+      gp = GuidePhoto.make!(:guide_taxon => gt)
+      gt.sync_eol_photos(:page => @mflagellum_page)
+      gt.save!
+      gt.reload
+      guide_photos = gt.guide_photos.sort_by(&:position)
+      expect(guide_photos.first).to eq gp
+      expect(guide_photos.last.position).to be > gp.position
+    end
 
-  #   # it "should not add maps" do
-  #   #   page = EolService.page(791500, :common_names => true, :maps => 1, :details => true, :photos => 0)
-  #   #   gt.sync_eol_photos(:page => page)
-  #   #   if gp = gt.guide_photos.last
-  #   #     puts "gp.attributes: #{gp.attributes.inspect}"
-  #   #   end
-  #   #   expect(gt.guide_photos).to be_blank
-  #   # end
-  # end
+    # it "should not add maps" do
+    #   page = EolService.page(791500, :common_names => true, :maps => 1, :details => true, :photos => 0)
+    #   gt.sync_eol_photos(:page => page)
+    #   if gp = gt.guide_photos.last
+    #     puts "gp.attributes: #{gp.attributes.inspect}"
+    #   end
+    #   expect(gt.guide_photos).to be_blank
+    # end
+  end
 
   describe "add_color_tags" do
     let(:yellow) { build :color, value: "yellow" }
