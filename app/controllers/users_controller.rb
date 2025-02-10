ff9f72a79f3fe57fd568757946007161ef8cab1f
sylvain-morin <sylvain.morin@inaturalist.org>
2025-02-10 12:54:03 +0100

    CD_20250207

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -349,14 +349,14 @@ class UsersController < ApplicationController
   end
 
   def ranking_stats
-    @ranking_stats_data = compute_ranking_stats.values
+    @ranking_stats_data = compute_ranking_stats
     respond_to do | format |
       format.html { render :ranking_stats_users, layout: "bootstrap" }
     end
   end
 
   def compute_ranking_stats
-    taxon_data = {}
+    internal_taxon_data = {}
     # Extract species from reseach grade observations of the user
     # and first date the user added an observation of this species
     @user.observations.where( quality_grade: "research" ).each do | observation |
@@ -371,24 +371,22 @@ class UsersController < ApplicationController
       taxon_name = taxon.name
       taxon_id = taxon.parent.id if taxon.rank == "subspecies"
       taxon_name = taxon.parent.name if taxon.rank == "subspecies"
-      taxon_photo = taxon.default_photo.best_url(:square)
       #puts "taxon = #{taxon_name} in #{country.name} at #{created_at}"
       key = "#{taxon_id}-#{country.id}"
-      if taxon_data.key?( key )
-        taxon_data[key][:first_added] = [created_at, taxon_data[key][:first_added]].min
+      if internal_taxon_data.key?( key )
+        internal_taxon_data[key][:first_added] = [created_at, internal_taxon_data[key][:first_added]].min
       else
-        taxon_data[key] = {
+        internal_taxon_data[key] = {
           taxon_id: taxon_id,
           country_id: country.id,
           country: country.name,
           taxon: taxon_name,
-          first_added: created_at,
-          taxon_photo: taxon_photo
+          first_added: created_at
         }
       end
     end
     # Add observations count for species observed by the user
-    taxon_data.each do | _, data |
+    internal_taxon_data.each do | _, data |
       taxon_id = data[:taxon_id]
       first_added = data[:first_added]
       country_id = data[:country_id]
@@ -463,6 +461,18 @@ class UsersController < ApplicationController
       ).total_entries
       #puts "=> #{data[:obs_count]} / #{data[:obs_count_at_creation]} / #{data[:obs_country_count]} / #{data[:obs_country_count_at_creation]}"
     end
+    # Sort by obs_count_at_creation and take the first 50
+    lowest_obs_count = internal_taxon_data.values.sort_by { |taxon| taxon[:obs_count_at_creation] }.first(50)
+    # Sort by obs_country_count_at_creation and take the first 50
+    lowest_obs_country_count = internal_taxon_data.values.sort_by { |taxon| taxon[:obs_country_count_at_creation] }.first(50)
+    # Combine both arrays and ensure we have unique results
+    taxon_data = (lowest_obs_count + lowest_obs_country_count).uniq.first(100)
+    # Add photos
+    # Add category based on selection criteria
+    taxon_data.each do |taxon|
+      taxon[:taxon_photo] = Taxon.find_by_id(taxon[:taxon_id]).default_photo.best_url(:square)
+    end
+    taxon_data
   end
 
   def get_nearby_taxa_obs_counts search_params
