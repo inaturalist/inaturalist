53456f63df1ef066a1b47b60d874b8a9db400215
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2025-02-12 11:00:33 -0800

    fix: reindex tags indices after changing collation

--- db/migrate/20250204222646_change_collation_on_tags_name.rb
+++ db/migrate/20250204222646_change_collation_on_tags_name.rb
@@ -5,9 +5,17 @@ class ChangeCollationOnTagsName < ActiveRecord::Migration[6.1]
     # This makes LOWER() behave the same way on Turkic characters as Ruby's
     # downcase method, which allows existing Turkic tags to be found
     change_column :tags, :name, :string, limit: 255, collation: "und-x-icu"
+    execute <<~SQL
+      REINDEX INDEX index_tags_on_name;
+      REINDEX INDEX index_tags_on_lower_name;
+    SQL
   end
 
   def down
     change_column :tags, :name, :string, limit: 255, collation: "default"
+    execute <<~SQL
+      REINDEX INDEX index_tags_on_name;
+      REINDEX INDEX index_tags_on_lower_name;
+    SQL
   end
 end
