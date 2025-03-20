9e2631013c42630c223a009e57c912d3438aabb6
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2025-03-20 10:50:14 -0700

    chore: remove vestigial code that might be blocking deploy

--- app/controllers/picasa_controller.rb
+++ app/controllers/picasa_controller.rb
@@ -22,58 +22,38 @@ class PicasaController < ApplicationController
     if pa.nil?
       @provider = 'picasa'
       uri = URI.parse( request.referrer ) # extracts params and puts them in the hash uri.query_values
-      query_values ||= {}
       query_values = Rack::Utils.parse_nested_query( uri.query ).symbolize_keys.merge( source: @provider, context: context )
       uri.query = query_values.to_query
       @auth_url = ProviderAuthorization::AUTH_URLS['google']
       session[:return_to] = uri.to_s 
       render(:partial => "photos/auth") and return
     end
-    if false
-    # if context == 'user' && params[:q].blank? # search is blank, so show all albums
-    #   @albums = picasa_albums(current_user)
-    #   render :partial => 'picasa/albums' and return
-    # elsif context == 'friends'
-    #   @friend_id = params[:object_id]
-    #   @friend_id = nil if @friend_id=='null'
-    #   if @friend_id.nil?  # if context is friends, but no friend id specified, we want to show the friend selector
-    #     @friends = picasa_friends(current_user)
-    #     render :partial => 'picasa/friends' and return
-    #   else
-    #     @albums = picasa_albums(current_user, @friend_id)
-    #     friend_data = current_user.picasa_client.user(@friend_id)
-    #     @friend_name = friend_data.author.name 
-    #     render :partial => 'picasa/albums' and return
-    #   end
-    else # context='public' or context='user' with a search query
-      picasa = current_user.picasa_client
-      search_params = {}
-      per_page = params[:limit] ? params[:limit].to_i : 10
-      if context == 'user'
-        search_params[:user_id] = current_user.picasa_identity.provider_uid
-      end
-      begin
-        Timeout::timeout(10) do
-          @page_token = params[:page_token]
-          results = PicasaPhoto.picasa_request_with_refresh(current_user.picasa_identity) do
-            # picasa.search(params[:q], search_params)
-            goog = GooglePhotosApi.new( current_user.picasa_identity.token )
-            goog.search( pageSize: per_page, pageToken: @page_token )
-          end
-          if results
-            @next_page_token = results["nextPageToken"]
-            @photos = results["mediaItems"].map do |api_response|
-              PicasaPhoto.new_from_api_response(api_response, :user => current_user)
-            end.compact
-          end
-        end
-      rescue Timeout::Error => e
-        @timeout = e
-      rescue RestClient::Forbidden
-        @forbidden = true
-      end
+
+    search_params = {}
+    per_page = params[:limit] ? params[:limit].to_i : 10
+    if context == 'user'
+      search_params[:user_id] = current_user.picasa_identity.provider_uid
     end
-    
+    begin
+      Timeout::timeout(10) do
+        @page_token = params[:page_token]
+        results = PicasaPhoto.picasa_request_with_refresh(current_user.picasa_identity) do
+          # picasa.search(params[:q], search_params)
+          goog = GooglePhotosApi.new( current_user.picasa_identity.token )
+          goog.search( pageSize: per_page, pageToken: @page_token )
+        end
+        if results
+          @next_page_token = results["nextPageToken"]
+          @photos = results["mediaItems"].map do |api_response|
+            PicasaPhoto.new_from_api_response(api_response, :user => current_user)
+          end.compact
+        end
+      end
+    rescue Timeout::Error => e
+      @timeout = e
+    rescue RestClient::Forbidden
+      @forbidden = true
+    end
     @synclink_base = params[:synclink_base] unless params[:synclink_base].blank?
 
     respond_to do |format|
@@ -139,22 +119,6 @@ class PicasaController < ApplicationController
       goog = GooglePhotosApi.new( current_user.picasa_identity.token )
       goog.albums["albums"]
     end
-  end
-
-  def picasa_friends(user)
-    return [] unless (pa = current_user.has_provider_auth('google'))
-    picasa = GData::Client::Photos.new
-    picasa.auth_token = pa.token
-    contacts_data = picasa.get("http://picasaweb.google.com/data/feed/api/user/default/contacts").to_xml 
-    friends = []
-    contacts_data.elements.each('entry'){|e|
-      friends << {
-        'id' => e.elements['gphoto:user'].text, # this is a feed url that id's the photo
-        'name' => e.elements['gphoto:nickname'].text,
-        'pic_url' => e.elements['gphoto:thumbnail'].text
-      }
-    }
-    friends
   end
 
 end
