80344e163649309b589fdd6b1289d152177b04b1
Patrick Leary <patrick.r.leary@gmail.com>
2025-03-21 15:18:28 -0400

    place taxon names are duplicated with taxon names during taxon swap

--- app/models/taxon_swap.rb
+++ app/models/taxon_swap.rb
@@ -1,24 +1,26 @@
+# frozen_string_literal: true
+
 class TaxonSwap < TaxonChange
-  has_many :old_taxa, :through => :taxon_change_taxa, :source => :taxon
+  has_many :old_taxa, through: :taxon_change_taxa, source: :taxon
 
   validate :only_has_one_input
   validate :has_inputs_and_outputs
 
   def only_has_one_input
-    errors.add(:base, I18n.t(:taxon_swap_only_has_one_input)) if input_taxa.size > 1
+    errors.add( :base, I18n.t( :taxon_swap_only_has_one_input ) ) if input_taxa.size > 1
   end
 
   def has_inputs_and_outputs
-    if input_taxa.size == 0 || output_taxa.size == 0
-      errors.add(:base, I18n.t(:taxon_swap_has_inputs_and_outputs))
-    end
+    return unless input_taxa.empty? || output_taxa.empty?
+
+    errors.add( :base, I18n.t( :taxon_swap_has_inputs_and_outputs ) )
   end
-  
+
   def old_taxon
     old_taxa.first
   end
-  
-  def opposite_taxon_from(subject_taxon)
+
+  def opposite_taxon_from( subject_taxon )
     if subject_taxon.id == taxon_id
       taxa.loaded? ? taxa.first : old_taxon
     else
@@ -26,16 +28,16 @@ class TaxonSwap < TaxonChange
     end
   end
 
-  def add_input_taxon(t)
-    self.taxon_change_taxa.build(:taxon => t, :taxon_change => self)
+  def add_input_taxon( adding_input_taxon )
+    taxon_change_taxa.build( taxon: adding_input_taxon, taxon_change: self )
   end
 
-  def add_output_taxon(t)
-    self.taxon = t
+  def add_output_taxon( adding_output_taxon )
+    self.taxon = adding_output_taxon
   end
 
   def input_taxa
-    taxon_change_taxa.select{|tct| !tct._destroy}.map(&:taxon).compact
+    taxon_change_taxa.reject( &:_destroy ).map( &:taxon ).compact
   end
 
   def output_taxa
@@ -43,7 +45,7 @@ class TaxonSwap < TaxonChange
   end
 
   def input_taxon
-    taxon_change_taxa.select{|tct| !tct._destroy}.first.try(:taxon)
+    taxon_change_taxa.reject( &:_destroy ).first.try( :taxon )
   end
 
   def output_taxon
@@ -53,11 +55,11 @@ class TaxonSwap < TaxonChange
   def verb_phrase
     "replaced with"
   end
-  
+
   def commit
     super
     # duplicate photos
-    input_taxon.taxon_photos.sort_by(&:id).each do |taxon_photo|
+    input_taxon.taxon_photos.sort_by( &:id ).each do | taxon_photo |
       begin
         output_taxon.photos << taxon_photo.photo
       rescue ActiveRecord::RecordInvalid => e
@@ -66,21 +68,21 @@ class TaxonSwap < TaxonChange
     end
 
     # duplicate conservation_statuses
-    input_taxon.conservation_statuses.each do |cs|
+    input_taxon.conservation_statuses.each do | cs |
       new_cs = cs.dup
       new_cs.taxon_id = output_taxon.id
       unless new_cs.save
-        Rails.logger.error "[ERROR #{Time.now}] TaxonChange #{id} failed to duplicate #{cs}: " + 
+        Rails.logger.error "[ERROR #{Time.now}] TaxonChange #{id} failed to duplicate #{cs}: " +
           new_cs.errors.full_messages.to_sentence
       end
     end
-    
+
     # duplicate taxon_names
-    input_taxon.taxon_names.each do |taxon_name|
+    input_taxon.taxon_names.each do | taxon_name |
       taxon_name.reload
       unless taxon_name.valid?
-        Rails.logger.info "[INFO] Destroying #{taxon_name} while committing taxon change " +
-          "#{self.id}: #{taxon_name.errors.full_messages.to_sentence}"
+        Rails.logger.info "[INFO] Destroying #{taxon_name} while committing taxon change " \
+          "#{id}: #{taxon_name.errors.full_messages.to_sentence}"
         taxon_name.destroy
         next
       end
@@ -95,45 +97,46 @@ class TaxonSwap < TaxonChange
       end
       new_taxon_name.taxon_id = output_taxon.id
       new_taxon_name.is_valid = false if taxon_name.is_scientific_names? && taxon_name.is_valid?
-      unless new_taxon_name.save
-        Rails.logger.error "[ERROR #{Time.now}] TaxonChange #{id} failed to duplicate #{taxon_name}: " + 
+      if new_taxon_name.save
+        taxon_name.place_taxon_names.each do | place_taxon_name |
+          new_place_taxon_name = place_taxon_name.dup
+          new_place_taxon_name.taxon_name_id = new_taxon_name.id
+          new_place_taxon_name.save
+        end
+      else
+        Rails.logger.error "[ERROR #{Time.now}] TaxonChange #{id} failed to duplicate #{taxon_name}: " +
           new_taxon_name.errors.full_messages.to_sentence
       end
     end
-    
+
     # duplicate taxon_range
-    if output_taxon.taxon_range.nil?
-      if taxon_range = input_taxon.taxon_range
-        new_taxon_range = taxon_range.dup
-        new_taxon_range.taxon_id = output_taxon.id
-        unless new_taxon_range.save
-          Rails.logger.error "[ERROR #{Time.now}] TaxonChange #{id} failed to duplicate #{taxon_range}: " +
-            new_taxon_range.errors.full_messages.to_sentence
-        end
-        unless new_taxon_range.create_kml_attachment
-          Rails.logger.error "[ERROR #{Time.now}] TaxonChange #{id} failed to duplicate #{taxon_range} attachment: " +
-            new_taxon_range.errors.full_messages.to_sentence
-        end
+    if output_taxon.taxon_range.nil? && ( taxon_range = input_taxon.taxon_range )
+      new_taxon_range = taxon_range.dup
+      new_taxon_range.taxon_id = output_taxon.id
+      unless new_taxon_range.save
+        Rails.logger.error "[ERROR #{Time.now}] TaxonChange #{id} failed to duplicate #{taxon_range}: " +
+          new_taxon_range.errors.full_messages.to_sentence
+      end
+      unless new_taxon_range.create_kml_attachment
+        Rails.logger.error "[ERROR #{Time.now}] TaxonChange #{id} failed to duplicate #{taxon_range} attachment: " +
+          new_taxon_range.errors.full_messages.to_sentence
       end
     end
-    
+
     # duplicate atlas
-    unless output_taxon.atlas
-      if atlas = input_taxon.atlas
-        new_atlas = atlas.dup
-        new_atlas.taxon_id = output_taxon.id
-        unless new_atlas.save
-          Rails.logger.error "[ERROR #{Time.now}] Atlas #{id} failed to duplicate #{atlas}: " +
-            new_atlas.errors.full_messages.to_sentence
-        end
+    if !output_taxon.atlas && ( atlas = input_taxon.atlas )
+      new_atlas = atlas.dup
+      new_atlas.taxon_id = output_taxon.id
+      unless new_atlas.save
+        Rails.logger.error "[ERROR #{Time.now}] Atlas #{id} failed to duplicate #{atlas}: " +
+          new_atlas.errors.full_messages.to_sentence
       end
     end
-    
+
     # duplicate colors
     output_taxon.colors << input_taxon.colors if output_taxon.colors.blank?
-    
+
     # Move input child taxa to the output taxon
     delay( priority: USER_PRIORITY ).move_input_children_to_output( input_taxon.id )
   end
-  
 end
