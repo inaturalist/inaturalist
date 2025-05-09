c97afa61d11306286023149707dacee1723fa5f4
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2025-05-09 11:42:42 -0700

    style: rubocop compliance for EOL stuff

--- app/controllers/eol_controller.rb
+++ app/controllers/eol_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class EolController < ApplicationController
-  before_action :return_here, :only => [:options]
+  before_action :return_here, only: [:options]
   before_action :authenticate_user!
 
   # Return an HTML fragment containing checkbox inputs for EOL photos.
@@ -10,30 +12,24 @@ class EolController < ApplicationController
     @photos = []
     @q = if !params[:q].blank?
       params[:q]
-    elsif taxon_id = params[:taxon_id]
-      @taxon = Taxon.find_by_id(taxon_id)
+    elsif ( taxon_id = params[:taxon_id] )
+      @taxon = Taxon.find_by_id( taxon_id )
       @taxon.name
     end
-    
+
     per_page = params[:limit].to_i
     per_page = 36 if per_page.blank? || per_page.to_i > 36
     page = params[:page].to_i
-    page = 1 if page == 0
-    offset = per_page*(page-1)+(page-1)
-    limit = if offset > per_page
-      75
-    else
-      per_page
-    end
+    page = 1 if page.zero?
 
-    @photos = EolPhoto.search_eol(@q, page: page, per_page: per_page, eol_page_id: params[:eol_page_id])
-    
+    @photos = EolPhoto.search_eol( @q, page: page, per_page: per_page, eol_page_id: params[:eol_page_id] )
+
     partial = params[:partial].to_s
-    partial = 'photo_list_form' unless %w(photo_list_form bootstrap_photo_list_form).include?(partial)    
-    respond_to do |format|
+    partial = "photo_list_form" unless %w(photo_list_form bootstrap_photo_list_form).include?( partial )
+    respond_to do | format |
       format.html do
         render partial: "photos/#{partial}", locals: {
-          photos: @photos, 
+          photos: @photos,
           index: params[:index],
           local_photos: false
         }
@@ -41,5 +37,4 @@ class EolController < ApplicationController
       format.json { render json: @photos }
     end
   end
-    
 end
--- spec/models/eol_photo_spec.rb
+++ spec/models/eol_photo_spec.rb
@@ -1,52 +1,56 @@
-require File.dirname(__FILE__) + '/../spec_helper.rb'
+# frozen_string_literal: true
+
+require "#{File.dirname( __FILE__ )}/../spec_helper.rb"
 
 describe EolPhoto, "new_from_api_response" do
   it "should set native_photo_id" do
-    api_response = EolPhoto.get_api_response( 5246083 )
-    p = EolPhoto.new_from_api_response(api_response)
-    expect(p.native_photo_id).not_to be_blank
+    api_response = EolPhoto.get_api_response( 5_246_083 )
+    p = EolPhoto.new_from_api_response( api_response )
+    expect( p.native_photo_id ).not_to be_blank
   end
 
   it "should work for a fragment from a page response" do
-    page = EolService.page( 485229, licenses: "all", images_per_page: 10, text_per_page: 0, videos_per_page: 0, details: true )
-    api_response = page.at('//dataObject[.//mediaURL]')
-    p = EolPhoto.new_from_api_response(api_response)
-    expect(p.native_photo_id).not_to be_blank
+    page = EolService.page( 485_229, licenses: "all", images_per_page: 10, text_per_page: 0, videos_per_page: 0,
+      details: true )
+    api_response = page.at( "//dataObject[.//mediaURL]" )
+    p = EolPhoto.new_from_api_response( api_response )
+    expect( p.native_photo_id ).not_to be_blank
   end
 
   it "should not set native_realname to inaturalist" do
-    page = EolService.page( 455040, licenses: "all", images_per_page: 10, text_per_page: 0, videos_per_page: 0, details: true )
-    api_response = page.at('//dataObject[.//mediaURL]')
-    p = EolPhoto.new_from_api_response(api_response)
-    expect(p.native_realname).not_to eq "inaturalist"
+    page = EolService.page( 455_040, licenses: "all", images_per_page: 10, text_per_page: 0, videos_per_page: 0,
+      details: true )
+    api_response = page.at( "//dataObject[.//mediaURL]" )
+    p = EolPhoto.new_from_api_response( api_response )
+    expect( p.native_realname ).not_to eq "inaturalist"
   end
 
   it "should work for an image in the public domain" do
-    p = EolPhoto.new_from_api_response( EolService.data_objects( 5246083 ) )
+    p = EolPhoto.new_from_api_response( EolService.data_objects( 5_246_083 ) )
     expect( p ).to be_valid
   end
 end
 
-describe EolPhoto, "repair", disabled: ENV["TRAVIS_CI"] do
+describe EolPhoto, "repair", disabled: ENV.fetch( "TRAVIS_CI", nil ) do
   elastic_models( Observation )
   it "should not fail" do
-    api_response = EolPhoto.get_api_response( 5246083 )
-    p = EolPhoto.new_from_api_response(api_response)
-    expect {
+    api_response = EolPhoto.get_api_response( 5_246_083 )
+    p = EolPhoto.new_from_api_response( api_response )
+    expect do
       p.repair
-    }.not_to raise_error
+    end.not_to raise_error
   end
 end
 
-describe EolPhoto, "sync", disabled: ENV["TRAVIS_CI"] do
+describe EolPhoto, "sync", disabled: ENV.fetch( "TRAVIS_CI", nil ) do
   elastic_models( Observation )
-  let(:api_response) { EolPhoto.get_api_response( 5246083 ) }
-  let(:p) { EolPhoto.new_from_api_response(api_response) }
+  let( :api_response ) { EolPhoto.get_api_response( 5_246_083 ) }
+  let( :p ) { EolPhoto.new_from_api_response( api_response ) }
   it "should reset native_realname" do
     orig = p.native_realname
-    p.update_attribute(:native_realname, nil)
-    expect(p.native_realname).to be_blank
+    p.update_attribute( :native_realname, nil )
+    expect( p.native_realname ).to be_blank
     p.sync
-    expect(p.native_realname).to eq orig
+    expect( p.native_realname ).to eq orig
   end
 end
