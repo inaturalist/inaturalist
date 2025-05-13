feba9e499e36b91af2c74d399b17a9f93dd0aaaa
sylvain-morin <sylvain.morin@inaturalist.org>
2025-05-13 14:01:52 +0200

    OPS-160 Fix Active Queries admin page

---
+++ app/controllers/admin/queries_controller.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class Admin::QueriesController < ApplicationController
+  def index
+    redirect_to :primary_admin_queries
+  end
+
+  def primary
+    @queries = active_queries( :primary )
+    render layout: "admin"
+  end
+
+  def replica
+    @queries = active_queries( :replica )
+    render layout: "admin"
+  end
+
+  private
+
+  def active_queries( type )
+    pool = ActiveRecord::Base.connection.instance_variable_get( "@#{type}_pool" )
+    queries = []
+
+    pool&.connections&.each do | connection |
+      queries += connection.active_queries.map do | q |
+        { db_host: connection.config[:host] }.merge( q )
+      end
+    end
+
+#    queries.each do | queries |
+#      queries.delete_if { |q| q["query"] =~ /pg_stat_activity/ }
+#    end
+
+    now = Time.current
+    queries.each do | q |
+      q["duration"] = q["query_start"] ? ( now - q["query_start"] ) / 1000.0 : 0
+    end
+
+    queries.sort_by {| q | q["duration"] }.reverse
+  end
+end
--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -249,43 +249,7 @@ class AdminController < ApplicationController
     redirect_to root_path
   end
 
-  def queries
-    redirect_to :primary_admin_queries
-  end
-
-  def primary_queries
-    @queries = active_queries( :primary )
-    render layout: "admin"
-  end
-
-  def replica_queries
-    @queries = active_queries( :replica )
-    render layout: "admin"
-  end
-
   private
-
-  def active_queries( type )
-    pool = ActiveRecord::Base.connection.instance_variable_get( "@#{type}_pool" )
-    queries = []
-
-    pool&.connections&.each do | connection |
-      queries += connection.active_queries.map do | q |
-        { db_host: connection.config[:host] }.merge( q )
-      end
-    end
-
-#    queries.each do | queries |
-#      queries.delete_if { |q| q["query"] =~ /pg_stat_activity/ }
-#    end
-
-    now = Time.current
-    queries.each do | q |
-      q["duration"] = q["query_start"] ? ( now - q["query_start"] ) / 1000.0 : 0
-    end
-
-    queries.sort_by {| q | q["duration"] }.reverse
-  end
 
   def load_user_content_info
     user_id = params[:id] || params[:user_id]
