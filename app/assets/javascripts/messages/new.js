01be51271ce177f24cf43a0fec1a74cf19e28a12
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2025-05-13 10:35:41 -0700

    refactor: support ES6 / ES2020 Javascript syntax
    Switches from uglifier to terser to support modern Javascript syntax. Uglifier
    supports ES6, but pretty much ends there, so if we want things like optional
    chaining, we need a different compressor.

--- app/assets/javascripts/messages/new.js
+++ app/assets/javascripts/messages/new.js
@@ -1,6 +1,6 @@
 /* global _ */
 
-$( document ).ready( function ( ) {
+$( document ).ready( ( ) => {
   if ( $( "#new_message .user_name" ).length > 0 ) {
     $( "#new_message .user_name" ).userAutocomplete( {
       resetOnChange: false,
@@ -16,13 +16,13 @@ $( document ).ready( function ( ) {
     } );
   }
 
-  $( "#new_message" ).submit( function ( ) {
+  $( "#new_message" ).submit( ( ) => {
     $( "#new_message" ).data( { submitted: true } );
   } );
 
   // use `window.onbeforeunload` to trigger a browser warning when leaving the page
   // and there is any text at all in either the subject or body of the new message
-  window.onbeforeunload = function ( ) {
+  window.onbeforeunload = ( ) => {
     if ( $( "#new_message" ).data( "submitted" ) ) {
       return null;
     }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS
   # Choose the compressors to use (if any)
   config.assets.compress = true
-  config.assets.js_compressor = Uglifier.new( mangle: false )
+  config.assets.js_compressor = :terser
   config.assets.css_compressor = :yui
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
