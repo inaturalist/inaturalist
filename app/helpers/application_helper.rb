18a4112e77d8326198a42f0ca1f7c8be231a0abd
Patrick Leary <patrick.r.leary@gmail.com>
2025-05-30 14:00:08 -0400

    feat: include fundraise up JS on donate page, related to WEB-702

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1795,4 +1795,18 @@ module ApplicationHelper
 
     URI.join( site.url.to_s, url.to_s ).to_s
   end
+
+  def fundraise_up_js
+    raw <<-HTML
+      <script type="text/javascript">
+        (function(w,d,s,n,a){if(!w[n]){var l='call,catch,on,once,set,then,track,openCheckout'
+        .split(','),i,o=function(n){return'function'==typeof n?o.l.push([arguments])&&o
+        :function(){return o.l.push([n,arguments])&&o}},t=d.getElementsByTagName(s)[0],
+        j=d.createElement(s);j.async=!0;j.src='https://cdn.fundraiseup.com/widget/'+a+'';
+        t.parentNode.insertBefore(j,t);o.s=Date.now();o.v=5;o.h=w.location.href;o.l=[];
+        for(i=0;i<8;i++)o[l[i]]=o(l[i]);w[n]=o}
+        })(window,document,'script','FundraiseUp','ACSSCSPN');
+      </script>
+    HTML
+  end
 end
