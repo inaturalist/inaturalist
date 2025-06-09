inaturalist/inaturalist

commit 3cd180705e954db9111d6617b71dcce7cdf84bfe

feat: remove support for Google Photos (#4599)
PicasaPhoto remains b/c there are still some photos with that subtype and
there's still some display logic around showing source info.

Closes WEB-582

app/assets/javascripts/jquery/plugins/inat/photo_selectors.js
@@ -101,6 +101,9 @@
   };
   
   function buildControls(wrapper, options) {
+    // If there are no urls and no sources, there's no reason to build out this UI
+    if ( ( !options.urls || options.urls.length === 0 ) && ( !options.sources || options.sources.length === 0 ) ) return;
+
     if (options.bootstrap) {
       // Insert a search field and button.  No forms, please
       var controls = $('<div class="photoSelectorControls"></div>').css(
@@ -263,23 +266,6 @@
       $.each($allContextWrappers, function(i,c){ $sourceWrapper.append(c); });
 
     }
-
-    $(wrapper).on('click', ".picasaAlbums .album", function() {
-      var aid = $(this).attr('data-aid'); // $(this).data('aid') doesn't work because of ridiculous type conversion
-      try {
-        updateSource({
-          url: '/picasa/album/'+aid,
-          object_id: $(this).closest('.picasaAlbums').attr('data-friend_id')
-          });
-      } catch(e) {
-        $.fn.photoSelector.changeBaseUrl(
-          wrapper, 
-          '/picasa/album/' + aid, 
-          'user', //contextSelect.val(), 
-          $(this).closest('.picasaAlbums').attr('data-friend_id'));
-      }
-      return false;
-    });
   
     $(wrapper).on('click', '.back_to_albums', function(){
       try { updateSource({ object_id: $(this).attr('data-friend_id') }); } 
@@ -337,9 +323,6 @@
     prev.click(function(e) {
       var prevOpts = $.extend({}, $(wrapper).data('photoSelectorOptions'));
       var currentURL = $( ".urlselect select", wrapper ).val( );
-      if ( currentURL && currentURL.match( /picasa/ ) ) {
-        return false;
-      }
       var pagenum = parseInt( $( wrapper ).find( ".photoSelectorPage" ).val( ) );
       pagenum -= 1;
       if ( pagenum < 1 ) pagenum = 1;
@@ -354,21 +337,10 @@
     next.click(function(e) {
       var nextOpts = $.extend({}, $(wrapper).data('photoSelectorOptions'));
       var currentURL = $( ".urlselect select", wrapper ).val( );
-      if ( currentURL && currentURL.match( /picasa/ ) ) {
-        var nextPageToken = $( "[name='next_page_token']", wrapper ).val( );
-        if ( nextPageToken ) {
-          nextOpts.urlParams = $.extend({}, nextOpts.urlParams, { page_token: nextPageToken } );
-          $(wrapper).find('.photoSelectorPageToken').val( nextPageToken );
-          $(wrapper).find('.photoSelectorNextPageToken').val( null );
-        } else {
-          return false;
-        }
-      } else {
-        var pagenum = parseInt($(wrapper).find('.photoSelectorPage').val());
-        pagenum += 1;
-        nextOpts.urlParams = $.extend({}, nextOpts.urlParams, {page: pagenum});
-        $(wrapper).find('.photoSelectorPage').val(pagenum);
-      }
+      var pagenum = parseInt($(wrapper).find('.photoSelectorPage').val());
+      pagenum += 1;
+      nextOpts.urlParams = $.extend({}, nextOpts.urlParams, {page: pagenum});
+      $(wrapper).find('.photoSelectorPage').val(pagenum);
       $.fn.photoSelector.queryPhotos(
         $searchInput.val(), 
         wrapper, 
@@ -460,13 +432,8 @@
     options.urlParams.context = (context || 'user');
     options.urlParams.object_id = (object_id || null);
     $(wrapper).data('photoSelectorOptions', options);
-    if ( url && url.match( /picasa/ ) ) {
-      $( ".photoSelectorSearch", wrapper ).hide( );
-      $( ".prevlink", wrapper ).hide( );
-    } else {
-      $( ".photoSelectorSearch", wrapper ).show( );
-      $( ".prevlink", wrapper ).show( );
-    }
+    $( ".photoSelectorSearch", wrapper ).show( );
+    $( ".prevlink", wrapper ).show( );
     $.fn.photoSelector.queryPhotos($(wrapper).find('.photoSelectorSearchField').val(), wrapper);
   };
 
@@ -545,12 +512,7 @@
           // Prevent adding files that are too big
           bindMaxFileSizeValidation( wrapper );
         } else {
-          if ( options.baseURL && options.baseURL.match( /picasa/ ) ) {
-            $('.nextlink, .allNone, .photoSelectorSearch', wrapper).show( );
-            $( ".photoSelectorSearch input" ).hide( );
-          } else {
-            $('.nextlink, .prevlink, .allNone, .photoSelectorSearch', wrapper).show()
-          }
+          $('.nextlink, .prevlink, .allNone, .photoSelectorSearch', wrapper).show()
           $(wrapper).find('.local_photos').hide()
         }
 

app/assets/javascripts/observations/edit.js
@@ -83,7 +83,7 @@ $(document).ready(function() {
     if (PHOTO_IDENTITY_URLS && PHOTO_IDENTITY_URLS.length > 0) {
       options.urls = PHOTO_IDENTITY_URLS
     }
-    $(this).photoSelector(options)
+    $( this ).photoSelector( options );
   })
 
   $('.observation_sounds').each(function(){

app/assets/stylesheets/observations/edit.css
@@ -65,7 +65,6 @@ input.place_guess { width: 336px; }
 }
 .soundSelector, 
 .photoSelector {
-  padding-top: 1px;
   margin-top: 0;
   margin-right: -3px;
   border-radius: 0;

app/controllers/observations_controller.rb
@@ -442,7 +442,6 @@ class ObservationsController < ApplicationController
     end
 
     sync_flickr_photo if params[:flickr_photo_id] && current_user.flickr_identity
-    sync_picasa_photo if params[:picasa_photo_id] && current_user.picasa_identity
     sync_local_photo if params[:local_photo_id]
       
     @welcome = params[:welcome]
@@ -500,7 +499,6 @@ class ObservationsController < ApplicationController
     end
 
     sync_flickr_photo if params[:flickr_photo_id]
-    sync_picasa_photo if params[:picasa_photo_id]
     sync_local_photo if params[:local_photo_id]
     @observation_fields = ObservationField.recently_used_by(current_user).limit(10)
 
@@ -1143,17 +1141,7 @@ class ObservationsController < ApplicationController
         :user => current_user, :photo_class => klass)
     end.flatten.compact
     @observations = photos.map do |p|
-      photo_obs = p.to_observation
-      if p.is_a?( PicasaPhoto )
-        # Sometimes Google doesn't return all the metadata for undetermined
-        # reasons. See https://github.com/inaturalist/inaturalist/issues/1408
-        if photo_obs.observed_on.blank? || photo_obs.latitude.blank?
-          local_photo = Photo.local_photo_from_remote_photo( p )
-          photo_obs = local_photo.to_observation
-          photo_obs.observation_photos.build( photo: p )
-        end
-      end
-      photo_obs
+      p.to_observation
     end
     @observation_photos = ObservationPhoto.joins(:photo, :observation).
       where("photos.native_photo_id IN (?)", photos.map(&:native_photo_id))
@@ -2265,44 +2253,6 @@ class ObservationsController < ApplicationController
       flash.now[:error] = t(:sorry_we_didnt_find_that_photo)
     end
   end
-  
-  def sync_picasa_photo
-    begin
-      api_response = PicasaPhoto.get_api_response(params[:picasa_photo_id], :user => current_user)
-    rescue Timeout::Error => e
-      flash.now[:error] = t(:sorry_picasa_isnt_responding_at_the_moment)
-      Rails.logger.error "[ERROR #{Time.now}] Timeout: #{e}"
-      Logstasher.write_exception(e, request: request, session: session, user: current_user)
-      return
-    end
-    unless api_response
-      Rails.logger.debug "[DEBUG] Failed to find Picasa photo for #{params[:picasa_photo_id]}"
-      return
-    end
-    @picasa_photo = PicasaPhoto.new_from_api_response(api_response, :user => current_user)
-    
-    if @picasa_photo && @picasa_photo.valid?
-      @picasa_observation = @picasa_photo.to_observation
-      sync_attrs = PHOTO_SYNC_ATTRS
-      unless params[:picasa_sync_attrs].blank?
-        sync_attrs = sync_attrs & params[:picasa_sync_attrs]
-      end
-      sync_attrs.each do |sync_attr|
-        @observation.send("#{sync_attr}=", @picasa_observation.send(sync_attr))
-      end
-      photo_already_exists = @observation.observation_photos.detect do |op|
-        op.photo.native_photo_id == @picasa_photo.native_photo_id &&
-        op.photo.subclass == "PicasaPhoto"
-      end
-      unless photo_already_exists
-        @observation.observation_photos.build(:photo => @picasa_photo)
-      end
-      
-      flash.now[:notice] = t(:preview_of_synced_observation, :url => url_for)
-    else
-      flash.now[:error] = t(:sorry_we_didnt_find_that_photo)
-    end
-  end
 
   def sync_local_photo
     unless @local_photo = Photo.find_by_id(params[:local_photo_id])
@@ -2340,7 +2290,6 @@ class ObservationsController < ApplicationController
       return true
     end
     if Rails.env.development?
-      PicasaPhoto
       LocalPhoto
       FlickrPhoto
     end
@@ -2384,12 +2333,7 @@ class ObservationsController < ApplicationController
       provider_name = nil
       provider_type = nil
       if identity.is_a?(ProviderAuthorization)
-        if identity.provider_name =~ /google/i
-          provider_type = "picasa"
-          provider_name = "Google Photos"
-        else
-          provider_type = identity.provider_name
-        end
+        provider_type = identity.provider_name
       else
         provider_type = identity.class.to_s.underscore.split('_').first # e.g. FlickrIdentity=>'flickr'
       end
@@ -2401,16 +2345,6 @@ class ObservationsController < ApplicationController
     @photo_sources = @photo_identities.inject({}) do |memo, ident| 
       if ident.respond_to?(:source_options)
         memo[ident.class.name.underscore.humanize.downcase.split.first] = ident.source_options
-      elsif ident.is_a?(ProviderAuthorization)
-        if ident.provider_name =~ /google/
-          memo[:picasa] = {
-            :title => 'Google Photos', 
-            :url => '/picasa/photo_fields', 
-            :contexts => [
-              ["Your photos", 'user', {:searchable => true}]
-            ]
-          }
-        end
       end
       memo
     end

app/controllers/photos_controller.rb
@@ -84,7 +84,7 @@ class PhotosController < ApplicationController
   end
 
   def fix
-    types = %w(FlickrPhoto PicasaPhoto)
+    types = %w(FlickrPhoto)
     @type = params[:type]
     @type = "FlickrPhoto" unless types.include?( @type )
     @provider_name = @type.underscore.gsub( /_photo/, "" )
@@ -101,7 +101,7 @@ class PhotosController < ApplicationController
   end
 
   def repair_all
-    @type = params[:type] if %w(FlickrPhoto PicasaPhoto).include?( params[:type] )
+    @type = params[:type] if %w(FlickrPhoto).include?( params[:type] )
     if @type.blank?
       respond_to do | format |
         format.json do

commit 1b1a2f074d584b9553dfa2c4f3626f39d8333ed3

fix: restore photo search for guide taxa (#4610)
I broke this in #4599. Now it should show the search UI when appropriate.

app/assets/javascripts/jquery/plugins/inat/photo_selectors.js
@@ -101,9 +101,6 @@
   };
   
   function buildControls(wrapper, options) {
-    // If there are no urls and no sources, there's no reason to build out this UI
-    if ( ( !options.urls || options.urls.length === 0 ) && ( !options.sources || options.sources.length === 0 ) ) return;
-
     if (options.bootstrap) {
       // Insert a search field and button.  No forms, please
       var controls = $('<div class="photoSelectorControls"></div>').css(
@@ -136,180 +133,140 @@
       $searchInput.val(options.defaultQuery)
     }
 
-    // this branch is for backwards compatibility 
-    // options.urls is used by legacy photoSelectors, but is now deprecated. 
-    // use options.sources (see below) instead.
-    if (typeof options != 'undefined' && typeof options.urls != 'undefined') {
-      var urlSelect = $('<select class="select" style="margin: 0 auto"></select>');
-      urlSelect.change(function() {
-        $.fn.photoSelector.changeBaseUrl(wrapper, urlSelect.val());
-      })
-      var urls = options.urls || [];
-      if (!options.skipLocal) {
-        urls.push({
-          title:  I18n.t('your_hard_drive'),
-          url: '/photos/local_photo_fields'
-        })
-      }
-      $.each(urls, function() {
-        if (this.url) {
-          var title = this.title;
-          var url = this.url;
-        } else {
-          var title = this;
-          var url = this;
-        }
-        var option = $('<option value="'+url+'">'+title+'</option>');
-        if (url === options.baseURL) $(option).attr('selected', 'selected');
-        $(urlSelect).append(option);
-      })
-      $sourceWrapper.append(urlSelect);
-    }
-
-    // this branch is for options.sources (new style of photoselector)
-    if (typeof options != 'undefined' && typeof options.sources != 'undefined') {
-
-      // this is called when you change either the source <select> or context <select>
-      function updateSource(sourceOptions){
-        $searchWrapper.hide()
-        $searchInput.val('')
-        var newSource = sources[currentSource]
-        sourceOptions = sourceOptions || {}
-        sourceOptions['url'] = sourceOptions.url || newSource.url
-        sourceOptions['object_id'] = sourceOptions.object_id || false
-        if (typeof newSource.$contextWrapper == 'undefined') {
-          // TODO: this is what happens when there isn't a $contextSelect for this source (i.e. only one available context)
-          //sourceOptions['context'] = newSource.defaultContext;
-        } else {
-          var currentContextName = newSource.$contextWrapper.find('select').val()
-          sourceOptions['context'] = currentContextName
-          for (var i = 0; i < newSource.contexts.length; i++) {
-            var currentContext = newSource.contexts[i]
-            if (newSource.contexts[i][0] == currentContextName) break
-          }
-          if (currentContext && currentContext[currentContext.length-1].searchable) {
-            // show search field
-            $searchWrapper.show();
-          } else {
-            $searchWrapper.hide();
-          }
-        }
-        $.fn.photoSelector.changeBaseUrl(wrapper, sourceOptions['url'], sourceOptions['context'], sourceOptions['object_id']);
-      }
-
-      $searchWrapper.hide();
-      var sources = options.sources || {};
-      var currentSource = options.defaultSource;
-      var $allContextWrappers = [];
-      if (!options.skipLocal) { sources['local'] = {title: "Your computer", url: '/photos/local_photo_fields'}; }
-      var $sourceSelect = $("<select class='select'></select>");
-      var sourceIndex = 0; // used as index when iterating over sources below
-      $.each(sources, function(sourceKey, sourceData){
-        var $sourceOption = $("<option value='"+sourceKey+"'>"+sourceData.title+"</option>");
-        $sourceSelect.append($sourceOption);
-        var $contextWrapper = $("<span style='display:none'></span>");
-        if (typeof options.defaultSource != 'undefined') { 
-          if (options.defaultSource == sourceKey) { // if we've specified a default source, and this it, show the associated contextSelect
-            $contextWrapper.css('display','inline-block');
-            $sourceOption.attr('selected','selected');
-            currentSource = sourceKey; 
-          } 
-        } else if (sourceIndex==0) { // if we haven't specified a default source but this is the first one, show the associated contextSelect
-          currentSource = sourceKey; 
-          $contextWrapper.css('display','inline-block');
-        }
-        sourceIndex += 1;
-        sourceData.contexts = (sourceData.contexts || []);
-        // create a sub-<select> menu for contexts, but only if this photo source has more than one possible context
-        if (sourceData.contexts.length > 1) {
-          var $contextSelect = $("<select class='select'></select>").change(updateSource);
-          $.each(sourceData.contexts, function(i,context){
-            var $contextOption = $("<option value='"+context[1]+"'>"+context[0]+"</option>");
-            // if searchable=true in context options, search box will be visible when this context is selected
-            // for example, if context is flickr public photos, we want to show the search box
-            // e.g. ["Public photos", "public", {searchable:true}]
-            var searchable = (context[2] && context[2].searchable);
-            if (searchable) { $contextOption.data('searchable', true); } 
-            if ((typeof options.defaultContext != 'undefined') && (options.defaultContext==context[1])) { // default context
-              $contextOption.attr('selected','selected');
-              if (searchable) { $searchWrapper.show() }
-            }
-            $contextSelect.append($contextOption);
-          });
-          $contextWrapper.append($contextSelect);
-        } else if (sourceData.contexts.length == 1) {
-          var context = sourceData.contexts[0],
-              searchable = (context[2] && context[2].searchable)
-          if (searchable) { $searchWrapper.show() }
-        }
-        sources[sourceKey].$contextWrapper = $contextWrapper;
-        $allContextWrappers.push($contextWrapper);
-      });
-
-
-      $sourceSelect.change(function(){
-        var sourceKey = $sourceSelect.val();
-        var sourceData = sources[sourceKey];
-        // show the associated context <select>, and hide all the other context <selects>
-        $.each($allContextWrappers, function(i,c) { 
-          if (sourceData.$contextWrapper && (sourceData.$contextWrapper==c)) {
-            c.show();
-          } else {
-            c.hide(); 
-          }
+    // Build source selector if necessary
+    if (
+      ( options.urls && options.urls.length > 0 )
+      || ( options.sources && options.sources.length > 0 )
+    ) {
+      // this branch is for backwards compatibility
+      // options.urls is used by legacy photoSelectors, but is now deprecated.
+      // use options.sources (see below) instead.
+      if (typeof options != 'undefined' && typeof options.urls != 'undefined') {
+        var urlSelect = $('<select class="select" style="margin: 0 auto"></select>');
+        urlSelect.change(function() {
+          $.fn.photoSelector.changeBaseUrl(wrapper, urlSelect.val());
         });
-        currentSource = sourceKey;
-        updateSource();
-      });
+        var urls = options.urls || [];
+        if (!options.skipLocal) {
+          urls.push({
+            title:  I18n.t('your_hard_drive'),
+            url: '/photos/local_photo_fields'
+          })
+        }
+        $.each(urls, function() {
+          if (this.url) {
+            var title = this.title;
+            var url = this.url;
+          } else {
+            var title = this;
+            var url = this;
+          }
+          var option = $('<option value="'+url+'">'+title+'</option>');
+          if (url === options.baseURL) $(option).attr('selected', 'selected');
+          $(urlSelect).append(option);
+        })
+        $sourceWrapper.append(urlSelect);
+      }
 
-      $sourceWrapper.append($sourceSelect);
-      $.each($allContextWrappers, function(i,c){ $sourceWrapper.append(c); });
+      // this branch is for options.sources (new style of photoselector)
+      if (typeof options != 'undefined' && typeof options.sources != 'undefined') {
+        // this is called when you change either the source <select> or context <select>
+        function updateSource(sourceOptions){
+          $searchWrapper.hide()
+          $searchInput.val('')
+          var newSource = sources[currentSource]
+          sourceOptions = sourceOptions || {}
+          sourceOptions['url'] = sourceOptions.url || newSource.url
+          sourceOptions['object_id'] = sourceOptions.object_id || false
+          if (typeof newSource.$contextWrapper == 'undefined') {
+            // TODO: this is what happens when there isn't a $contextSelect for this source (i.e. only one available context)
+            //sourceOptions['context'] = newSource.defaultContext;
+          } else {
+            var currentContextName = newSource.$contextWrapper.find('select').val()
+            sourceOptions['context'] = currentContextName
+            for (var i = 0; i < newSource.contexts.length; i++) {
+              var currentContext = newSource.contexts[i]
+              if (newSource.contexts[i][0] == currentContextName) break
+            }
+            if (currentContext && currentContext[currentContext.length-1].searchable) {
+              // show search field
+              $searchWrapper.show();
+            } else {
+              $searchWrapper.hide();
+            }
+          }
+          $.fn.photoSelector.changeBaseUrl(wrapper, sourceOptions['url'], sourceOptions['context'], sourceOptions['object_id']);
+        }
 
+        $searchWrapper.hide();
+        var sources = options.sources || {};
+        var currentSource = options.defaultSource;
+        var $allContextWrappers = [];
+        if (!options.skipLocal) { sources['local'] = {title: "Your computer", url: '/photos/local_photo_fields'}; }
+        var $sourceSelect = $("<select class='select'></select>");
+        var sourceIndex = 0; // used as index when iterating over sources below
+        $.each(sources, function(sourceKey, sourceData){
+          var $sourceOption = $("<option value='"+sourceKey+"'>"+sourceData.title+"</option>");
+          $sourceSelect.append($sourceOption);
+          var $contextWrapper = $("<span style='display:none'></span>");
+          if (typeof options.defaultSource != 'undefined') {
+            if (options.defaultSource == sourceKey) { // if we've specified a default source, and this it, show the associated contextSelect
+              $contextWrapper.css('display','inline-block');
+              $sourceOption.attr('selected','selected');
+              currentSource = sourceKey;
+            }
+          } else if (sourceIndex==0) { // if we haven't specified a default source but this is the first one, show the associated contextSelect
+            currentSource = sourceKey;
+            $contextWrapper.css('display','inline-block');
+          }
+          sourceIndex += 1;
+          sourceData.contexts = (sourceData.contexts || []);
+          // create a sub-<select> menu for contexts, but only if this photo source has more than one possible context
+          if (sourceData.contexts.length > 1) {
+            var $contextSelect = $("<select class='select'></select>").change(updateSource);
+            $.each(sourceData.contexts, function(i,context){
+              var $contextOption = $("<option value='"+context[1]+"'>"+context[0]+"</option>");
+              // if searchable=true in context options, search box will be visible when this context is selected
+              // for example, if context is flickr public photos, we want to show the search box
+              // e.g. ["Public photos", "public", {searchable:true}]
+              var searchable = (context[2] && context[2].searchable);
+              if (searchable) { $contextOption.data('searchable', true); }
+              if ((typeof options.defaultContext != 'undefined') && (options.defaultContext==context[1])) { // default context
+                $contextOption.attr('selected','selected');
+                if (searchable) { $searchWrapper.show() }
+              }
+              $contextSelect.append($contextOption);
+            });
+            $contextWrapper.append($contextSelect);
+          } else if (sourceData.contexts.length == 1) {
+            var context = sourceData.contexts[0],
+                searchable = (context[2] && context[2].searchable)
+            if (searchable) { $searchWrapper.show() }
+          }
+          sources[sourceKey].$contextWrapper = $contextWrapper;
+          $allContextWrappers.push($contextWrapper);
+        });
+
+
+        $sourceSelect.change(function(){
+          var sourceKey = $sourceSelect.val();
+          var sourceData = sources[sourceKey];
+          // show the associated context <select>, and hide all the other context <selects>
+          $.each($allContextWrappers, function(i,c) {
+            if (sourceData.$contextWrapper && (sourceData.$contextWrapper==c)) {
+              c.show();
+            } else {
+              c.hide();
+            }
+          });
+          currentSource = sourceKey;
+          updateSource();
+        });
+
+        $sourceWrapper.append($sourceSelect);
+        $.each($allContextWrappers, function(i,c){ $sourceWrapper.append(c); });
+      }
     }
-  
-    $(wrapper).on('click', '.back_to_albums', function(){
-      try { updateSource({ object_id: $(this).attr('data-friend_id') }); } 
-      catch(e) {
-        $.fn.photoSelector.changeBaseUrl(
-          wrapper, 
-          urlSelect.val(), 
-          'user', //contextSelect.val(), 
-          $(this).attr('data-friend_id'));
-      }
-      return false;
-    });
-
-    $(wrapper).on('click', '.back_to_friends', function(){
-      try { updateSource(); } 
-      catch(e) { $.fn.photoSelector.changeBaseUrl(wrapper, urlSelect.val()); }
-      return false;
-    });
-
-    $(wrapper).on('click', '.back_to_groups', function(){
-      try { updateSource({ object_id: $(this).attr('data-group_id') }); } 
-      catch(e) {
-        $.fn.photoSelector.changeBaseUrl(
-          wrapper, 
-          urlSelect.val(), 
-          'groups', //contextSelect.val(), 
-          $(this).attr('data-group_id'));
-      }
-      return false;
-    });
-
-    // friend selector
-    $(wrapper).on('click', '.friendSelector .friend', function(){
-      try { updateSource({ object_id: $(this).attr('data-friend_id') }); } 
-      catch(e) {
-        $.fn.photoSelector.changeBaseUrl(
-          wrapper, 
-          urlSelect.val(), 
-          'user', // contextSelect.val(), 
-          $(this).attr('data-friend_id'));
-      }
-      return false;
-    });
-
     
     // Append next & prev links
     var page = $('<input class="photoSelectorPage" type="hidden" value="1"/>')
@@ -396,7 +353,16 @@
       clear: 'both'})
     )
     
-    $(wrapper).append(controls);
+    // We don't need search controls if we're only showing local photos,
+    // otherwise we do
+    const searchControlsRequired = !(
+      options.baseURL?.match( /local_photo/ )
+      && ( !options.sources || options.sources.length === 0 )
+      && ( !options.urls || options.urls.length === 0 )
+    );
+    if ( searchControlsRequired ) {
+      $( wrapper ).append( controls );
+    }
     
     if (options.baseURL && options.baseURL.match(/local_photo/) && $sourceWrapper.find('select').length != 0) {
       $('.nextlink, .prevlink, .allNone, .photoSelectorSearch', wrapper).hide()
@@ -424,7 +390,7 @@
         return false;
       };
     });
-  }
+  };
   
   $.fn.photoSelector.changeBaseUrl = function(wrapper, url, context, object_id) {
     var options = $(wrapper).data('photoSelectorOptions');