38fd85a921e73a72d753d4830294d41b1889c14f
Patrick Leary <patrick.r.leary@gmail.com>
2025-07-17 16:57:08 -0400

    fix: ensure proper query context for replica OPS-160

--- app/controllers/admin/queries_controller.rb
+++ app/controllers/admin/queries_controller.rb
@@ -3,19 +3,22 @@
 class Admin::QueriesController < ApplicationController
   before_action :authenticate_user!
   before_action :admin_required
+  prepend_around_action :enable_replica_and_release_context, only: :replica
 
   def index
     redirect_to :primary_admin_queries
   end
 
   def primary
+    @source = :primary
     @queries = active_queries( :primary )
-    render layout: "admin"
+    render "admin/queries/queries_table", layout: "admin"
   end
 
   def replica
+    @source = :replica
     @queries = active_queries( :replica )
-    render layout: "admin"
+    render "admin/queries/queries_table", layout: "admin"
   end
 
   private
@@ -38,5 +41,17 @@ class Admin::QueriesController < ApplicationController
     end
 
     queries.sort_by {| q | q["duration"] }.reverse
+  end
+
+  def enable_replica_and_release_context
+    begin
+      ActiveRecord::Base.connection.enable_replica
+      Makara::Context.release_all
+      yield
+    rescue Makara::Errors::AllConnectionsBlacklisted
+      yield
+    ensure
+      ActiveRecord::Base.connection.disable_replica
+    end
   end
 end
