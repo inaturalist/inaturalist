935ed8fe338c3d3b678ec1e4cd6f88a416ce7436
loarie <loarie@gmail.com>
2025-08-26 20:19:56 -0700

    translation bug

--- db/migrate/20250826232001_add_status_to_taxon_changes.rb
+++ db/migrate/20250826232001_add_status_to_taxon_changes.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 class AddStatusToTaxonChanges < ActiveRecord::Migration[6.1]
-  def change
+  def up
     add_column :taxon_changes, :status, :string, default: "draft", null: false
+
+    # backfill: any record with committed_on not null becomes 'committed'
+    execute <<~SQL
+      UPDATE taxon_changes
+      SET status = 'committed'
+      WHERE committed_on IS NOT NULL
+    SQL
+  end
+
+  def down
+    remove_column :taxon_changes, :status
   end
 end
