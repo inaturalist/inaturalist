3319745f15f199d7860069070d1ef996087b6c71
sylvain-morin <sylvain.morin@inaturalist.org>
2025-12-02 22:04:55 +0100

    feat: open the demo

--- spec/controllers/id_summaries_demo_controller_spec.rb
+++ spec/controllers/id_summaries_demo_controller_spec.rb
@@ -3,37 +3,43 @@ require File.dirname(__FILE__) + "/../spec_helper"
 describe IdSummariesDemoController, "index" do
   let(:user) { User.make! }
 
-  context "when no allowlist is configured" do
-    before do
-      allow( CONFIG ).to receive_message_chain( :id_summaries_demo, :allowed_user_ids ).and_return( nil )
-    end
-
-    it "allows admins" do
-      sign_in make_admin
+  context "without admin mode enabled" do
+    it "allows non-admin users" do
+      sign_in user
       get :index
       expect( response ).to be_successful
     end
   end
 
-  context "when allowlist is configured" do
-    let(:authorized_user) { User.make! }
-    let(:allowlist) { [authorized_user.id] }
+  context "with admin mode enabled" do
+    let(:admin) { make_admin }
+    let(:message) { I18n.t( :you_dont_have_permission_to_do_that ) }
 
-    before do
-      allow( CONFIG ).to receive_message_chain( :id_summaries_demo, :allowed_user_ids ).and_return( allowlist )
-    end
-
-    it "allows listed users" do
-      sign_in authorized_user
-      get :index
+    it "allows admins" do
+      sign_in admin
+      get :index, params: { admin_mode: true }
       expect( response ).to be_successful
     end
 
-    it "denies unlisted non-admin users" do
+    it "denies non-admin users for HTML requests" do
       sign_in user
-      get :index
+      get :index, params: { admin_mode: true }
       expect( response ).to redirect_to( root_url )
-      expect( flash[:error] ).to eq I18n.t( :you_dont_have_permission_to_do_that )
+      expect( flash[:error] ).to eq message
+    end
+
+    it "returns forbidden for JSON requests" do
+      sign_in user
+      get :index, params: { admin_mode: true }, format: :json
+      expect( response ).to have_http_status( :forbidden )
+      expect( JSON.parse( response.body )["error"] ).to eq message
+    end
+
+    it "returns forbidden for JS requests" do
+      sign_in user
+      get :index, params: { admin_mode: true }, format: :js
+      expect( response ).to have_http_status( :forbidden )
+      expect( response.body ).to eq message
     end
   end
 end
