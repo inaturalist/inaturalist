b5c17424ea6956567d4ae5ba9c55920730f5199c
Sylvain Morin <56630013+sylvain-morin@users.noreply.github.com>
2025-12-03 20:07:34 +0100

    Deploy id summaries (#4733)
    * feat: open the demo
    
    * feat: open the demo
    
    * feat: open the demo

--- app/controllers/id_summaries_demo_controller.rb
+++ app/controllers/id_summaries_demo_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class IdSummariesDemoController < ApplicationController
-  before_action :require_id_summaries_demo_access
+  before_action :restrict_admin_mode
 
   def index
     render layout: "basic"
@@ -9,11 +9,9 @@ class IdSummariesDemoController < ApplicationController
 
   private
 
-  def require_id_summaries_demo_access
-    allowed_ids = Array.wrap( CONFIG.id_summaries_demo&.allowed_user_ids ).map( &:to_i ).reject( &:zero? )
-    return admin_required if allowed_ids.blank?
+  def restrict_admin_mode
+    return true unless params[:admin_mode].present?
     return true if logged_in? && current_user.is_admin?
-    return true if logged_in? && allowed_ids.include?( current_user.id )
 
     message = t( :you_dont_have_permission_to_do_that )
     respond_to do |format|
