ba34ccd008c87ab247d3dd264d80fef35b809d50
Sylvain Morin <56630013+sylvain-morin@users.noreply.github.com>
2026-01-26 11:38:12 +0100

    feat: ensure script errors are reported with TaskLogger for cron tasks (WEB-899)

--- tools/virtuous_donation_sync.rb
+++ tools/virtuous_donation_sync.rb
@@ -26,188 +26,193 @@ if opts.log_task_name
   task_logger = TaskLogger.new( opts.log_task_name, nil, "sync" )
 end
 
-task_logger&.start
-start_time = Time.now
+begin
+  task_logger&.start
+  start_time = Time.now
 
-full_contact_endpoint = "https://api.virtuoussoftware.com/api/Contact/Query/FullContact"
-gift_query_endpoint = "https://api.virtuoussoftware.com/api/Gift/Query"
+  full_contact_endpoint = "https://api.virtuoussoftware.com/api/Contact/Query/FullContact"
+  gift_query_endpoint = "https://api.virtuoussoftware.com/api/Gift/Query"
 
-potential_errors = [
-  Timeout::Error,
-  RestClient::ServiceUnavailable,
-  RestClient::GatewayTimeout,
-  RestClient::TooManyRequests,
-  RestClient::InternalServerError,
-  RestClient::BadGateway
-]
+  potential_errors = [
+    Timeout::Error,
+    RestClient::ServiceUnavailable,
+    RestClient::GatewayTimeout,
+    RestClient::TooManyRequests,
+    RestClient::InternalServerError,
+    RestClient::BadGateway
+  ]
 
-total_verified_users = 0
-new_verified_users = 0
-new_verified_user_parents = 0
-failed_users = 0
-failed_parents = 0
-user_donations = 0
-donors = {}
+  total_verified_users = 0
+  new_verified_users = 0
+  new_verified_user_parents = 0
+  failed_users = 0
+  failed_parents = 0
+  user_donations = 0
+  donors = {}
 
-last_gift_id = nil
+  last_gift_id = nil
 
-loop do
-  conditions = []
-  if opts.after_date
-    conditions << {
-      parameter: "Gift Date",
-      operator: "GreaterThan",
-      value: opts.after_date
+  loop do
+    conditions = []
+    if opts.after_date
+      conditions << {
+        parameter: "Gift Date",
+        operator: "GreaterThan",
+        value: opts.after_date
+      }
+    end
+    if last_gift_id
+      conditions << {
+        parameter: "Gift Id",
+        operator: "GreaterThan",
+        value: last_gift_id
+      }
+    end
+    post_body = {
+      groups: [{
+        conditions: conditions
+      }],
+      sortBy: "Id",
+      descending: "false"
     }
-  end
-  if last_gift_id
-    conditions << {
-      parameter: "Gift Id",
-      operator: "GreaterThan",
-      value: last_gift_id
+    url = "#{gift_query_endpoint}?take=1000"
+    puts "Calling #{url} with:"
+    pp post_body
+
+    # fetch a batch of gifts
+    response = try_and_try_again( potential_errors, exponential_backoff: true, sleep: 3 ) do
+      RestClient.post( url, post_body.to_json, {
+        Authorization: "Bearer #{CONFIG.virtuous.token}",
+        content_type: :json,
+        accept: :json
+      } )
+    end
+    break if response&.code != 200
+
+    puts "Ratelimit remaining: #{response.headers[:x_ratelimit_remaining]}"
+    response_body = JSON.parse( response.body )
+    break if response_body["list"].empty?
+
+    puts "Total results: #{response_body['total']}"
+    puts "Run time: #{( Time.now - start_time ).round( 2 )}s"
+    puts
+
+    # fetch the contacts for the batch of gifts
+    contact_ids = response_body["list"].map {| gift | gift["contactId"] }
+    contacts_post_body = {
+      groups: [],
+      sortBy: "Id",
+      descending: "false"
     }
-  end
-  post_body = {
-    groups: [{
-      conditions: conditions
-    }],
-    sortBy: "Id",
-    descending: "false"
-  }
-  url = "#{gift_query_endpoint}?take=1000"
-  puts "Calling #{url} with:"
-  pp post_body
+    contact_ids.each do | contact_id |
+      contacts_post_body[:groups] << {
+        conditions: [{
+          parameter: "Contact Id",
+          operator: "Is",
+          value: contact_id
+        }]
+      }
+    end
+    contacts_url = "#{full_contact_endpoint}?take=1000"
+    puts "Calling #{contacts_url} with long post body"
 
-  # fetch a batch of gifts
-  response = try_and_try_again( potential_errors, exponential_backoff: true, sleep: 3 ) do
-    RestClient.post( url, post_body.to_json, {
-      Authorization: "Bearer #{CONFIG.virtuous.token}",
-      content_type: :json,
-      accept: :json
-    } )
-  end
-  break if response&.code != 200
+    contacts_response = try_and_try_again( potential_errors, exponential_backoff: true, sleep: 3 ) do
+      RestClient.post( contacts_url, contacts_post_body.to_json, {
+        Authorization: "Bearer #{CONFIG.virtuous.token}",
+        content_type: :json,
+        accept: :json
+      } )
+    end
+    next unless contacts_response&.code == 200
 
-  puts "Ratelimit remaining: #{response.headers[:x_ratelimit_remaining]}"
-  response_body = JSON.parse( response.body )
-  break if response_body["list"].empty?
+    puts "Ratelimit remaining: #{contacts_response.headers[:x_ratelimit_remaining]}"
+    puts
 
-  puts "Total results: #{response_body['total']}"
-  puts "Run time: #{( Time.now - start_time ).round( 2 )}s"
-  puts
+    contact_inaturalist_users = {}
+    contact_user_parents = {}
+    contacts_response_body = JSON.parse( contacts_response.body )
+    contacts_response_body["list"]&.each do | contact |
+      contact_id = contact["id"]
+      contact["contactIndividuals"]&.each do | individual |
+        individual["contactMethods"]&.each do | contact_method |
+          next unless contact_method["type"].downcase.include?( "email" )
+          next unless contact_method["value"].include?( "@" )
 
-  # fetch the contacts for the batch of gifts
-  contact_ids = response_body["list"].map {| gift | gift["contactId"] }
-  contacts_post_body = {
-    groups: [],
-    sortBy: "Id",
-    descending: "false"
-  }
-  contact_ids.each do | contact_id |
-    contacts_post_body[:groups] << {
-      conditions: [{
-        parameter: "Contact Id",
-        operator: "Is",
-        value: contact_id
-      }]
-    }
-  end
-  contacts_url = "#{full_contact_endpoint}?take=1000"
-  puts "Calling #{contacts_url} with long post body"
-
-  contacts_response = try_and_try_again( potential_errors, exponential_backoff: true, sleep: 3 ) do
-    RestClient.post( contacts_url, contacts_post_body.to_json, {
-      Authorization: "Bearer #{CONFIG.virtuous.token}",
-      content_type: :json,
-      accept: :json
-    } )
-  end
-  next unless contacts_response&.code == 200
-
-  puts "Ratelimit remaining: #{contacts_response.headers[:x_ratelimit_remaining]}"
-  puts
-
-  contact_inaturalist_users = {}
-  contact_user_parents = {}
-  contacts_response_body = JSON.parse( contacts_response.body )
-  contacts_response_body["list"]&.each do | contact |
-    contact_id = contact["id"]
-    contact["contactIndividuals"]&.each do | individual |
-      individual["contactMethods"]&.each do | contact_method |
-        next unless contact_method["type"].downcase.include?( "email" )
-        next unless contact_method["value"].include?( "@" )
-
-        if ( inaturalist_user = User.find_by_email( contact_method["value"].downcase ) )
-          contact_inaturalist_users[contact_id] ||= []
-          contact_inaturalist_users[contact_id] << inaturalist_user
-          if inaturalist_user.parentages.length.positive?
+          if ( inaturalist_user = User.find_by_email( contact_method["value"].downcase ) )
+            contact_inaturalist_users[contact_id] ||= []
+            contact_inaturalist_users[contact_id] << inaturalist_user
+            if inaturalist_user.parentages.length.positive?
+              contact_user_parents[contact_id] ||= []
+              contact_user_parents[contact_id] += inaturalist_user.parentages
+            end
+          end
+          user_parents = UserParent.where( email: contact_method["value"].downcase )
+          if user_parents.length.positive?
             contact_user_parents[contact_id] ||= []
-            contact_user_parents[contact_id] += inaturalist_user.parentages
+            contact_user_parents[contact_id] += user_parents
           end
         end
-        user_parents = UserParent.where( email: contact_method["value"].downcase )
-        if user_parents.length.positive?
-          contact_user_parents[contact_id] ||= []
-          contact_user_parents[contact_id] += user_parents
-        end
       end
     end
-  end
 
-  response_body["list"].each do | gift |
-    contact_id = gift["contactId"]
-    donors[contact_id] = true
-    contact_inaturalist_users[contact_id]&.uniq&.each do | user |
-      puts "\tDonor: #{user.donor?}" if opts.debug
-      if user.virtuous_donor_contact_id.blank?
-        if opts.dry || user.update( virtuous_donor_contact_id: contact_id )
-          puts "\tMarked #{user} as a donor"
-          new_verified_users += 1
-        else
-          puts "Failed to mark #{user} as a donor: #{user.errors.full_messages.to_sentence}"
-          failed_users += 1
+    response_body["list"].each do | gift |
+      contact_id = gift["contactId"]
+      donors[contact_id] = true
+      contact_inaturalist_users[contact_id]&.uniq&.each do | user |
+        puts "\tDonor: #{user.donor?}" if opts.debug
+        if user.virtuous_donor_contact_id.blank?
+          if opts.dry || user.update( virtuous_donor_contact_id: contact_id )
+            puts "\tMarked #{user} as a donor"
+            new_verified_users += 1
+          else
+            puts "Failed to mark #{user} as a donor: #{user.errors.full_messages.to_sentence}"
+            failed_users += 1
+          end
         end
+        total_verified_users += 1
+
+        gift_date = Date.strptime( gift["giftDate"], "%m/%e/%Y" )
+        next if UserDonation.where( user: user ).where( "DATE(donated_at) = ?", gift_date ).exists?
+
+        puts "\tAdding donation for #{user} on #{gift['giftDate']}"
+        unless opts.dry
+          UserDonation.create( user: user, donated_at: gift_date )
+        end
+        user_donations += 1
       end
-      total_verified_users += 1
 
-      gift_date = Date.strptime( gift["giftDate"], "%m/%e/%Y" )
-      next if UserDonation.where( user: user ).where( "DATE(donated_at) = ?", gift_date ).exists?
+      contact_user_parents[contact_id]&.uniq&.each do | user_parent |
+        next unless user_parent.virtuous_donor_contact_id.blank?
 
-      puts "\tAdding donation for #{user} on #{gift['giftDate']}"
-      unless opts.dry
-        UserDonation.create( user: user, donated_at: gift_date )
-      end
-      user_donations += 1
-    end
-
-    contact_user_parents[contact_id]&.uniq&.each do | user_parent |
-      next unless user_parent.virtuous_donor_contact_id.blank?
-
-      begin
-        if opts.dry || user_parent.update( virtuous_donor_contact_id: contact_id )
-          puts "\tMarked #{user_parent} as a donor"
-          new_verified_user_parents += 1
-        else
-          puts "Failed to mark #{user_parent} as a donor: #{user_parent.errors.full_messages.to_sentence}"
+        begin
+          if opts.dry || user_parent.update( virtuous_donor_contact_id: contact_id )
+            puts "\tMarked #{user_parent} as a donor"
+            new_verified_user_parents += 1
+          else
+            puts "Failed to mark #{user_parent} as a donor: #{user_parent.errors.full_messages.to_sentence}"
+            failed_parents += 1
+          end
+        rescue OpenSSL::SSL::SSLError, Net::ReadTimeout => e
+          # Mail failed to send
+          puts "Failed to mark #{user_parent} as a donor: mail delivery failed (#{e})"
           failed_parents += 1
         end
-      rescue OpenSSL::SSL::SSLError, Net::ReadTimeout => e
-        # Mail failed to send
-        puts "Failed to mark #{user_parent} as a donor: mail delivery failed (#{e})"
-        failed_parents += 1
       end
+
+      last_gift_id = gift["id"]
     end
-
-    last_gift_id = gift["id"]
   end
+
+  puts
+  puts "#{donors.size} donors, #{total_verified_users} donor users, " \
+    "#{new_verified_users} new donor users, #{new_verified_user_parents} new " \
+    "donor parents, #{failed_users} failed users, #{failed_parents} failed " \
+    "parents, #{user_donations} user donations in " \
+    "#{( Time.now - start_time ).round( 2 )}s"
+  puts
+rescue => e # rubocop:disable Style/RescueStandardError
+  task_logger&.error( "#{e}\n#{e.backtrace[0..30].join( "\n" )}" )
+  raise
+ensure
+  task_logger&.end
 end
-
-puts
-puts "#{donors.size} donors, #{total_verified_users} donor users, " \
-  "#{new_verified_users} new donor users, #{new_verified_user_parents} new " \
-  "donor parents, #{failed_users} failed users, #{failed_parents} failed " \
-  "parents, #{user_donations} user donations in " \
-  "#{( Time.now - start_time ).round( 2 )}s"
-puts
-
-task_logger&.end
--- tools/virtuous_user_tag_sync.rb
+++ tools/virtuous_user_tag_sync.rb
@@ -25,115 +25,120 @@ if opts.log_task_name
   task_logger = TaskLogger.new( opts.log_task_name, nil, "sync" )
 end
 
-task_logger&.start
-start_time = Time.now
+begin
+  task_logger&.start
+  start_time = Time.now
 
-full_contact_endpoint = "https://api.virtuoussoftware.com/api/Contact/Query/FullContact"
+  full_contact_endpoint = "https://api.virtuoussoftware.com/api/Contact/Query/FullContact"
 
-potential_errors = [
-  Timeout::Error,
-  RestClient::ServiceUnavailable,
-  RestClient::GatewayTimeout,
-  RestClient::TooManyRequests,
-  RestClient::InternalServerError,
-  RestClient::BadGateway
-]
+  potential_errors = [
+    Timeout::Error,
+    RestClient::ServiceUnavailable,
+    RestClient::GatewayTimeout,
+    RestClient::TooManyRequests,
+    RestClient::InternalServerError,
+    RestClient::BadGateway
+  ]
 
-tag_users = UserVirtuousTag::POSSIBLE_TAGS.to_h {| k | [k, []] }
-last_contact_id = nil
+  tag_users = UserVirtuousTag::POSSIBLE_TAGS.to_h {| k | [k, []] }
+  last_contact_id = nil
 
-loop do
-  conditions = [{
-    parameter: "Tag",
-    operator: "IsAnyOf",
-    values: UserVirtuousTag::POSSIBLE_TAGS
-  }]
-  if last_contact_id
-    conditions << {
-      parameter: "Contact Id",
-      operator: "GreaterThan",
-      value: last_contact_id
+  loop do
+    conditions = [{
+      parameter: "Tag",
+      operator: "IsAnyOf",
+      values: UserVirtuousTag::POSSIBLE_TAGS
+    }]
+    if last_contact_id
+      conditions << {
+        parameter: "Contact Id",
+        operator: "GreaterThan",
+        value: last_contact_id
+      }
+    end
+    post_body = {
+      groups: [{
+        conditions: conditions
+      }],
+      sortBy: "Id",
+      descending: "false"
     }
-  end
-  post_body = {
-    groups: [{
-      conditions: conditions
-    }],
-    sortBy: "Id",
-    descending: "false"
-  }
-  url = "#{full_contact_endpoint}?take=1000"
-  puts "Calling #{url} with:"
-  pp post_body
+    url = "#{full_contact_endpoint}?take=1000"
+    puts "Calling #{url} with:"
+    pp post_body
 
-  response = try_and_try_again( potential_errors, exponential_backoff: true, sleep: 3 ) do
-    RestClient.post( url, post_body.to_json, {
-      Authorization: "Bearer #{CONFIG.virtuous.token}",
-      content_type: :json,
-      accept: :json
-    } )
-  end
-  break if response&.code != 200
+    response = try_and_try_again( potential_errors, exponential_backoff: true, sleep: 3 ) do
+      RestClient.post( url, post_body.to_json, {
+        Authorization: "Bearer #{CONFIG.virtuous.token}",
+        content_type: :json,
+        accept: :json
+      } )
+    end
+    break if response&.code != 200
 
-  puts "Ratelimit remaining: #{response.headers[:x_ratelimit_remaining]}"
-  puts
+    puts "Ratelimit remaining: #{response.headers[:x_ratelimit_remaining]}"
+    puts
 
-  response_body = JSON.parse( response.body )
-  break if response_body["list"].empty?
+    response_body = JSON.parse( response.body )
+    break if response_body["list"].empty?
 
-  response_body["list"]&.each do | contact |
-    contact_inaturalist_user_ids = []
-    contact["contactIndividuals"]&.each do | individual |
-      individual["contactMethods"]&.each do | contact_method |
-        next unless contact_method["type"].downcase.include?( "email" )
-        next unless contact_method["value"].include?( "@" )
+    response_body["list"]&.each do | contact |
+      contact_inaturalist_user_ids = []
+      contact["contactIndividuals"]&.each do | individual |
+        individual["contactMethods"]&.each do | contact_method |
+          next unless contact_method["type"].downcase.include?( "email" )
+          next unless contact_method["value"].include?( "@" )
 
-        inaturalist_user = User.find_by_email( contact_method["value"].downcase )
-        next unless inaturalist_user
+          inaturalist_user = User.find_by_email( contact_method["value"].downcase )
+          next unless inaturalist_user
 
-        contact_inaturalist_user_ids << inaturalist_user.id
+          contact_inaturalist_user_ids << inaturalist_user.id
+        end
       end
-    end
 
-    contact["tags"].each do | tag |
-      next unless tag_users[tag]
+      contact["tags"].each do | tag |
+        next unless tag_users[tag]
 
-      tag_users[tag] += contact_inaturalist_user_ids
+        tag_users[tag] += contact_inaturalist_user_ids
+      end
+      last_contact_id = contact["id"]
     end
-    last_contact_id = contact["id"]
   end
-end
 
-user_tags_created = 0
-tag_users.each do | tag, user_ids |
-  user_ids.uniq.each do | user_id |
-    next if UserVirtuousTag.where( user_id: user_id, virtuous_tag: tag ).exists?
+  user_tags_created = 0
+  tag_users.each do | tag, user_ids |
+    user_ids.uniq.each do | user_id |
+      next if UserVirtuousTag.where( user_id: user_id, virtuous_tag: tag ).exists?
 
-    puts "Creating new UserVirtuousTag => { user_id: #{user_id}, virtuous_tag: #{tag} }"
-    user_tags_created += 1
+      puts "Creating new UserVirtuousTag => { user_id: #{user_id}, virtuous_tag: #{tag} }"
+      user_tags_created += 1
+      next if opts.dry
+
+      UserVirtuousTag.create( user_id: user_id, virtuous_tag: tag )
+    end
+  end
+
+  user_tags_removed = 0
+  UserVirtuousTag.find_each do | uvt |
+    next if tag_users[uvt.virtuous_tag]&.include?( uvt.user_id )
+
+    puts "Deleting vestigial UserVirtuousTag => { user_id: #{uvt.user_id}, virtuous_tag: #{uvt.virtuous_tag} }"
+    user_tags_removed += 1
     next if opts.dry
 
-    UserVirtuousTag.create( user_id: user_id, virtuous_tag: tag )
+    UserVirtuousTag.where( user_id: uvt.user_id, virtuous_tag: uvt.virtuous_tag ).delete_all
   end
+
+  total_user_tags = tag_users.sum {| _k, v | v.size }
+  puts
+  puts "Total User Tags: #{total_user_tags}"
+  puts "User Tags Created: #{user_tags_created}"
+  puts "User Tags Removed: #{user_tags_removed}"
+  puts "Completed In: #{( Time.now - start_time ).round( 2 )}s"
+  puts
+rescue => e # rubocop:disable Style/RescueStandardError
+  task_logger&.error( "#{e}\n#{e.backtrace[0..30].join( "\n" )}" )
+  raise
+ensure
+  task_logger&.end
 end
-
-user_tags_removed = 0
-UserVirtuousTag.find_each do | uvt |
-  next if tag_users[uvt.virtuous_tag]&.include?( uvt.user_id )
-
-  puts "Deleting vestigial UserVirtuousTag => { user_id: #{uvt.user_id}, virtuous_tag: #{uvt.virtuous_tag} }"
-  user_tags_removed += 1
-  next if opts.dry
-
-  UserVirtuousTag.where( user_id: uvt.user_id, virtuous_tag: uvt.virtuous_tag ).delete_all
-end
-
-total_user_tags = tag_users.sum {| _k, v | v.size }
-puts
-puts "Total User Tags: #{total_user_tags}"
-puts "User Tags Created: #{user_tags_created}"
-puts "User Tags Removed: #{user_tags_removed}"
-puts "Completed In: #{( Time.now - start_time ).round( 2 )}s"
-puts
-
-task_logger&.end
