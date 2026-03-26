1646893b162670234422a38179ebbfa09c149883
Dustin Baxter <baxterd@gmail.com>
2026-03-26 11:27:19 -0400

    load optimizations

--- app/controllers/welcome_v2_controller.rb
+++ app/controllers/welcome_v2_controller.rb
@@ -3,6 +3,8 @@
 class WelcomeV2Controller < ApplicationController
   def index
     @responsive = true
+    @skip_external_connections = true
+    @skip_react = true
     render layout: "bootstrap"
   end
 end
