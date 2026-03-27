f6cd997f39791681258e57a8ed7ddc5e0470381a
Casey Hofford <casey.hofford@gmail.com>
2026-03-27 10:52:31 -0700

    Revert "wip form"
    This reverts commit 8598c4d6c47f7f9fa42cf87d35f522e384383931.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -77,8 +77,7 @@ class UsersController < ApplicationController
     @moderator_action = ModeratorAction.new(
       resource: @user,
       user: current_user,
-      action: ModeratorAction::SUSPEND,
-      suspended_until: params[:suspend_until]
+      action: ModeratorAction::SUSPEND
     )
     render layout: "bootstrap"
   end
