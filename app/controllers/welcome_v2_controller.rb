c9609909f9a65d7360f22cd13dad544cd4660a5d
Dustin Baxter <baxterd@gmail.com>
2026-04-03 16:31:43 -0400

    cleanup and units

--- app/controllers/welcome_v2_controller.rb
+++ app/controllers/welcome_v2_controller.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
 class WelcomeV2Controller < ApplicationController
-  # TODO: load and freeze observations as constant on controller initialization?
+  unless defined?( OBSERVATIONS_DATA )
+    OBSERVATIONS_DATA = JSON.parse(
+      File.read( Rails.root.join( "app/assets/images/welcome_v2/observations/observations.json" ) )
+    ).freeze
+  end
 
   def index
     @responsive = true
     @skip_external_connections = true
     @skip_react = true
-    data = JSON.parse(
-      File.read( Rails.root.join( "app/assets/images/welcome_v2/observations/observations.json" ) )
-    )
-    if data.is_a?( Array )
-      @explore_observations = data
-    else
-      @explore_observations = data["explore"] || []
-      @sample_observation   = data["sample"]
-      @story_common_names   = data["stories"] || {}
-    end
+    @explore_observations = OBSERVATIONS_DATA["explore"] || []
+    @sample_observation   = OBSERVATIONS_DATA["sample"]
+    @story_common_names   = OBSERVATIONS_DATA["stories"] || {}
+    @locale_key           = I18n.locale.to_s
+    @locale_base          = @locale_key.split( "-" ).first
+
     render layout: "bootstrap"
   end
 end
