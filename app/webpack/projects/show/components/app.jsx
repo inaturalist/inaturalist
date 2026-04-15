928f1a05f2a6b59c910f02ebaa142627879dab0c
Patrick Leary <patrick.r.leary@gmail.com>
2026-04-15 10:32:11 -0400

    apply to collections and umbrellas

--- app/webpack/projects/show/components/app.jsx
+++ app/webpack/projects/show/components/app.jsx
@@ -107,9 +107,9 @@ const App = ( {
     <div className="header-members-button">
       <button
         type="button"
-        className={`btn btn-nostyle action clicky ${project.project_type === "collection" && !project.currentUserIsMember && loggedIn ? "inverted" : ""}`}
+        className={`btn btn-nostyle action clicky ${!project.currentUserIsMember && loggedIn ? "inverted" : ""}`}
         onClick={membershipAction}
-        style={project.project_type === "collection" && !project.currentUserIsMember && loggedIn ? {
+        style={!project.currentUserIsMember && loggedIn ? {
           color: `rgba(${colorRGB.r},${colorRGB.g},${colorRGB.b},1)`
         } : null}
       >
