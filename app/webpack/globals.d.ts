c014b94706b2a6fd876ad2cdf9d548b7a987c7fc
Dustin Baxter <baxterd@gmail.com>
2026-04-27 17:00:32 -0400

    leader wip

--- app/webpack/globals.d.ts
+++ app/webpack/globals.d.ts
@@ -1,3 +1,4 @@
 declare const I18n: {
   t: ( key: string, options?: Record<string, unknown> ) => string;
+  toNumber: ( value: number, options?: Record<string, unknown> ) => string;
 };
---
+++ app/webpack/taxa/show/components/leaders.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import TopObserverContainer from "../containers/top_observer_container";
+import TopIdentifierContainer from "../containers/top_identifier_container";
+import NumSpeciesContainer from "../containers/num_species_container";
+import LastObservationContainer from "../containers/last_observation_container";
+import NumObservationsContainer from "../containers/num_observations_container";
+
+interface Taxon {
+  rank_level?: number;
+  complete_species_count?: number;
+}
+
+interface LeadersProps {
+  taxon: Taxon;
+}
+
+const Leaders = ( { taxon }: LeadersProps ) => {
+  const optional = ( taxon.rank_level ?? 0 ) > 10 && taxon.complete_species_count
+    ? <NumSpeciesContainer />
+    : <LastObservationContainer />;
+  return (
+    <div className="Leaders">
+      <div className="leaders-grid">
+        <TopObserverContainer />
+        <TopIdentifierContainer />
+        { optional }
+        <NumObservationsContainer />
+      </div>
+    </div>
+  );
+};
+
+export default Leaders;
