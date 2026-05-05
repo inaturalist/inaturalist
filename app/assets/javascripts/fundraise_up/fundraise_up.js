d8515bf5c56a920d0f24528bc29441b7497409ae
Casey Hofford <casey.hofford@gmail.com>
2026-05-05 13:15:06 -0700

    Improve fallback button styling

--- app/assets/javascripts/fundraise_up/fundraise_up.js
+++ app/assets/javascripts/fundraise_up/fundraise_up.js
@@ -1,13 +1,13 @@
 function initFruFallback( containerSelector ) {
   var container = document.querySelector( containerSelector );
   if ( !container ) return;
-  var fallback = container.querySelector( ".fru-fallback" );
+  var fallback = container.querySelector( ".fru-fallback-link" );
   if ( !fallback ) return;
-  var fruAnchor = container.querySelector( "a[href^=\"#\"][style*=\"display\"]" );
+  var fruAnchor = container.querySelector( "a:empty[href^=\"#\"][style*=\"display: none\"]" );
   if ( fruAnchor ) {
-    fruAnchor.parentNode.insertBefore( fallback, fruAnchor.nextSibling );
+    container.insertBefore( fallback, container.lastElementChild );
   }
-  var observer = new MutationObserver( function( ) {
+  var observer = new MutationObserver( function ( ) {
     if ( container.querySelector( "iframe" ) ) {
       fallback.style.display = "none";
       observer.disconnect( );
