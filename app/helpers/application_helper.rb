a00504b0c227b69d30761c4be91f7434b41d0cc0
Casey Hofford <casey.hofford@gmail.com>
2026-05-11 16:26:34 -0700

    remove #fru_donate_url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1828,8 +1828,4 @@ module ApplicationHelper
       </script>
     HTML
   end
-
-  def fru_donate_url
-    "https://giving.inaturalist.org/login?form=supportinaturalist&segment=renmobile26&utm_campaign=2026ren&utm_medium=mobile&utm_source=iosandroid&utm_content=button&utm_term=2389&redirectTo=/"
-  end
 end
