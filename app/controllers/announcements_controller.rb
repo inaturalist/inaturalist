650e75f71ae23d393ca885a96a6666a8c21f2c6a
Casey Hofford <casey.hofford@gmail.com>
2026-05-14 09:31:16 -0700

    display variants on announcements

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -41,6 +41,13 @@ class AnnouncementsController < ApplicationController
   end
 
   def show
+    @family_root = @announcement.parent_announcement || @announcement
+    @translation_variants = if @announcement.parent_announcement_id.present?
+      [@family_root] + @family_root.child_announcements.where.not( id: @announcement.id ).order( :id )
+    else
+      @announcement.child_announcements.order( :id )
+    end
+
     respond_to do | format |
       format.html do
         if params[:body]
