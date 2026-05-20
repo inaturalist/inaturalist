a4cbe139a62a54faeac81daf11e4febbe4e6fa80
Casey Hofford <casey.hofford@gmail.com>
2026-05-20 09:22:15 -0700

    add error reporting around FRU

--- app/assets/javascripts/fundraise_up/fundraise_up.js
+++ app/assets/javascripts/fundraise_up/fundraise_up.js
@@ -1,19 +1,39 @@
+if ( window.FundraiseUp ) {
+  FundraiseUp.catch( function ( e ) {
+    if ( window.iNaturalist ) iNaturalist.logError( e );
+  } );
+}
+
 function hideFruButtons() {
   var container = document.querySelector( ".announcement" );
+  if ( !container ) {
+    if ( window.iNaturalist ) iNaturalist.log( { "fru-embed": "no-announcement-container" } );
+    return;
+  }
   var observer = new MutationObserver( function ( ) {
-    var iframe = container.getElementsByTagName( "iframe" )[0];
-    if ( !iframe || !iframe.contentDocument ) return;
-    observer.disconnect( );
-    var innerObserver = new MutationObserver( function ( ) {
-      var renderContainer = iframe.contentDocument.getElementById( "render-container" );
-      if ( !renderContainer ) return;
-      var buttons = renderContainer.getElementsByClassName( "button" );
-      if ( buttons.length > 1 ) {
-        Array.from( buttons ).slice( 1 ).forEach( function ( button ) { button.style.display = "none"; } );
-        innerObserver.disconnect( );
-      }
-    } );
-    innerObserver.observe( iframe.contentDocument, { childList: true, subtree: true } );
+    try {
+      var iframe = container.getElementsByTagName( "iframe" )[0];
+      if ( !iframe || !iframe.contentDocument ) return;
+      observer.disconnect( );
+      var innerObserver = new MutationObserver( function ( ) {
+        try {
+          var renderContainer = iframe.contentDocument.getElementById( "render-container" );
+          if ( !renderContainer ) return;
+          var buttons = renderContainer.getElementsByClassName( "button" );
+          if ( buttons.length > 1 ) {
+            Array.from( buttons ).slice( 1 ).forEach( function ( button ) { button.style.display = "none"; } );
+            innerObserver.disconnect( );
+          }
+        } catch ( e ) {
+          if ( window.iNaturalist ) iNaturalist.logError( e );
+          innerObserver.disconnect( );
+        }
+      } );
+      innerObserver.observe( iframe.contentDocument, { childList: true, subtree: true } );
+    } catch ( e ) {
+      if ( window.iNaturalist ) iNaturalist.logError( e );
+      observer.disconnect( );
+    }
   } );
   observer.observe( container, { childList: true, subtree: true } );
 }
