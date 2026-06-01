b1e946d8ce86676e2ae61e62c8b22a0721eb4e87
sylvain-morin <sylvain.morin@inaturalist.org>
2026-06-01 17:40:42 +0200

    feat: generate sitemaps WEB-751

--- app/controllers/robots_txt_controller.rb
+++ app/controllers/robots_txt_controller.rb
@@ -1,39 +1,33 @@
 # frozen_string_literal: true
 
-class RobotsTxtController < ActionController::Base
+class RobotsTxtController < ApplicationController
+  skip_before_action( *_process_action_callbacks.map( &:filter ), raise: false )
+  skip_around_action :set_time_zone, :logstash_catchall, raise: false
+  before_action :set_site
+
   MAIN_SITE_ROBOTS_TEMPLATE_PATH = Rails.root.join( "public", "robots-www.txt" )
   PARTNER_ROBOTS_TEMPLATE_PATH = Rails.root.join( "public", "robots-partners.txt" )
 
   def robots
-    host = request.host.to_s.downcase
-    if default_site_host?( host )
+    if @site == Site.default
       render plain: main_robots_content, content_type: "text/plain; charset=utf-8"
       return
     end
 
-    site = Site.where( "url LIKE ?", "%#{host}%" ).first
-    if site
-      render plain: partner_robots_content( site ), content_type: "text/plain; charset=utf-8"
-      return
-    end
-
-    render plain: default_robots_content, content_type: "text/plain; charset=utf-8"
+    render plain: partner_robots_content, content_type: "text/plain; charset=utf-8"
   end
 
   private
 
   def main_robots_content
-    site = Site.default
-    return default_robots_content unless site
-
     template = read_robots_template( MAIN_SITE_ROBOTS_TEMPLATE_PATH )
-    sitemap_url = "#{base_url_for_site( site )}/sitemap-www/sitemap.xml"
+    sitemap_url = URI.join( @site.url, "sitemap-www/sitemap.xml" ).to_s
     [template, "Sitemap: #{sitemap_url}", ""].join( "\n" )
   end
 
-  def partner_robots_content( site )
+  def partner_robots_content
     template = read_robots_template( PARTNER_ROBOTS_TEMPLATE_PATH )
-    sitemap_url = "#{base_url_for_site( site )}/sitemap-partners/sitemap-#{site.id}.xml"
+    sitemap_url = URI.join( @site.url, "sitemap-partners/sitemap-#{@site.id}.xml" ).to_s
     [template, "Sitemap: #{sitemap_url}", ""].join( "\n" )
   end
 
@@ -41,20 +35,5 @@ class RobotsTxtController < ActionController::Base
     return "" unless File.file?( path )
 
     File.read( path ).rstrip
-  end
-
-  def base_url_for_site( site )
-    site.url.to_s.sub( %r{/$}, "" )
-  end
-
-  def default_site_host?( host )
-    Site.default&.url.to_s.downcase.include?( host )
-  end
-
-  def default_robots_content
-    <<~ROBOTS
-      User-agent: *
-      Disallow: /
-    ROBOTS
   end
 end
