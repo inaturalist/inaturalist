08b66981f31b5156d714454407515a489753125e
Dustin Baxter <baxterd@gmail.com>
2026-09-03 12:04:37 -0400

    styling and caching

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -238,7 +238,7 @@ class UsersController < ApplicationController
     @recently_active_key = "recently_active_#{I18n.locale}_site_#{@site.id}"
     @updates = @site.users_index_recent_activity
 
-    @leaderboard_key = "leaderboard_#{I18n.locale}_site_#{@site.id}_4"
+    @leaderboard_key = "leaderboard_#{I18n.locale}_site_#{@site.id}_4#{variant_cache_suffix}"
     unless fragment_exist?( @leaderboard_key )
       @most_observations = most_observations( per: "month" )
       @most_species = most_species( per: "month" )
@@ -248,7 +248,7 @@ class UsersController < ApplicationController
       @most_identifications_year = most_identifications( per: "year" )
     end
 
-    @curators_key = "users_index_curators_#{I18n.locale}_site_#{@site.id}_4"
+    @curators_key = "users_index_curators_#{I18n.locale}_site_#{@site.id}_4#{variant_cache_suffix}"
     unless fragment_exist?( @curators_key )
       @curators = User.curators.limit( 500 ).includes( :roles ).order( "updated_at DESC" )
       @curators = @curators.where( "users.site_id = ?", @site ) if @site&.prefers_site_only_users?
@@ -276,7 +276,7 @@ class UsersController < ApplicationController
     @month = params[:month].to_i unless params[:month].blank?
     @date = Date.parse("#{@year}-#{@month || '01'}-01")
     @time_unit = params[:month].blank? ? 'year' : 'month'
-    @leaderboard_key = "leaderboard_#{I18n.locale}_site_#{@site.id}_#{@year}_#{@month}"
+    @leaderboard_key = "leaderboard_#{I18n.locale}_site_#{@site.id}_#{@year}_#{@month}#{variant_cache_suffix}"
     unless fragment_exist?(@leaderboard_key)
       if params[:month].blank?
         @most_observations = most_observations(:per => 'year', :year => @year)
@@ -1090,6 +1090,10 @@ class UsersController < ApplicationController
   end
 
   protected
+
+  def variant_cache_suffix
+    request.variant.responsive? ? "_responsive" : ""
+  end
 
   def add_friend
     error_msg = nil
